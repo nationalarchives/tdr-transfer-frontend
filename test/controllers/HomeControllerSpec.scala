c3b33cadb3e62efbb31bfda23402e56814860878
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-09 08:18:21 +0000

    Add basic home page
    Home Page content taken from existing TDR prototype as placeholder until confirmation of Home Page content

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -2,15 +2,12 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
-import play.api.test._
+import play.api.mvc.Result
 import play.api.test.Helpers._
+import play.api.test._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
+import scala.concurrent._
+
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
@@ -20,8 +17,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      checkContent(home)
     }
 
     "render the index page from the application" in {
@@ -29,8 +25,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      checkContent(home)
     }
 
     "render the index page from the router" in {
@@ -38,8 +33,13 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = route(app, request).get
 
       status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      checkContent(home)
     }
+  }
+
+  private def checkContent(result: Future[Result]): Unit = {
+    contentType(result) mustBe Some("text/html")
+    contentAsString(result) must include ("Transfer Digital Records")
+    contentAsString(result) must include ("Welcome")
   }
 }
