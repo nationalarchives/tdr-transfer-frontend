60de8f90b70f3e40054585e8eeb0e168d5c563c5
Sam Palmer <github@sampalmer.dev>
2020-01-14 14:40:57 +0000

    Fixed the issues found by the scala style checks.
    These were mostly missing new lines and missing return types on the
    controller methods.

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -3,11 +3,11 @@ package controllers
 import javax.inject.{Inject, Singleton}
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.play.scala.{Security, SecurityComponents}
-import play.api.mvc.{AnyContent, Request}
+import play.api.mvc.{Action, AnyContent, Request}
 
 @Singleton
 class DashboardController @Inject()(val controllerComponents: SecurityComponents) extends Security[CommonProfile]  {
-  def dashboard() = Secure("OidcClient") { implicit request: Request[AnyContent] =>
+  def dashboard(): Action[AnyContent] = Secure("OidcClient") { implicit request: Request[AnyContent] =>
     Ok(views.html.dashboard())
   }
 }
