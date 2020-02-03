ef99523c9270171e3bd0015a1dbc854d4e2d8ccd
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-02-03 15:01:44 +0000

    Abstracted dropdown functionality to template
    Created dropdown HTML template for reuse by other pages
    
    Added function to set whether input is required for reuse by other input types

--- app/controllers/SeriesDetailsController.scala
+++ app/controllers/SeriesDetailsController.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject.{Inject, Singleton}
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.play.scala.{Security, SecurityComponents}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, Request}
 
@@ -10,7 +12,21 @@ import play.api.mvc.{Action, AnyContent, Request}
 class SeriesDetailsController @Inject()(val controllerComponents: SecurityComponents)
                                          extends Security[CommonProfile] with I18nSupport  {
 
+  val form = Form(
+    mapping(
+      "seriesNo" -> number
+    )(SelectedSeriesData.apply)(SelectedSeriesData.unapply)
+  )
+
+  val mockedData: Seq[(String, String)] = Seq(("series1", "Series 1"), ("series2", "Series 2"), ("series3", "Series 3"))
+
   def seriesDetails(): Action[AnyContent] = Secure("OidcClient") { implicit request: Request[AnyContent] =>
-    Ok(views.html.seriesDetails())
+    Ok(views.html.seriesDetails(mockedData, form))
+  }
+
+  def seriesSubmit(): Action[AnyContent] =  Secure("OidcClient") { implicit request: Request[AnyContent] =>
+    Redirect(routes.SeriesDetailsController.seriesDetails())
   }
 }
+
+case class SelectedSeriesData (seriesId: Int)
