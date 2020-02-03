e20dcdccb2c9f17e26c3f308385c5bf8cf5a5ddd
techncl <nigel_lutale@hotmail.co.uk>
2020-02-03 16:59:30 +0000

    added some more tests to the series page

--- test/viewsapi/FormFunctionSpec.scala
+++ test/viewsapi/FormFunctionSpec.scala
@@ -5,11 +5,25 @@ import org.scalatest.Matchers._
 
 class FormFunctionSpec extends FrontEndTestHelper  {
   "requiredInput function" should {
-    "render 'required' on html select tag when set to true " in {
+    "render 'required' on html 'select' tag when set to 'true'" in {
       val args: Map[Symbol, Any] = Map('_requiredOption -> true)
 
       val result = FormFunctions.inputRenderOptions(args).requiredInput()
       result shouldBe "required"
+    }
+
+    "does not render 'required' on html 'select' tag when set to 'false'" in {
+      val args: Map[Symbol, Any] = Map('_requiredOption -> false)
+
+      val result = FormFunctions.inputRenderOptions(args).requiredInput()
+      result shouldBe ""
+    }
+
+    "render 'required' on html 'select' tag when value is empty String" in {
+      val args: Map[Symbol, Any] = Map('_requiredOption -> "")
+
+      val result = FormFunctions.inputRenderOptions(args).requiredInput()
+      result shouldBe ""
     }
   }
 }
