acc04b163cc5041528d088f92b83623d14d41b6e
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-02-05 12:17:32 +0000

    Change series details routes to series

--- test/controllers/SeriesDetailsControllerSpec.scala
+++ test/controllers/SeriesDetailsControllerSpec.scala
@@ -10,7 +10,7 @@ class SeriesDetailsControllerSpec extends FrontEndTestHelper {
 
     "render the series details page with an authenticated user" in {
       val controller = new SeriesDetailsController(getAuthorisedSecurityComponents())
-      val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/seriesDetails"))
+      val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/series"))
       status(seriesDetailsPage) mustBe OK
       contentType(seriesDetailsPage) mustBe Some("text/html")
       contentAsString(seriesDetailsPage) must include ("seriesDetails.header")
@@ -21,7 +21,7 @@ class SeriesDetailsControllerSpec extends FrontEndTestHelper {
 
     "return a redirect to the auth server with an unauthenticated user" in {
       val controller = new SeriesDetailsController(getUnauthorisedSecurityComponents())
-      val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/seriesDetails"))
+      val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/series"))
       redirectLocation(seriesDetailsPage) must be(Some("/auth/realms/tdr/protocol/openid-connect/auth"))
       status(seriesDetailsPage) mustBe 303
     }
