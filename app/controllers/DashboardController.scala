4158680f40f1a7aa48da248a43d73f0838abcb9f
sam <github@sampalmer.dev>
2020-02-12 18:27:48 +0000

    Integrate graphql library into frontend

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -1,65 +1,14 @@
 package controllers
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.{ActorMaterializer, Materializer}
-import io.circe.generic.auto._
-import com.nimbusds.oauth2.sdk.token.BearerAccessToken
-import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import graphql.codegen.query.getSeries
-import io.circe.Json
+
 import javax.inject.{Inject, Singleton}
-import org.pac4j.core.profile.{CommonProfile, ProfileManager}
-import org.pac4j.play.PlayWebContext
+import org.pac4j.core.profile.CommonProfile
 import org.pac4j.play.scala.{Security, SecurityComponents}
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, Request}
-import sangria.renderer.QueryRenderer
-
-import scala.concurrent.{ExecutionContext, Future}
-
 
 @Singleton
-class DashboardController @Inject()(val controllerComponents: SecurityComponents)(implicit ec: ExecutionContext)
-  extends Security[CommonProfile] with I18nSupport with FailFastCirceSupport  {
-
-  implicit val actorSystem: ActorSystem = ActorSystem("graphql-server")
-  implicit val materializer: Materializer = Materializer(actorSystem)
+class DashboardController @Inject()(val controllerComponents: SecurityComponents) extends Security[CommonProfile] with I18nSupport  {
   def dashboard(): Action[AnyContent] = Secure("OidcClient") { implicit request: Request[AnyContent] =>
     Ok(views.html.dashboard())
-  }
-  def create(): Action[AnyContent] = Secure("OidcClient").async { implicit request: Request[AnyContent] =>
-    val webContext = new PlayWebContext(request, playSessionStore)
-    val profileManager = new ProfileManager[CommonProfile](webContext)
-    val profile = profileManager.get(true)
-    val token: BearerAccessToken = profile.get().getAttribute("access_token").asInstanceOf[BearerAccessToken]
-    val host = "localhost"
-    val port = 8081
-    val uri: Uri = Uri(s"https://$host:$port/graphql")
-    val queryJson = Json.fromString(QueryRenderer.render(getSeries.document, QueryRenderer.Compact))
-    val fields =
-      List("query" -> queryJson)
-    val body = Json.obj(fields: _*).noSpaces
-    val entity = HttpEntity(ContentTypes.`application/json`, body)
-
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8081/graphql",
-      method=HttpMethods.POST, headers=List(Authorization(OAuth2BearerToken(token.getValue))), entity=entity))
-
-    val data: Future[getSeries.Data] = responseFuture.flatMap(Unmarshal(_).to[getSeries.Data])
-
-    data.map(t => {
-      val id = t.getSeries.head.seriesid
-      Ok(views.html.dashboard())
-    })
-    //    responseFuture
-    //      .map {
-    //        case response @ HttpResponse(StatusCodes.OK, _, _, _) =>
-    //          response.entity
-    //        case _   => print(_)
-    //      }
-    //    Future.successful(Ok(views.html.dashboard()))
-
   }
 }
--- test/controllers/SeriesDetailsControllerSpec.scala
+++ test/controllers/SeriesDetailsControllerSpec.scala
@@ -1,15 +1,19 @@
 package controllers
 
+import configuration.GraphQLConfiguration
 import org.scalatest.Matchers._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import util.FrontEndTestHelper
 
+import scala.concurrent.ExecutionContext
+
 class SeriesDetailsControllerSpec extends FrontEndTestHelper {
+  implicit val ec: ExecutionContext = ExecutionContext.global
   "SeriesDetailsController GET" should {
 
     "render the series details page with an authenticated user" in {
-      val controller = new SeriesDetailsController(getAuthorisedSecurityComponents())
+      val controller = new SeriesDetailsController(getAuthorisedSecurityComponents(), new GraphQLConfiguration(app.configuration))
       val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/series"))
       status(seriesDetailsPage) mustBe OK
       contentType(seriesDetailsPage) mustBe Some("text/html")
@@ -20,7 +24,7 @@ class SeriesDetailsControllerSpec extends FrontEndTestHelper {
     }
 
     "return a redirect to the auth server with an unauthenticated user" in {
-      val controller = new SeriesDetailsController(getUnauthorisedSecurityComponents())
+      val controller = new SeriesDetailsController(getUnauthorisedSecurityComponents(), new GraphQLConfiguration(app.configuration))
       val seriesDetailsPage = controller.seriesDetails().apply(FakeRequest(GET, "/series"))
       redirectLocation(seriesDetailsPage) must be(Some("/auth/realms/tdr/protocol/openid-connect/auth"))
       status(seriesDetailsPage) mustBe 303
