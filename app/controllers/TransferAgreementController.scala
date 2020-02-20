6240ac0991b97b8f49dd93b997ace22a691f0e3a
techncl <nigel_lutale@hotmail.co.uk>
2020-02-20 12:23:57 +0000

    Basic Transfer Agreement page

---
+++ app/controllers/TransferAgreementController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import org.pac4j.core.profile.CommonProfile
+import org.pac4j.play.scala.{Security, SecurityComponents}
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, Request}
+
+@Singleton
+class TransferAgreementController @Inject()(val controllerComponents: SecurityComponents) extends Security[CommonProfile] with I18nSupport  {
+  def transferAgreement(): Action[AnyContent] = Secure("OidcClient") { implicit request: Request[AnyContent] =>
+    Ok(views.html.transferAgreement())
+  }
+}
