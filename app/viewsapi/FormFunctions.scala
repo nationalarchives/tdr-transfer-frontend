cddc5416ddec261a7f0b29dbe8992f68b059a731
Sam Palmer <github@sampalmer.dev>
2020-02-26 11:36:07 +0000

    Add error message to series details page

--- app/viewsapi/FormFunctions.scala
+++ app/viewsapi/FormFunctions.scala
@@ -1,5 +1,8 @@
 package viewsapi
 
+import play.twirl.api.Html
+import views.html.helper.FieldElements
+
 object FormFunctions {
   val requiredInputArg = '_requiredOption
 
@@ -13,6 +16,28 @@ object FormFunctions {
       if (args.exists(_ == (requiredInputArg, true))) "*" else ""
     }
   }
+  class ErrorHandling(elements: FieldElements) {
+
+    def setErrorClass(): String = {
+      if(elements.hasErrors) {
+        "govuk-form-group--error"
+      } else {
+        ""
+      }
+    }
+
+    def renderErrorMessage(): Html = {
+      if(elements.hasErrors) {
+        Html("<span id=\"error\" class=\"govuk-error-message\"><span class=\"govuk-visually-hidden\">Error:</span>" +
+          elements.errors.mkString(", ")
+        )
+      } else {
+        Html("<span/>")
+      }
+    }
+  }
+
+  implicit def errorHandling(elements: FieldElements): ErrorHandling = new ErrorHandling(elements)
 
   implicit def inputRenderOptions(args: Map[Symbol, Any]): InputRenderOptions = new InputRenderOptions(args)
 }
