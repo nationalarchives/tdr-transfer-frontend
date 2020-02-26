6715a85e4fba3f0985d351fa6b485ef395231d93
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-02-26 14:51:19 +0000

    Add Transfer Agreement page and associated tests
    Transfer agreement form back-end and front-end added. selectedInput and
    disabledInput form functions added.
    
    Unit tests for new form functions added.
    
    Co-authored-by: techncl nigel_lutale@hotmail.co.uk

--- app/viewsapi/FormFunctions.scala
+++ app/viewsapi/FormFunctions.scala
@@ -1,12 +1,26 @@
 package viewsapi
 
 object FormFunctions {
+  val selectedInputArg = '_checkedOption
   val requiredInputArg = '_requiredOption
+  val disabledInputArg = '_disabledOption
 
   class InputRenderOptions(args: Map[Symbol, Any]) {
 
+    def selectedInput(value: String): Any = {
+      if(args.contains(selectedInputArg)) {
+        args.get(selectedInputArg).filter( _ == value).map{_ => "checked"}
+      }
+    }
+
     def requiredInput(): String = {
       if (args.exists(_ == (requiredInputArg, true))) "required" else ""
+    }
+
+    def disabledInput(value: String): Any = {
+      if(args.contains(disabledInputArg)) {
+        args.get(disabledInputArg).filter( _ == value).map{_ => "disabled"}
+      }
     }
 
     def requiredLabelSuffix(): String = {
