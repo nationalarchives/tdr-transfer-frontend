4166648dee4640a223ce743d535d378d5737ffe1
Sam Palmer <github@sampalmer.dev>
2020-02-26 10:45:48 +0000

    Add an error controller

---
+++ app/controllers/ErrorController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ErrorController @Inject()(val cc: ControllerComponents)(implicit val ec: ExecutionContext)  extends AbstractController(cc) with I18nSupport {
+  def error(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.error(""))
+  }
+}
