26f0fa8c1d0fd24b549ed716a50cb9c4f30a1c5a
Sam Palmer <github@sampalmer.dev>
2020-02-27 11:58:59 +0000

    Add tests for the form functions

--- test/viewsapi/FormFunctionSpec.scala
+++ test/viewsapi/FormFunctionSpec.scala
@@ -1,9 +1,27 @@
 package viewsapi
 
-import util.FrontEndTestHelper
 import org.scalatest.Matchers._
+import play.api.data.Forms.{mapping, nonEmptyText}
+import play.api.data.{Field, Form}
+import play.api.i18n.{DefaultMessagesApi, MessagesImpl}
+import play.i18n.Lang
+import play.twirl.api.Html
+import util.FrontEndTestHelper
+import views.html.helper.FieldElements
 
 class FormFunctionSpec extends FrontEndTestHelper  {
+
+  def getFieldElements(errors: Map[Symbol, String]) = {
+    val form = Form(
+      mapping(
+        "id" -> nonEmptyText
+      )(TestData.apply)(TestData.unapply)
+    )
+    val field = Field(form, "", Seq(), Option.empty, Seq(), Option.empty)
+    val fieldElements: FieldElements = new FieldElements("", field, Html(""),errors, MessagesImpl(Lang.forCode("en-gb"), new DefaultMessagesApi()))
+    fieldElements
+  }
+
   "requiredInput function" should {
     "render 'required' on html 'select' tag when set to 'true'" in {
       val args: Map[Symbol, Any] = Map('_requiredOption -> true)
@@ -49,4 +67,40 @@ class FormFunctionSpec extends FrontEndTestHelper  {
       result shouldBe ""
     }
   }
+
+  "setErrorClass function" should {
+    "return the error class if the element has errors" in {
+      val errors: Map[Symbol, String] = Map(Symbol("_error") -> "Error")
+      val fieldElements: FieldElements = getFieldElements(errors)
+      val errorClass = viewsapi.FormFunctions.errorHandling(fieldElements).setErrorClass()
+      errorClass should be("govuk-form-group--error")
+    }
+
+    "return empty string if the element has no errors" in {
+      val errors: Map[Symbol, String] = Map()
+      val fieldElements: FieldElements = getFieldElements(errors)
+      val errorClass = viewsapi.FormFunctions.errorHandling(fieldElements).setErrorClass()
+      errorClass should be("")
+    }
+  }
+
+  "renderErrorMessage function" should {
+    "return the error html if the element has errors" in {
+      val errors: Map[Symbol, String] = Map(Symbol("_error") -> "Test Error")
+      val fieldElements: FieldElements = getFieldElements(errors)
+      val html: Html = viewsapi.FormFunctions.errorHandling(fieldElements).renderErrorMessage()
+      val expectedResult = Html("<span id=\"error\" class=\"govuk-error-message\"><span class=\"govuk-visually-hidden\">Error:</span>Test Error")
+      html should be(expectedResult)
+    }
+
+    "return empty html if the element has no errors" in {
+      val errors: Map[Symbol, String] = Map()
+      val fieldElements: FieldElements = getFieldElements(errors)
+      val html: Html = viewsapi.FormFunctions.errorHandling(fieldElements).renderErrorMessage()
+      val expectedResult = Html("")
+      html should be(expectedResult)
+    }
+  }
 }
+
+case class TestData(id: String)
