4c3be80e36c701e00cd32644cb665f16373bb554
Sam Palmer <github@sampalmer.dev>
2020-02-27 09:05:25 +0000

    Update to use Symbol() explicitly
    This is a scala 2.13 warning, I've changed them to clear the warnings.

--- test/viewsapi/FormFunctionSpec.scala
+++ test/viewsapi/FormFunctionSpec.scala
@@ -6,21 +6,21 @@ import org.scalatest.Matchers._
 class FormFunctionSpec extends FrontEndTestHelper  {
   "requiredInput function" should {
     "render 'required' on html 'select' tag when set to 'true'" in {
-      val args: Map[Symbol, Any] = Map('_requiredOption -> true)
+      val args: Map[Symbol, Any] = Map(Symbol("_requiredOption") -> true)
 
       val result = FormFunctions.inputRenderOptions(args).requiredInput()
       result shouldBe "required"
     }
 
     "not render 'required' on html 'select' tag when set to 'false'" in {
-      val args: Map[Symbol, Any] = Map('_requiredOption -> false)
+      val args: Map[Symbol, Any] = Map(Symbol("_requiredOption") -> false)
 
       val result = FormFunctions.inputRenderOptions(args).requiredInput()
       result shouldBe ""
     }
 
     "not render 'required' on html 'select' tag when no '_requiredOption" in {
-      val args: Map[Symbol, Any] = Map('_madeUpOption -> true)
+      val args: Map[Symbol, Any] = Map(Symbol("_madeUpOption") -> true)
 
       val result = FormFunctions.inputRenderOptions(args).requiredInput()
       result shouldBe ""
@@ -29,21 +29,21 @@ class FormFunctionSpec extends FrontEndTestHelper  {
 
   "requiredLabelSuffix function" should {
     "render an asterisk when set to 'true'" in {
-      val args: Map[Symbol, Any] = Map('_requiredOption -> true)
+      val args: Map[Symbol, Any] = Map(Symbol("_requiredOption") -> true)
 
       val result = FormFunctions.inputRenderOptions(args).requiredLabelSuffix()
       result shouldBe "*"
     }
 
     "not render an asterisk when set to 'false'" in {
-      val args: Map[Symbol, Any] = Map('_requiredOption -> false)
+      val args: Map[Symbol, Any] = Map(Symbol("_requiredOption") -> false)
 
       val result = FormFunctions.inputRenderOptions(args).requiredLabelSuffix()
       result shouldBe ""
     }
 
     "not render an asterisk when no '_requiredOption" in {
-      val args: Map[Symbol, Any] = Map('_madeUpOption -> true)
+      val args: Map[Symbol, Any] = Map(Symbol("_madeUpOption") -> true)
 
       val result = FormFunctions.inputRenderOptions(args).requiredLabelSuffix()
       result shouldBe ""
