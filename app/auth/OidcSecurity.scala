37df9a89557a97cb88b00aa6ff00ae99e721401c
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-03-17 16:16:15 +0000

    Remove custom Pac4J authorizer
    Use Pac4J's built-in `DefaultAuthorizers.NONE` setting to prevent
    Pac4J applying extra authorization rules, rather than defining our
    own empty authorizer.
    
    This may eventually be replaced if we add client-side authorization
    (on top of the existing API-level authorization).

--- app/auth/OidcSecurity.scala
+++ app/auth/OidcSecurity.scala
@@ -1,9 +1,10 @@
 package auth
 
+import org.pac4j.core.authorization.authorizer.DefaultAuthorizers
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.play.scala.{SecureAction, Security}
 import play.api.mvc.AnyContent
 
 trait OidcSecurity extends Security[CommonProfile] {
-  val secureAction: SecureAction[CommonProfile, AnyContent, AuthenticatedRequest] = Secure("OidcClient", authorizers = "custom")
+  val secureAction: SecureAction[CommonProfile, AnyContent, AuthenticatedRequest] = Secure("OidcClient", authorizers = DefaultAuthorizers.NONE)
 }
