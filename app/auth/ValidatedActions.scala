cbc3e86103fed220c42be6dd6a681541eb9ed6ea
sam <github@sampalmer.dev>
2020-03-18 14:45:28 +0000

    Check to see if all fields have been agreed to.
    As I said in a previous commit, it's unlikely but not impossible that
    this can happen a the moment so it's worth checking for.

--- app/auth/ValidatedActions.scala
+++ app/auth/ValidatedActions.scala
@@ -12,11 +12,24 @@ abstract class ValidatedActions() extends TokenSecurity {
   val graphqlConfiguration: GraphQLConfiguration
 
   private val getTransferAgreementClient = graphqlConfiguration.getClient[gta.Data, gta.Variables]()
+
+  private def agreed(field: Option[Boolean]) = field.isDefined && field.get
+
+  private def isTransferAgreementComplete(ta: gta.GetTransferAgreement): Boolean = {
+    val fields = List(ta.allCrownCopyright, ta.allDigital, ta.allEnglish, ta.allPublicRecords, ta.appraisalSelectionSignedOff, ta.sensitivityReviewSignedOff)
+    fields.forall(agreed)
+  }
+
   def transferAgreementExistsAction(consignmentId: Long)(f: Request[AnyContent] => Result): Action[AnyContent]
   = secureAction.async { implicit request: Request[AnyContent] =>
     val variables = gta.Variables(consignmentId)
     getTransferAgreementClient.getResult(request.token.bearerAccessToken, gta.document, Some(variables)).map(data => {
-      if (data.data.isDefined && data.data.get.getTransferAgreement.isDefined) {
+      val isComplete: Option[Boolean] = for {
+        dataDefined <- data.data
+        transferAgreement <- dataDefined.getTransferAgreement
+      } yield isTransferAgreementComplete(transferAgreement)
+
+      if (isComplete.isDefined && isComplete.get) {
         f(request)
       } else {
         Redirect(routes.TransferAgreementController.transferAgreement(consignmentId))
