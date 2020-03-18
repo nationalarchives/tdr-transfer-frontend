2377988c54bda52ca13ca54c0a1ab3ee33c5ea88
sam <github@sampalmer.dev>
2020-03-18 13:29:18 +0000

    Skeleton for the transfer agreement check action.
    This is based on
    https://www.playframework.com/documentation/2.0.3/ScalaActionsComposition
    
    The logic for checking the transfer agreement exists will go in here.
    Any time we want to re-use this, we can just re-use the action.

--- app/auth/OidcSecurity.scala
+++ app/auth/OidcSecurity.scala
@@ -1,10 +1,22 @@
 package auth
 
 import org.pac4j.core.authorization.authorizer.DefaultAuthorizers
+import controllers.routes
+
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.play.scala.{SecureAction, Security}
-import play.api.mvc.AnyContent
+import play.api.mvc.{Action, AnyContent, Request, Result}
 
 trait OidcSecurity extends Security[CommonProfile] {
+
   val secureAction: SecureAction[CommonProfile, AnyContent, AuthenticatedRequest] = Secure("OidcClient", authorizers = DefaultAuthorizers.NONE)
+
+  def transferAgreementExistsAction(consignmentId: Long)(f: Request[AnyContent] => Result): Action[AnyContent]
+  = secureAction { implicit request: Request[AnyContent] =>
+    if(false) {
+      f(request)
+    } else {
+      Redirect(routes.TransferAgreementController.transferAgreement(consignmentId))
+    }
+  }
 }
