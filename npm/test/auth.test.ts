38ed12ad6f65ffc2d4776a8988abad8bbd2d3962
sam <github@sampalmer.dev>
2020-03-19 16:09:26 +0000

    Add tests for the getToken function.
    These include a test where the user is authenticated, a test where
    they're not, an a test where there's an unexpected error and it fails.

--- npm/test/auth.test.ts
+++ npm/test/auth.test.ts
@@ -1,10 +1,46 @@
 import { getToken } from "../src/auth"
-import Keycloak, { KeycloakPromise, KeycloakError } from "keycloak-js"
+jest.mock("keycloak-js")
 
-jest.mock("keycloak-js", () => jest.fn)
-// const a: KeycloakPromise<boolean, KeycloakError> = Keycloak().init({})
-// jest.spyOn(Keycloak(), "init").mockImplementation(() => KeycloakPromise())
-test("a sample test", () => {
-  getToken()
-  expect(1 + 2).toBe(3)
+import Keycloak from "keycloak-js"
+
+class MockKeycloakAuthenticated {
+  token: string = "fake-auth-token"
+
+  init() {
+    return new Promise((resolve, _) => resolve(true))
+  }
+}
+class MockKeycloakUnauthenticated {
+  init() {
+    return new Promise((resolve, _) => resolve(false))
+  }
+}
+class MockKeycloakError {
+  init() {
+    return new Promise((_, reject) => reject("There has been an error"))
+  }
+}
+
+beforeEach(() => jest.resetModules())
+
+test("Returns an error if the user is not logged in", async () => {
+  ;(Keycloak as jest.Mock).mockImplementation(() => {
+    return new MockKeycloakUnauthenticated()
+  })
+  await expect(getToken()).rejects.toEqual("User is not authenticated")
+  jest.clearAllMocks()
+})
+
+test("Returns a token if the user is logged in", async () => {
+  ;(Keycloak as jest.Mock).mockImplementation(() => {
+    return new MockKeycloakAuthenticated()
+  })
+  await expect(getToken()).resolves.toEqual("fake-auth-token")
+})
+
+test("Returns an error if login attempt fails", async () => {
+  ;(Keycloak as jest.Mock).mockImplementation(() => {
+    return new MockKeycloakError()
+  })
+  await expect(getToken()).rejects.toEqual("There has been an error")
 })
