322a768b87fe218619b3aefa1b0c31446a34820e
sam <github@sampalmer.dev>
2020-03-20 10:00:19 +0000

    Updated the imports.
    The ValidatedActions class has moved to a new module.

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -1,11 +1,12 @@
 package controllers
 
-import auth.{OidcSecurity, ValidatedActions}
+import auth.OidcSecurity
 import configuration.{GraphQLConfiguration, KeycloakConfiguration}
 import javax.inject.{Inject, Singleton}
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, Request}
+import validation.ValidatedActions
 
 import scala.concurrent.ExecutionContext
 
