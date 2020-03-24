ddf56d3ac5f00d2a836ad5e48b6fc16c3db84547
sam <github@sampalmer.dev>
2020-03-24 11:29:48 +0000

    Add comments and example code.
    This just shows how I imagine the code being used.

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,1 +1,8 @@
-// This is the entrypoint file.
+// Pass this class instance to any class or function that needs it
+// This means we don't create the graphql client each time we need a query or mutation
+
+// import { GraphqlClient } from "./graphql"
+
+// declare var TDR_API_URL: string
+
+// const graphqlClient = new GraphqlClient(TDR_API_URL)
