175b18d0158f0cb1fbe8de4216d7ed9b24b9fb2a
sam <github@sampalmer.dev>
2020-03-24 11:49:41 +0000

    Added a concrete example on how to use the client.
    This doesn't really belong in the README as it's how to code it and once
    we start actually using it, it will be obvious but for now, this might
    help.

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,8 +1,28 @@
+import { GraphqlClient } from "./graphql"
+
+// All queries can be imported from this package. You add them in the same way you do as for the scala classes.
+import {
+  GetSeries,
+  GetSeriesQueryVariables,
+  GetSeriesQuery
+} from "@nationalarchives/tdr-generated-graphql"
+import { ApolloQueryResult } from "apollo-boost"
+
+// Needed to keep typescript happy
+declare var TDR_API_URL: string
+
 // Pass this class instance to any class or function that needs it
 // This means we don't create the graphql client each time we need a query or mutation
+const graphqlClient = new GraphqlClient(TDR_API_URL)
 
-// import { GraphqlClient } from "./graphql"
-
-// declare var TDR_API_URL: string
-
-// const graphqlClient = new GraphqlClient(TDR_API_URL)
+// Then use the imported types like this
+const getSeriesDescription: (
+  graphqlClient: GraphqlClient
+) => Promise<string> = async graphqlClient => {
+  const variables = { body: "Test" }
+  const result: ApolloQueryResult<GetSeriesQuery> = await graphqlClient.query(
+    GetSeries,
+    variables
+  )
+  return result.data!.getSeries[0].description!
+}
