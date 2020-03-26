2a41e976b613a8a4b78b0a22e4d74afa9a611f43
TomJKing <tom.king@nationalarchives.gov.uk>
2020-03-26 10:50:41 +0000

    Additional changes

--- npm/src/auth/index.ts
+++ npm/src/auth/index.ts
@@ -1,7 +1,10 @@
 import Keycloak from "keycloak-js"
 
 export const getToken: () => Promise<string | undefined> = async () => {
-  const keycloak: Keycloak.KeycloakInstance<"native"> = Keycloak()
+  //const keycloak: Keycloak.KeycloakInstance<"native"> = Keycloak()
+  const keycloak: Keycloak.KeycloakInstance<"native"> = Keycloak(
+    `${window.location.origin}/keycloak.json`
+  )
 
   const authenticated: boolean = await keycloak.init({
     promiseType: "native",
