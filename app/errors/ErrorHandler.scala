14c7ecd1e2518d75247eb45e79c568a6a497f2a1
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-07 08:46:24 +0100

    Add Play error handler
    This class captures exceptions thrown when a controller handles a request. This lets us
    do several useful things:
    
    - Return the branded error page when an exception is thrown, without having to catch
      exceptions in every controller action
    - Convert specific exceptions to specific HTTP status codes. For example, we can create
      an authorisation exception, and return a 403 Forbidden if it is thrown
    - Log all exceptions thrown by requests, without having logging scattered over all the
      controllers

---
+++ app/errors/ErrorHandler.scala
@@ -0,0 +1,29 @@
+package errors
+
+import javax.inject.Inject
+import play.api.Logging
+import play.api.http.HttpErrorHandler
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.Results._
+import play.api.mvc.{RequestHeader, Result}
+
+import scala.concurrent.Future
+
+class ErrorHandler @Inject() (val messagesApi: MessagesApi) extends HttpErrorHandler with I18nSupport with Logging {
+
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    logger.error(s"Client error with status code $statusCode at path '${request.path}' with message: '$message'")
+
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    logger.error(s"Internal server error at path '${request.path}'", exception)
+
+    Future.successful(
+      InternalServerError(views.html.error(s"A server error occurred: ${exception.getMessage}")(request2Messages(request)))
+    )
+  }
+}
