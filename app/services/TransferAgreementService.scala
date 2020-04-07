8facf0f54bde79bba67596e2f39bdc6675dc5a1f
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-07 21:19:06 +0100

    Add authorisation check to upload page
    Check the API response when checking the upload page consignment state,
    and return a 403 Forbidden response if the user is not the owner of the
    consignment.

--- app/services/TransferAgreementService.scala
+++ app/services/TransferAgreementService.scala
@@ -4,8 +4,10 @@ import java.util.UUID
 
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import configuration.GraphQLConfiguration
+import errors.AuthorisationException
 import graphql.codegen.IsTransferAgreementComplete.{isTransferAgreementComplete => itac}
 import javax.inject.Inject
+import uk.gov.nationalarchives.tdr.error.NotAuthorisedError
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -17,13 +19,12 @@ class TransferAgreementService @Inject()(val graphqlConfiguration: GraphQLConfig
   def transferAgreementExists(consignmentId: UUID, token: BearerAccessToken): Future[Boolean] = {
     val variables = itac.Variables(consignmentId)
 
-    isTransferAgreementCompleteClient.getResult(token, itac.document, Some(variables)).map(data => {
-      val isComplete: Option[Boolean] = for {
-        dataDefined <- data.data
-        transferAgreement <- dataDefined.getTransferAgreement
-      } yield transferAgreement.isAgreementComplete
-
-      isComplete.isDefined && isComplete.get
+    isTransferAgreementCompleteClient.getResult(token, itac.document, Some(variables)).map(graphQlResponse => {
+      graphQlResponse.errors match {
+        case Nil => graphQlResponse.data.flatMap(_.getTransferAgreement).exists(_.isAgreementComplete)
+        case List(authError: NotAuthorisedError) => throw new AuthorisationException(authError.message)
+        case errors => throw new RuntimeException(errors.map(e => e.message).mkString)
+      }
     })
   }
 }
