23cb60fea54e3986402fd9cd96dd8c9fcb5f0eca
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-08 14:38:00 +0100

    Return a 403 Forbidden error if there is an error when fetching series
    The frontend should currently never encounter this error, because it
    should correctly fetch series for the user's own transferring body.
    But it will catch the error if more authorisation checks are added to
    the API.

--- app/services/SeriesService.scala
+++ app/services/SeriesService.scala
@@ -1,11 +1,13 @@
 package services
 
 import configuration.GraphQLConfiguration
-
-import scala.concurrent.{ExecutionContext, Future}
+import errors.AuthorisationException
 import graphql.codegen.GetSeries.getSeries
 import javax.inject.Inject
+import uk.gov.nationalarchives.tdr.error.NotAuthorisedError
 import uk.gov.nationalarchives.tdr.keycloak.Token
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class SeriesService @Inject()(val graphqlConfiguration: GraphQLConfiguration)(implicit ec: ExecutionContext) {
 
@@ -16,14 +18,12 @@ class SeriesService @Inject()(val graphqlConfiguration: GraphQLConfiguration)(im
       .getOrElse(throw new RuntimeException(s"Transferring body missing from token for user ${token.userId}"))
     val variables: getSeries.Variables = new getSeries.Variables(userTransferringBody)
 
-    getSeriesClient.getResult(token.bearerAccessToken, getSeries.document, Some(variables)).map(data => {
-      // TODO: Use pattern matching
-      if (data.data.isDefined) {
-        data.data.get.getSeries
-      } else {
+    getSeriesClient.getResult(token.bearerAccessToken, getSeries.document, Some(variables)).map(result => {
+      result.errors match {
+        case Nil => result.data.get.getSeries
+        case List(authError: NotAuthorisedError) => throw new AuthorisationException(authError.message)
         // TODO: Create generic exception
-        val errors = data.errors.map(e => e.message).mkString
-        throw new RuntimeException(s"Error getting series for user `${token.userId}`: $errors")
+        case errors => throw new RuntimeException(errors.map(e => e.message).mkString)
       }
     })
   }
