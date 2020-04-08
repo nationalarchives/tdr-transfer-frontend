fcb8425f42de4e6ee4b259e900734a6c0f06698a
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-08 14:54:43 +0100

    Commonise error handling when making API calls
    Throw an exception if the GraphQL response contains any errors.
    
    This is being added to the frontend rather than the GraphQL client
    itself because GraphQL can potentially return multiple errors for
    a single query, and we may have to handle those differently in future.

--- app/services/TransferAgreementService.scala
+++ app/services/TransferAgreementService.scala
@@ -4,10 +4,9 @@ import java.util.UUID
 
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import configuration.GraphQLConfiguration
-import errors.AuthorisationException
 import graphql.codegen.IsTransferAgreementComplete.{isTransferAgreementComplete => itac}
 import javax.inject.Inject
-import uk.gov.nationalarchives.tdr.error.NotAuthorisedError
+import services.ApiErrorHandling.sendApiRequest
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -19,12 +18,7 @@ class TransferAgreementService @Inject()(val graphqlConfiguration: GraphQLConfig
   def transferAgreementExists(consignmentId: UUID, token: BearerAccessToken): Future[Boolean] = {
     val variables = itac.Variables(consignmentId)
 
-    isTransferAgreementCompleteClient.getResult(token, itac.document, Some(variables)).map(graphQlResponse => {
-      graphQlResponse.errors match {
-        case Nil => graphQlResponse.data.flatMap(_.getTransferAgreement).exists(_.isAgreementComplete)
-        case List(authError: NotAuthorisedError) => throw new AuthorisationException(authError.message)
-        case errors => throw new RuntimeException(errors.map(e => e.message).mkString)
-      }
-    })
+    sendApiRequest(isTransferAgreementCompleteClient, itac.document, token, variables)
+      .map(data => data.getTransferAgreement.exists(_.isAgreementComplete))
   }
 }
