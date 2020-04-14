99b2f063866fe7b4e34e8c9896cee2a3b09e8c6f
sam <github@sampalmer.dev>
2020-04-14 15:14:04 +0100

    Add function to get authenticated S3 object.
    This function logs into cognito using the keycloak token and then
    returns the S3 object once authentication has happened. This makes it
    easier to test.

--- npm/src/auth/index.ts
+++ npm/src/auth/index.ts
@@ -1,6 +1,21 @@
 import Keycloak from "keycloak-js"
+import AWS, {
+  Credentials,
+  CognitoIdentity,
+  CognitoIdentityCredentials
+} from "aws-sdk"
 
-export const getToken: () => Promise<string | undefined> = async () => {
+export interface IAuthenticatedUpload {
+  s3: AWS.S3
+  identityId: string
+}
+
+declare var TDR_AUTH_URL: string
+declare var TDR_IDENTITY_POOL_ID: string
+
+export const getToken: () => Promise<
+  Keycloak.KeycloakInstance<"native">
+> = async () => {
   const keycloak: Keycloak.KeycloakInstance<"native"> = Keycloak()
 
   const authenticated: boolean = await keycloak.init({
@@ -8,8 +23,23 @@ export const getToken: () => Promise<string | undefined> = async () => {
     onLoad: "check-sso"
   })
   if (authenticated) {
-    return keycloak.token
+    return keycloak
   } else {
     throw "User is not authenticated"
   }
+}
+
+export const getAuthenticatedUploadObject: (
+  token: string
+) => Promise<IAuthenticatedUpload> = async token => {
+  const credentials = new CognitoIdentityCredentials({
+    IdentityPoolId: TDR_IDENTITY_POOL_ID,
+    Logins: {
+      [TDR_AUTH_URL]: token
+    }
+  })
+  AWS.config.update({ region: "eu-west-2", credentials })
+  await credentials.getPromise()
+  const { identityId } = credentials
+  return { s3: new AWS.S3(), identityId }
 }
