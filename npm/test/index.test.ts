de79b31bf173388515f6354f3a8012b1ece1571b
TomJKing <tom.king@nationalarchives.gov.uk>
2020-04-16 08:53:27 +0100

    Added checks that mocks are called

--- npm/test/index.test.ts
+++ npm/test/index.test.ts
@@ -1,23 +1,18 @@
-const mockAuth = {
-  getKeycloakInstance: jest.fn()
-}
-
+import { getKeycloakInstance } from "../src/auth"
 import { renderModules } from "../src/index"
 import { mockKeycloakInstance } from "./utils"
 
-jest.mock("../src/auth", () => mockAuth)
+jest.mock("../src/auth")
+
+const mockGetKeycloakInstance = getKeycloakInstance as jest.Mock
+mockGetKeycloakInstance.mockImplementation(() =>
+  Promise.resolve(mockKeycloakInstance)
+)
 
 beforeEach(() => jest.resetModules())
-
-beforeEach(() => {
-  jest.resetModules()
-})
+afterEach(() => mockGetKeycloakInstance.mockReset())
 
 test("renderModules calls authorisation when upload form present on page", async () => {
-  const spyAuth = jest
-    .spyOn(mockAuth, "getKeycloakInstance")
-    .mockImplementation(() => Promise.resolve(mockKeycloakInstance))
-
   document.body.innerHTML =
     '<div class="govuk-file-upload">' +
     '<form id="file-upload-form">' +
@@ -26,16 +21,10 @@ test("renderModules calls authorisation when upload form present on page", async
     "</div>"
 
   renderModules()
-  expect(spyAuth).toBeCalledTimes(1)
-
-  spyAuth.mockRestore()
+  expect(mockGetKeycloakInstance).toBeCalledTimes(1)
 })
 
 test("renderModules does not call authorisation when no upload form present on page", async () => {
-  const spyAuth = jest
-    .spyOn(mockAuth, "getKeycloakInstance")
-    .mockImplementation(() => Promise.resolve(mockKeycloakInstance))
-
   document.body.innerHTML =
     "<div>" +
     '<form id="file-upload-form">' +
@@ -45,7 +34,5 @@ test("renderModules does not call authorisation when no upload form present on p
 
   renderModules()
 
-  expect(spyAuth).toBeCalledTimes(0)
-
-  spyAuth.mockRestore()
+  expect(mockGetKeycloakInstance).toBeCalledTimes(0)
 })
