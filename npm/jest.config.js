142fe2dd991e87f1f4bf90853b0d8f4b71dfad2f
sam <github@sampalmer.dev>
2020-04-17 12:34:08 +0100

    Update the environment variables

--- npm/jest.config.js
+++ npm/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   globals: {
-    TDR_AUTH_URL: "TEST_AUTH_URL",
+    TDR_IDENTITY_PROVIDER_NAME: "TEST_AUTH_URL",
     TDR_IDENTITY_POOL_ID: "TEST_IDENTITY_POOL",
-    TDR_API_URL: true
+    API_URL: true
   },
   preset: "ts-jest"
 }
