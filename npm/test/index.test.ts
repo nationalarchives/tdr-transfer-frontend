bdfc6449d89b2e870493f4bce4748c0f33ddbcf5
sam <github@sampalmer.dev>
2020-04-23 17:44:04 +0100

    Update typescript tests
    The new method of getting the front end info means that some functions
    have different arguments. The tests have been updated to show this.

--- npm/test/index.test.ts
+++ npm/test/index.test.ts
@@ -30,6 +30,16 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
+const getFrontEndInfoHtml: () => string = () => {
+  return `
+    <input type="hidden" class="api-url">
+    <input type="hidden" class="identity-provider-name">
+    <input type="hidden" class="identity-pool-id">
+    <input type="hidden" class="stage">
+    <input type="hidden" class="region">
+  `.toString()
+}
+
 test("renderModules calls authorisation when upload form present on page", async () => {
   const keycloakInstance = getKeycloakInstance as jest.Mock
   keycloakInstance.mockImplementation(() => Promise.resolve(mockKeycloak))
@@ -41,6 +51,7 @@ test("renderModules calls authorisation when upload form present on page", async
   document.body.innerHTML =
     '<div class="govuk-file-upload">' +
     '<form id="file-upload-form">' +
+    getFrontEndInfoHtml() +
     '<button class="govuk-button" type="submit" data-module="govuk-button" role="button" />' +
     "</form>" +
     "</div>"
@@ -62,12 +73,56 @@ test("renderModules does not call authorisation when no upload form present on p
   document.body.innerHTML =
     "<div>" +
     '<form id="file-upload-form">' +
+    getFrontEndInfoHtml() +
     '<button class="govuk-button" type="submit" data-module="govuk-button" role="button" />' +
     "</form>" +
     "</div>"
 
   renderModules()
 
+  expect(keycloakInstance).toBeCalledTimes(0)
+
+  keycloakInstance.mockRestore()
+})
+
+test("renderModules does not call authorisation when no identity pool id present on page", async () => {
+  const keycloakInstance = getKeycloakInstance as jest.Mock
+  keycloakInstance.mockImplementation(() => Promise.resolve(mockKeycloak))
+  const authenticateAndGetIdentity = authenticateAndGetIdentityId as jest.Mock
+  authenticateAndGetIdentity.mockImplementation(() =>
+    Promise.resolve("identityId")
+  )
+
+  document.body.innerHTML =
+    "<div>" +
+    '<form id="file-upload-form">' +
+    getFrontEndInfoHtml() +
+    '<button class="govuk-button" type="submit" data-module="govuk-button" role="button" />' +
+    "</form>" +
+    "</div>"
+
+  renderModules()
+
+  expect(keycloakInstance).toBeCalledTimes(0)
+
+  keycloakInstance.mockRestore()
+})
+
+test("renderModules does not call authorisation when the front end info is missing", () => {
+  const keycloakInstance = getKeycloakInstance as jest.Mock
+  keycloakInstance.mockImplementation(() => Promise.resolve(mockKeycloak))
+  const authenticateAndGetIdentity = authenticateAndGetIdentityId as jest.Mock
+  authenticateAndGetIdentity.mockImplementation(() =>
+    Promise.resolve("identityId")
+  )
+  document.body.innerHTML =
+    "<div>" +
+    '<form id="file-upload-form">' +
+    '<button class="govuk-button" type="submit" data-module="govuk-button" role="button" />' +
+    "</form>" +
+    "</div>"
+
+  renderModules()
   expect(keycloakInstance).toBeCalledTimes(0)
 
   keycloakInstance.mockRestore()
