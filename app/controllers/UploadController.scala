174803a6a5058ef2d828bafa4cc133fb65757a87
Rooha Sulehri <rooha.sulehri@nationalarchives.gov.uk>
2020-04-27 15:05:07 +0100

    Removed unnescessary routes

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -20,8 +20,4 @@ class UploadController @Inject()(val controllerComponents: SecurityComponents,
   def uploadPage(consignmentId: UUID): Action[AnyContent] = transferAgreementExistsAction(consignmentId) { implicit request: Request[AnyContent] =>
     Ok(views.html.upload(consignmentId))
   }
-
-  def uploadSubmit(consignmentId: UUID): Action[AnyContent] = secureAction { implicit request: Request[AnyContent] =>
-      Redirect(routes.FileProgressController.fileProgress(consignmentId))
-  }
 }
