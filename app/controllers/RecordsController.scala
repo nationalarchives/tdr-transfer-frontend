03140577d75f6411a0de6685083f881eb209f184
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-07-17 14:14:30 +0100

    Add set up for record progress update on records page
    This is a WIP and mostly set up work for getting the progress of
    AVFileMetadata on records processing page upon page load. The rest of
    the work on this will have to wait until the generated GraphQL pr has
    been merged.
    
    This is mostly just draft work so front-end may not completely match
    wireframes etc.

--- app/controllers/RecordsController.scala
+++ app/controllers/RecordsController.scala
@@ -15,6 +15,15 @@ class RecordsController @Inject()(val controllerComponents: SecurityComponents,
                                   val keycloakConfiguration: KeycloakConfiguration
                                  ) extends OidcSecurity with I18nSupport {
 
+
+  private def getRecordProcessingProgress(consignmentId: UUID): Unit = {
+  //    Create function in ConsignmentService to get the current progress of AVmetadata file processing.
+  //    Call service function here & do action to load record processing page.
+  //    Call this function in recordsPage function below.
+  //    Keeping this private helps so public functions can't directly access the API.
+  //    This will end up being similar to the seriesDetails function and private getSeriesDetails in SeriesDetailsController
+  }
+
   def recordsPage(consignmentId: UUID): Action[AnyContent] = secureAction { implicit request: Request[AnyContent] =>
     Ok(views.html.records(consignmentId))
   }
