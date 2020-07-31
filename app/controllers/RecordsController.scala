d1230469fbe0f4924739ed7c9b900d00f4657510
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-07-31 10:37:36 +0100

    Add processed AVMetadata progress bar on processing page
    This change calls the consignment API using a service to get the current
    progress of AVMetadata. This is called when the 'processing your
    records' page is loaded and updates every time the page refreshes.

--- app/controllers/RecordsController.scala
+++ app/controllers/RecordsController.scala
@@ -2,29 +2,35 @@ package controllers
 
 import java.util.UUID
 
-import auth.OidcSecurity
+import auth.TokenSecurity
 import configuration.{GraphQLConfiguration, KeycloakConfiguration}
 import javax.inject.{Inject, Singleton}
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, Request}
+import play.api.mvc.{Action, AnyContent, Request, RequestHeader}
+import services.ConsignmentService
+
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class RecordsController @Inject()(val controllerComponents: SecurityComponents,
                                   val graphqlConfiguration: GraphQLConfiguration,
-                                  val keycloakConfiguration: KeycloakConfiguration
-                                 ) extends OidcSecurity with I18nSupport {
+                                  val keycloakConfiguration: KeycloakConfiguration,
+                                  consignmentService: ConsignmentService
+                                 )(implicit val ec: ExecutionContext) extends TokenSecurity with I18nSupport {
 
-
-  private def getRecordProcessingProgress(consignmentId: UUID): Unit = {
-  //    Create function in ConsignmentService to get the current progress of AVmetadata file processing.
-  //    Call service function here & do action to load record processing page.
-  //    Call this function in recordsPage function below.
-  //    Keeping this private helps so public functions can't directly access the API.
-  //    This will end up being similar to the seriesDetails function and private getSeriesDetails in SeriesDetailsController
+  private def getRecordProcessingProgress(request: Request[AnyContent], status: Status, consignmentId: UUID)(implicit requestHeader: RequestHeader) = {
+    consignmentService.getConsignmentFileChecks(consignmentId, request.token.bearerAccessToken)
+      .map({fileChecks =>
+        status(views.html.records(
+          consignmentId,
+          fileChecks.totalFiles,
+          (fileChecks.fileChecks.antivirusProgress.filesProcessed/ fileChecks.totalFiles) * 100)
+        )
+      })
   }
 
-  def recordsPage(consignmentId: UUID): Action[AnyContent] = secureAction { implicit request: Request[AnyContent] =>
-    Ok(views.html.records(consignmentId))
+  def recordsPage(consignmentId: UUID): Action[AnyContent] = secureAction.async { implicit request: Request[AnyContent] =>
+    getRecordProcessingProgress(request, Ok, consignmentId)
   }
 }
