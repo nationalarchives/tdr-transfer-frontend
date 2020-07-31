4c99991345cb889e5c690871dfa1219cebb0a9ef
techncl <nigel_lutale@hotmail.co.uk>
2020-07-31 12:46:39 +0100

    Refactor upload code to separate logic from HTML
    This reduces the mocking set up required by the tests and also makes the upload workflow clearer.

---
+++ npm/src/upload/upload-form.ts
@@ -0,0 +1,47 @@
+import { TdrFile } from "@nationalarchives/file-information"
+import { stringType } from "aws-sdk/clients/iam"
+
+interface InputElement {
+  files?: TdrFile[]
+}
+
+interface HTMLInputTarget extends EventTarget {
+  files?: InputElement
+}
+
+export class UploadForm {
+  formElement: HTMLFormElement
+
+  constructor(formElement: HTMLFormElement) {
+    this.formElement = formElement
+  }
+
+  consignmentId: () => string = () => {
+    const value: string | null = this.formElement.getAttribute(
+      "data-consignment-id"
+    )
+    if (!value) {
+      throw Error("No consignment provided")
+    }
+    return value
+  }
+
+  addSubmitListener(
+    uploadFiles: (files: TdrFile[], consignmentId: string) => void
+  ) {
+    this.formElement.addEventListener("submit", ev => {
+      ev.preventDefault()
+      const target: HTMLInputTarget | null = ev.currentTarget
+      const files = this.retrieveFiles(target)
+      uploadFiles(files, this.consignmentId())
+    })
+  }
+
+  private retrieveFiles(target: HTMLInputTarget | null): TdrFile[] {
+    const files: TdrFile[] = target!.files!.files!
+    if (files === null || files.length === 0) {
+      throw Error("No files selected")
+    }
+    return files
+  }
+}
