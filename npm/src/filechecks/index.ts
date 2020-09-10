7d5e647d1cc49d04f973e93b1aef1e968029256b
sam <github@sampalmer.dev>
2020-09-10 15:55:48 +0100

    Add file check processing functions

---
+++ npm/src/filechecks/index.ts
@@ -0,0 +1,46 @@
+import { GraphqlClient } from "../graphql"
+import {
+  getConsignmentData,
+  updateProgressBar,
+  IFileCheckProcessed,
+  getConsignmentId
+} from "./file-check-processing"
+export class FileChecks {
+  client: GraphqlClient
+
+  constructor(client: GraphqlClient) {
+    this.client = client
+  }
+
+  dataCallback: (
+    fileChecksProcessed: IFileCheckProcessed | null
+  ) => void = fileCheckProcessed => {
+    if (fileCheckProcessed) {
+      const {
+        antivirusProcessed,
+        checksumProcessed,
+        totalFiles
+      } = fileCheckProcessed
+
+      updateProgressBar(
+        antivirusProcessed,
+        totalFiles,
+        "#av-metadata-progress-bar"
+      )
+      updateProgressBar(checksumProcessed, totalFiles, "#checksum-progress-bar")
+
+      if (antivirusProcessed == totalFiles && checksumProcessed == totalFiles) {
+        const location = `${
+          window.location.origin
+        }/consignment/${getConsignmentId()}/records-results`
+        window.location.href = location
+      }
+    }
+  }
+
+  updateFileCheckProgress() {
+    const interval = setInterval(() => {
+      getConsignmentData(this.client, this.dataCallback)
+    }, 2000)
+  }
+}
