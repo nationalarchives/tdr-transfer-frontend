713922d159eb001e09f0d10cba567e3fa2bbb408
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-10-22 14:02:51 +0100

    Change tests to reflect FileUploadInfo interface

--- npm/test/clientfilemetataupload.test.ts
+++ npm/test/clientfilemetataupload.test.ts
@@ -146,11 +146,10 @@ test("saveFileInformation returns list of file ids", async () => {
 
   const client = new GraphqlClient("https://test.im", mockKeycloakInstance)
   const uploadMetadata = new ClientFileMetadataUpload(client)
-  const result = await uploadMetadata.saveFileInformation(
-    "1",
-    3,
-    "TEST PARENT FOLDER NAME"
-  )
+  const result = await uploadMetadata.saveFileInformation(3, {
+    consignmentId: "1",
+    parentFolder: "TEST PARENT FOLDER NAME"
+  })
 
   expect(result).toEqual([1, 2, 3])
 })
@@ -160,7 +159,10 @@ test("saveFileInformation returns error if no data returned", async () => {
   const client = new GraphqlClient("https://test.im", mockKeycloakInstance)
   const uploadMetadata = new ClientFileMetadataUpload(client)
   await expect(
-    uploadMetadata.saveFileInformation("1", 3, "TEST PARENT FOLDER NAME")
+    uploadMetadata.saveFileInformation(3, {
+      consignmentId: "1",
+      parentFolder: "TEST PARENT FOLDER NAME"
+    })
   ).rejects.toStrictEqual(Error("Add files failed: no data"))
 })
 
@@ -169,7 +171,10 @@ test("saveFileInformation returns error if returned data contains errors", async
   const client = new GraphqlClient("https://test.im", mockKeycloakInstance)
   const uploadMetadata = new ClientFileMetadataUpload(client)
   await expect(
-    uploadMetadata.saveFileInformation("1", 3, "TEST PARENT FOLDER NAME")
+    uploadMetadata.saveFileInformation(3, {
+      consignmentId: "1",
+      parentFolder: "TEST PARENT FOLDER NAME"
+    })
   ).rejects.toStrictEqual(Error("Add files failed: error 1,error 2"))
 })
 
