e502778bb9930a33c1ceb51118a6b4a0435754c1
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-10-28 16:26:11 +0000

    Add controller method to use service and feed results to front-end view

--- app/controllers/FileChecksResultsController.scala
+++ app/controllers/FileChecksResultsController.scala
@@ -3,17 +3,34 @@ package controllers
 import java.util.UUID
 
 import auth.TokenSecurity
-import configuration.KeycloakConfiguration
-import javax.inject.Inject
+import configuration.{FrontEndInfoConfiguration, GraphQLConfiguration, KeycloakConfiguration}
+import javax.inject.{Inject, Singleton}
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, Request}
+import play.api.mvc.{Action, AnyContent, Request, RequestHeader}
+import services.ConsignmentService
 
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
 class FileChecksResultsController @Inject()(val controllerComponents: SecurityComponents,
-                                  val keycloakConfiguration: KeycloakConfiguration) extends TokenSecurity with I18nSupport {
+                                            val keycloakConfiguration: KeycloakConfiguration,
+                                            val graphqlConfiguration: GraphQLConfiguration,
+                                            val consignmentService: ConsignmentService,
+                                            val frontEndInfoConfiguration: FrontEndInfoConfiguration
+                                           )(implicit val ec: ExecutionContext) extends TokenSecurity with I18nSupport {
 
-  def results(consignmentId: UUID): Action[AnyContent] = secureAction { implicit request: Request[AnyContent] =>
-    Ok(views.html.fileChecksResults())
+  private def getConsignmentFolderDetails(request: Request[AnyContent], consignmentId: UUID)
+                                         (implicit requestHeader: RequestHeader): Future[ConsignmentFolderInfo] = {
+
+    consignmentService.getConsignmentFolderInfo(consignmentId, request.token.bearerAccessToken)
+      .map(consignmentInfo => ConsignmentFolderInfo(consignmentInfo.totalFiles, consignmentInfo.parentFolder))
+  }
+
+  def fileCheckResultsPage(consignmentId: UUID): Action[AnyContent] = secureAction.async { implicit request: Request[AnyContent] =>
+    getConsignmentFolderDetails(request, consignmentId).map(consignmentInfo => Ok(views.html.fileChecksResults(consignmentInfo)))
   }
 
 }
+
+case class ConsignmentFolderInfo(numberOfFiles: Int, parentFolder: Option[String])
