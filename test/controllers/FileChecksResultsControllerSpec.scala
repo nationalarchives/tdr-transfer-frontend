a153a56b3cb60baacda7adba8b69e165e28aa3c2
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-11-09 10:46:14 +0000

    Add test covering authenticated user querying consignment they don't own

--- test/controllers/FileChecksResultsControllerSpec.scala
+++ test/controllers/FileChecksResultsControllerSpec.scala
@@ -3,14 +3,18 @@ package controllers
 import java.util.UUID
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock.{okJson, post, urlEqualTo}
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, okJson, post, unauthorized, urlEqualTo}
 import configuration.GraphQLConfiguration
+import org.apache.shiro.authz.{AuthorizationException, UnauthorizedException}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import services.ConsignmentService
 import util.FrontEndTestHelper
+import org.scalatest.RecoverMethods._
+import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
+import play.api.mvc.Result
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class FileChecksResultsControllerSpec extends FrontEndTestHelper {
   implicit val ec: ExecutionContext = ExecutionContext.global
@@ -75,6 +79,35 @@ class FileChecksResultsControllerSpec extends FrontEndTestHelper {
 
       status(recordChecksResultsPage) mustBe FOUND
       redirectLocation(recordChecksResultsPage).get must startWith("/auth/realms/tdr/protocol/openid-connect/auth")
+    }
+
+    "return an error if an authenticated user tries to get information for a consignment they don't own" in {
+      val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
+      val consignmentService = new ConsignmentService(graphQLConfiguration)
+      val controller = new FileChecksResultsController(
+        getAuthorisedSecurityComponents,
+        getValidKeycloakConfiguration,
+        new GraphQLConfiguration(app.configuration),
+        consignmentService,
+        frontEndInfoConfiguration
+      )
+      val exampleApiResponse = "{\"data\":{" +
+        "\"getConsignment\":null}," +
+        "\"errors\":[{" +
+        "\"message\":\"User '7bee3c41-c059-46f6-8e9b-9ba44b0489b7' does not own consignment '0a3f617c-04e8-41c2-9f24-99622a779528'\"," +
+        "\"path\":[\"getConsignment\"],\"locations\":[{" +
+        "\"column\":3,\"line\":2}]," +
+        "\"extensions\":{" +
+        "\"code\":\"NOT_AUTHORISED\"}}]}"
+
+      wiremockServer.stubFor(post(urlEqualTo("/graphql"))
+        .willReturn(okJson(exampleApiResponse)))
+
+      val results: Throwable = controller.fileCheckResultsPage(consignmentId).apply(
+        FakeRequest(GET, s"consignment/$consignmentId/records-results")
+      ).failed.futureValue
+
+      results.getMessage mustBe("User '7bee3c41-c059-46f6-8e9b-9ba44b0489b7' does not own consignment '0a3f617c-04e8-41c2-9f24-99622a779528'")
     }
   }
 }
