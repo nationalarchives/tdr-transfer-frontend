c794d85756b0267440ddc22683a824c042d8bff5
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-18 09:50:32 +0000

    Add consignment export service

---
+++ app/services/ConsignmentExportService.scala
@@ -0,0 +1,19 @@
+package services
+
+import java.util.UUID
+
+import javax.inject.Inject
+import play.api.Configuration
+import play.api.libs.ws.{WSClient, WSResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ConsignmentExportService @Inject()(val ws: WSClient, val configuration: Configuration)(implicit val executionContext: ExecutionContext) {
+
+  def triggerExport(consignmentId: UUID, token: String): Future[Boolean] =
+    ws.url(s"${configuration.get[String]("export.baseUrl")}/$consignmentId")
+      .addHttpHeaders(("Authorization", token), ("Content-Type", "application/json"))
+      .post("{}")
+      .map(r => r.status == 200)
+      .recover(_ => false)
+}
