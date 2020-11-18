655513eba0780ea123bba95d59256811e8a8589d
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-18 11:06:50 +0000

    Move the /export from the conf path

--- app/services/ConsignmentExportService.scala
+++ app/services/ConsignmentExportService.scala
@@ -11,7 +11,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class ConsignmentExportService @Inject()(val ws: WSClient, val configuration: Configuration)(implicit val executionContext: ExecutionContext) {
 
   def triggerExport(consignmentId: UUID, token: String): Future[Boolean] =
-    ws.url(s"${configuration.get[String]("export.baseUrl")}/$consignmentId")
+    ws.url(s"${configuration.get[String]("export.baseUrl")}/export/$consignmentId")
       .addHttpHeaders(("Authorization", token), ("Content-Type", "application/json"))
       .post("{}")
       .map(r => r.status == 200)
