8ae4f96a4d0187a323f2be3d5251b7d1f625dfa2
techncl <nigel.lutale@nationalarchives.gov.uk>
2020-11-25 16:33:54 +0000

    Update transferSummaryController tests to use getConsignmentSummary query

--- test/controllers/TransferSummaryControllerSpec.scala
+++ test/controllers/TransferSummaryControllerSpec.scala
@@ -6,7 +6,7 @@ import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.{okJson, post, urlEqualTo}
 import configuration.GraphQLConfiguration
 import errors.AuthorisationException
-import graphql.codegen.GetConsignment.{getConsignment => gc}
+import graphql.codegen.GetConsignmentSummary.{getConsignmentSummary => gcs}
 import io.circe.Printer
 import io.circe.generic.auto._
 import io.circe.syntax._
@@ -40,22 +40,25 @@ class TransferSummaryControllerSpec extends FrontEndTestHelper {
 
   val langs: Langs = new EnglishLang
 
-  val consignmentId = UUID.randomUUID()
+  val consignmentId: UUID = UUID.randomUUID()
 
   "TransferSummaryController GET" should {
 
     "render the transfer summary page with an authenticated user" in {
 
-      val client = new GraphQLConfiguration(app.configuration).getClient[gc.Data, gc.Variables]()
+      val client = new GraphQLConfiguration(app.configuration).getClient[gcs.Data, gcs.Variables]()
       val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
       val consignmentService = new ConsignmentService(graphQLConfiguration)
 
       val controller = new TransferSummaryController(getAuthorisedSecurityComponents,
         new GraphQLConfiguration(app.configuration), getValidKeycloakConfiguration, consignmentService, langs)
 
+      val seriesCode = Some(gcs.GetConsignment.Series(Some("Mock Series")))
+      val transferringBodyName = Some(gcs.GetConsignment.TransferringBody(Some("MockBody")))
+      val totalFiles: Int = 3
 
-      val consignmentResponse: gc.GetConsignment = new gc.GetConsignment(UUID.randomUUID(), UUID.randomUUID())
-      val data: client.GraphqlData = client.GraphqlData(Some(gc.Data(Some(consignmentResponse))), List())
+      val consignmentResponse: gcs.GetConsignment = new gcs.GetConsignment(seriesCode, transferringBodyName, totalFiles)
+      val data: client.GraphqlData = client.GraphqlData(Some(gcs.Data(Some(consignmentResponse))), List())
       val dataString: String = data.asJson.printWith(Printer(dropNullValues = false, ""))
       wiremockServer.stubFor(post(urlEqualTo("/graphql"))
         .willReturn(okJson(dataString)))
@@ -66,9 +69,16 @@ class TransferSummaryControllerSpec extends FrontEndTestHelper {
       playStatus(transferSummaryPage) mustBe OK
       contentType(transferSummaryPage) mustBe Some("text/html")
       contentAsString(transferSummaryPage) must include("transferSummary.header")
+
       contentAsString(transferSummaryPage) must include("transferSummary.seriesReference")
+      contentAsString(transferSummaryPage) must include(seriesCode.get.code.get)
+
       contentAsString(transferSummaryPage) must include("transferSummary.transferringBody")
+      contentAsString(transferSummaryPage) must include(transferringBodyName.get.name.get)
+
       contentAsString(transferSummaryPage) must include("transferSummary.filesUploadedForTransfer")
+      contentAsString(transferSummaryPage) must include(s"$totalFiles files uploaded")
+
       contentAsString(transferSummaryPage) must include("transferSummary.openRecords")
       contentAsString(transferSummaryPage) must include("transferSummary.transferLegalOwnership")
     }
@@ -84,29 +94,10 @@ class TransferSummaryControllerSpec extends FrontEndTestHelper {
       playStatus(transferSummaryPage) mustBe FOUND
     }
 
-    "return a not found error page if an existing consignment does not have a transfer summary yet" in {
-      val client = new GraphQLConfiguration(app.configuration).getClient[gc.Data, gc.Variables]()
-      val data: client.GraphqlData = client.GraphqlData(Some(gc.Data(None)), List())
-      val dataString: String = data.asJson.printWith(Printer(dropNullValues = false, ""))
-      wiremockServer.stubFor(post(urlEqualTo("/graphql"))
-        .willReturn(okJson(dataString)))
-      val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
-      val consignmentService = new ConsignmentService(graphQLConfiguration)
-
-      val controller = new TransferSummaryController(getAuthorisedSecurityComponents, new GraphQLConfiguration(app.configuration),
-        getValidKeycloakConfiguration, consignmentService, langs)
-
-      val transferSummaryPage = controller.transferSummary(consignmentId)
-        .apply(FakeRequest(GET, s"/consignment/$consignmentId/transfer-summary").withCSRFToken)
-
-      playStatus(transferSummaryPage) mustBe NOT_FOUND
-      contentAsString(transferSummaryPage) must include("This page doesn't exist")
-    }
-
     "throws an authorisation exception when the user does not have permission to see a consignment's transfer summary" in {
-      val client = new GraphQLConfiguration(app.configuration).getClient[gc.Data, gc.Variables]()
+      val client = new GraphQLConfiguration(app.configuration).getClient[gcs.Data, gcs.Variables]()
       val graphQlError = GraphQLClient.Error("Error", Nil, Nil, Some(Extensions(Some("NOT_AUTHORISED"))))
-      val data: client.GraphqlData = client.GraphqlData(Some(gc.Data(None)), List(graphQlError))
+      val data: client.GraphqlData = client.GraphqlData(Some(gcs.Data(None)), List(graphQlError))
       val dataString: String = data.asJson.printWith(Printer(dropNullValues = false, ""))
       wiremockServer.stubFor(post(urlEqualTo("/graphql"))
         .willReturn(okJson(dataString)))
@@ -125,10 +116,22 @@ class TransferSummaryControllerSpec extends FrontEndTestHelper {
     }
 
     "display errors when an invalid form is submitted" in {
+
+      val client = new GraphQLConfiguration(app.configuration).getClient[gcs.Data, gcs.Variables]()
       val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
       val consignmentService = new ConsignmentService(graphQLConfiguration)
       val controller = new TransferSummaryController(getAuthorisedSecurityComponents, new GraphQLConfiguration(app.configuration),
         getValidKeycloakConfiguration, consignmentService, langs)
+
+      val seriesCode = Some(gcs.GetConsignment.Series(Some("Mock Series 2")))
+      val transferringBodyName = Some(gcs.GetConsignment.TransferringBody(Some("MockBody 2")))
+      val totalFiles: Int = 4
+
+      val consignmentResponse: gcs.GetConsignment = new gcs.GetConsignment(seriesCode, transferringBodyName, totalFiles)
+      val data: client.GraphqlData = client.GraphqlData(Some(gcs.Data(Some(consignmentResponse))), List())
+      val dataString: String = data.asJson.printWith(Printer(dropNullValues = false, ""))
+      wiremockServer.stubFor(post(urlEqualTo("/graphql"))
+        .willReturn(okJson(dataString)))
 
       val transferSummarySubmit = controller.transferSummarySubmit(consignmentId)
         .apply(FakeRequest(POST, s"/consignment/$consignmentId/transfer-summary").withCSRFToken)
