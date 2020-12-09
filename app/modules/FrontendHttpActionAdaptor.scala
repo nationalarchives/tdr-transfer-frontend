428b379e77487b6a2948c6ad96a1b08fbf081840
sam <sam.palmer@nationalarchives.gov.uk>
2020-12-09 08:35:39 +0000

    Render html returned by pac4j

--- app/modules/FrontendHttpActionAdaptor.scala
+++ app/modules/FrontendHttpActionAdaptor.scala
@@ -1,22 +1,28 @@
 package modules
 
 import org.pac4j.core.context.HttpConstants
-import org.pac4j.core.exception.http.HttpAction
+import org.pac4j.core.exception.http.{HttpAction, OkAction}
 import org.pac4j.play.PlayWebContext
 import org.pac4j.play.http.PlayHttpActionAdapter
 import play.mvc.{Result, Results}
+import play.twirl.api.Html
 
 class FrontendHttpActionAdaptor extends PlayHttpActionAdapter {
 
   override def adapt(action: HttpAction, context: PlayWebContext): Result = {
-    val code = action.getCode
-    if (code == HttpConstants.UNAUTHORIZED) {
-      Results.redirect("/")
-    } else if (code == HttpConstants.FORBIDDEN) {
-      Results.redirect("/")
-    } else {
-      super.adapt(action, context)
+    action match {
+      case a: OkAction =>
+        Results.ok(Html(Option(a.getContent)))
+      case _ => val code = action.getCode
+        if (code == HttpConstants.UNAUTHORIZED) {
+          Results.redirect("/")
+        } else if (code == HttpConstants.FORBIDDEN) {
+          Results.redirect("/")
+        } else {
+          super.adapt(action, context)
+        }
     }
+
   }
 
 
