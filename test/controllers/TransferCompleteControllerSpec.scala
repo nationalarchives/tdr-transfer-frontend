2e9cdde67560421be71533aa922e45960d5c3506
sam <sam.palmer@nationalarchives.gov.uk>
2021-01-20 14:32:44 +0000

    Create transfer complete page.
    We already had a placeholder but this is implementing the design for the
    page.
    We're not displaying a specific error if triggering the export fails so
    I've removed the parameter and the test for it.
    I've also renamed everything from transfer confirmation to transfer
    complete to match with the title.

---
+++ test/controllers/TransferCompleteControllerSpec.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import java.util.UUID
+
+import play.api.test.CSRFTokenHelper.CSRFRequest
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout}
+import util.FrontEndTestHelper
+
+class TransferCompleteControllerSpec extends FrontEndTestHelper {
+
+  "TransferConfirmationController GET" should {
+    "render the success page if the export was triggered successfully" in {
+      val consignmentId = UUID.randomUUID()
+      val controller = new TransferCompleteController(getAuthorisedSecurityComponents, getValidKeycloakConfiguration)
+      val transferCompleteSubmit = controller.transferComplete(consignmentId)
+        .apply(FakeRequest(GET, s"/consignment/$consignmentId/transfer-complete").withCSRFToken)
+      contentAsString(transferCompleteSubmit) must include("transferComplete.title")
+    }
+  }
+}
