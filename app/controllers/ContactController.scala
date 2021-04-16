974bd149ce766f89418907248bc0eb13b428dea4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-16 11:24:25 +0100

    Only show the sign out link if you're signed in.
    For the authenticated pages, you know the user is signed in if they can
    see them but for the home and contact pages, which don't need
    authentication, we need to check if the user is logged in and pass this
    to the view so it knows whether to show the sign out link or not.

--- app/controllers/ContactController.scala
+++ app/controllers/ContactController.scala
@@ -1,13 +1,16 @@
 package controllers
 
+import auth.UnauthenticatedController
+import org.pac4j.play.scala.SecurityComponents
+
 import javax.inject.{Inject, Singleton}
 import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
 
 @Singleton
-class ContactController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport  {
+class ContactController @Inject()(securityComponents: SecurityComponents) extends UnauthenticatedController(securityComponents) with I18nSupport  {
   def contact(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.contact())
+    Ok(views.html.contact(request.isLoggedIn))
   }
 }
 
