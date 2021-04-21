156ab79953c08f0443807b8bb3647d8fb633bb08
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-21 12:33:11 +0100

    Rename the view

--- test/controllers/SignOutControllerSpec.scala
+++ test/controllers/SignOutControllerSpec.scala
@@ -14,8 +14,8 @@ class SignOutControllerSpec extends FrontEndTestHelper {
 
       status(signOutPage) mustBe OK
       contentType(signOutPage) mustBe Some("text/html")
-      contentAsString(signOutPage) must include ("signOut.title")
-      contentAsString(signOutPage) must include ("signOut.signInLink")
+      contentAsString(signOutPage) must include ("signedOut.title")
+      contentAsString(signOutPage) must include ("signedOut.signInLink")
     }
   }
 }
