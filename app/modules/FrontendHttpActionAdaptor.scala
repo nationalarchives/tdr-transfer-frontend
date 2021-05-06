ca852a5fdcf42713ba94bd4119ebbda0e110d7c8
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-05-06 10:07:55 +0100

    Remove the match for OkAction
    Now we have control over the cache, we're returning a FoundAction which
    redirects to the url we want so this can be handled by the call to
    super.adapt

--- app/modules/FrontendHttpActionAdaptor.scala
+++ app/modules/FrontendHttpActionAdaptor.scala
@@ -1,18 +1,15 @@
 package modules
 
 import org.pac4j.core.context.HttpConstants
-import org.pac4j.core.exception.http.{HttpAction, OkAction}
+import org.pac4j.core.exception.http.HttpAction
 import org.pac4j.play.PlayWebContext
 import org.pac4j.play.http.PlayHttpActionAdapter
 import play.mvc.{Result, Results}
-import play.twirl.api.Html
 
 class FrontendHttpActionAdaptor extends PlayHttpActionAdapter {
 
   override def adapt(action: HttpAction, context: PlayWebContext): Result = {
     action match {
-      case a: OkAction =>
-        Results.ok(Html(Option(a.getContent)))
       case _ => val code = action.getCode
         if (code == HttpConstants.UNAUTHORIZED) {
           Results.redirect("/")
