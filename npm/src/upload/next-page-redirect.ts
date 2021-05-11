c40c4715ae512d19b03f27d8359b3be58e3ec784
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-05-11 09:08:41 +0100

    Fix style changes after merge with master

--- npm/src/upload/next-page-redirect.ts
+++ npm/src/upload/next-page-redirect.ts
@@ -1,7 +1,6 @@
 export const goToNextPage: () => void = () => {
-  const uploadDataFormRedirect: HTMLFormElement | null = document.querySelector(
-    "#upload-data-form"
-  )
+  const uploadDataFormRedirect: HTMLFormElement | null =
+    document.querySelector("#upload-data-form")
   if (uploadDataFormRedirect) {
     uploadDataFormRedirect.submit()
   }
