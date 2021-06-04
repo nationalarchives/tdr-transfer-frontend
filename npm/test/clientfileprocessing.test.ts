7302ecbd48ad95ea9ba67353cbbe82275b81fb9e
Rooha Sulehri <rooha.sulehri@nationalarchives.gov.uk>
2021-06-04 11:47:01 +0100

    fix test

--- npm/test/clientfileprocessing.test.ts
+++ npm/test/clientfileprocessing.test.ts
@@ -461,6 +461,7 @@ function setupUploadPageHTML() {
     </div>
     <div id="progress-bar" class="govuk-grid-row" hidden>
         <div>
+          <span id="upload-status-screen-reader"><label for="upload-records-progress-bar" class="govuk-label progress-label"></label></span>
           <progress class="progress-display" value="" max="50"></progress>
         </div>
     </div>`
