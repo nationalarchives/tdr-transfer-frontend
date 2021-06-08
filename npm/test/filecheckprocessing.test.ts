c4fe3ba03b0bd670cbf5fa28af47b4327867de5c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-06-08 12:57:16 +0100

    Remove progress bar tests and add test to check for notification bar and disabled button

--- npm/test/filecheckprocessing.test.ts
+++ npm/test/filecheckprocessing.test.ts
@@ -1,11 +1,9 @@
 const mockGraphqlClient = {
-  updateProgressBar: jest.fn(),
   getConsignmentData: jest.fn(),
   getConsignmentId: jest.fn()
 }
 import {
   IFileCheckProcessed,
-  updateProgressBar,
   getConsignmentData,
   getConsignmentId
 } from "../src/filechecks/file-check-processing"
@@ -60,25 +58,6 @@ class GraphqlClientFailure {
     return Promise.reject("error")
   }
 }
-
-test.each`
-  processed | total   | expected
-  ${1}      | ${10}   | ${10}
-  ${20}     | ${60}   | ${33}
-  ${0}      | ${1000} | ${0}
-  ${0}      | ${0}    | ${0}
-`(
-  "updateProgressBar sets the progress bar value to $expected for processed $processed and total $total",
-  ({ processed, total, expected }) => {
-    document.body.innerHTML = `<progress id="test" value="0" max="100"/>`
-    updateProgressBar(processed, total, "#test")
-    const progressBar: HTMLProgressElement | null = document.querySelector(
-      "#test"
-    )
-    expect(progressBar).not.toBeNull()
-    expect(progressBar!.value).toBe(expected)
-  }
-)
 
 test("getConsignmentData returns the correct consignment data with a successful api call", done => {
   const consignmentId = "7d4ae1dd-caeb-496d-b503-ab0e8d82a12c"
