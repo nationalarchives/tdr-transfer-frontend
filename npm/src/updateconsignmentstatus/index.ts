256bf6f563ecfc0337adb6f43b7a89608c3c93aa
techncl <nigel.lutale@nationalarchives.gov.uk>
2021-07-09 15:23:08 +0100

    Catch error when uploading status to 'completed' fails so that it could display error on frontend

--- npm/src/updateconsignmentstatus/index.ts
+++ npm/src/updateconsignmentstatus/index.ts
@@ -8,6 +8,7 @@ import {
 
 import { FetchResult } from "apollo-boost"
 import { FileUploadInfo } from "../upload/upload-form"
+import { handleUploadError } from "../errorhandling"
 
 export class UpdateConsignmentStatus {
   client: GraphqlClient
@@ -18,23 +19,26 @@ export class UpdateConsignmentStatus {
 
   async markConsignmentStatusAsCompleted(
     uploadFilesInfo: FileUploadInfo
-  ): Promise<number> {
-    const variables: MarkUploadAsCompletedMutationVariables = {
-      consignmentId: uploadFilesInfo.consignmentId
-    }
+  ): Promise<void> {
+    try {
+      const variables: MarkUploadAsCompletedMutationVariables = {
+        consignmentId: uploadFilesInfo.consignmentId
+      }
 
-    const result: FetchResult<MarkUploadAsCompletedMutation> =
-      await this.client.mutation(MarkUploadAsCompleted, variables)
+      const result: FetchResult<MarkUploadAsCompletedMutation> =
+        await this.client.mutation(MarkUploadAsCompleted, variables)
 
-    if (!result.data || !result.data.markUploadAsCompleted || result.errors) {
-      const errorMessage: string = result.errors
-        ? result.errors.toString()
-        : "no data"
-      throw Error(
-        `Marking the Consignment Status as "Completed" failed: ${errorMessage}`
+      if (!result.data || !result.data.markUploadAsCompleted || result.errors) {
+        const errorMessage: string = result.errors
+          ? result.errors.toString()
+          : "no data"
+        throw Error(errorMessage)
+      }
+    } catch (e) {
+      handleUploadError(
+        e,
+        `Marking the Consignment Status as "Completed" failed`
       )
-    } else {
-      return result.data.markUploadAsCompleted
     }
   }
 }
