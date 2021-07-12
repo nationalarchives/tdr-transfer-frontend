ad56ac5a2c1493e4da2550c668069f6543ed93c0
techncl <nigel.lutale@nationalarchives.gov.uk>
2021-07-12 17:21:41 +0100

    Throw exception instead of returning null if getting file checks info fails/returns errors

--- npm/src/filechecks/get-file-check-progress.ts
+++ npm/src/filechecks/get-file-check-progress.ts
@@ -24,40 +24,35 @@ export const getConsignmentId: () => string = () => {
 
 export const getFileChecksProgress: (
   client: GraphqlClient
-) => Promise<IFileCheckProgress | null> = async (client) => {
+) => Promise<IFileCheckProgress> = async (client) => {
   const consignmentId = getConsignmentId()
   const variables: GetFileCheckProgressQueryVariables = {
     consignmentId
   }
 
-  try {
-    const result: FetchResult<GetFileCheckProgressQuery> =
-      await client.mutation(GetFileCheckProgress, variables)
+  const result: FetchResult<GetFileCheckProgressQuery> = await client.mutation(
+    GetFileCheckProgress,
+    variables
+  )
 
-    if (!result.data || result.errors) {
-      const errorMessage: string = result.errors
-        ? result.errors.toString()
-        : "no data"
-      throw Error(`Add files failed: ${errorMessage}`)
-    } else {
-      const getConsignment = result.data.getConsignment
+  if (!result.data || result.errors) {
+    const errorMessage: string = result.errors
+      ? result.errors.toString()
+      : "no data"
+    throw Error(`Add files failed: ${errorMessage}`)
+  } else {
+    const getConsignment = result.data.getConsignment
 
-      if (getConsignment) {
-        const fileChecks = getConsignment.fileChecks
-        return {
-          antivirusProcessed: fileChecks.antivirusProgress.filesProcessed,
-          checksumProcessed: fileChecks.checksumProgress.filesProcessed,
-          ffidProcessed: fileChecks.ffidProgress.filesProcessed,
-          totalFiles: getConsignment.totalFiles
-        }
-      } else {
-        console.log(
-          `No progress metadata found for consignment ${consignmentId}`
-        )
-        return null
+    if (getConsignment) {
+      const fileChecks = getConsignment.fileChecks
+      return {
+        antivirusProcessed: fileChecks.antivirusProgress.filesProcessed,
+        checksumProcessed: fileChecks.checksumProgress.filesProcessed,
+        ffidProcessed: fileChecks.ffidProgress.filesProcessed,
+        totalFiles: getConsignment.totalFiles
       }
+    } else {
+      throw Error(`No progress metadata found for consignment ${consignmentId}`)
     }
-  } catch (error) {
-    return null
   }
 }
--- npm/src/filechecks/verify-checks-have-completed.ts
+++ npm/src/filechecks/verify-checks-have-completed.ts
@@ -1,18 +1,14 @@
 import { IFileCheckProgress } from "./get-file-check-progress"
 
 export const haveFileChecksCompleted: (
-  fileChecksProgress: IFileCheckProgress | null
-) => boolean = (fileChecksProgress: IFileCheckProgress | null) => {
-  if (fileChecksProgress) {
-    const { antivirusProcessed, checksumProcessed, ffidProcessed, totalFiles } =
-      fileChecksProgress
+  fileChecksProgress: IFileCheckProgress
+) => boolean = (fileChecksProgress: IFileCheckProgress) => {
+  const { antivirusProcessed, checksumProcessed, ffidProcessed, totalFiles } =
+    fileChecksProgress
 
-    return (
-      antivirusProcessed == totalFiles &&
-      checksumProcessed == totalFiles &&
-      ffidProcessed == totalFiles
-    )
-  } else {
-    return false
-  }
+  return (
+    antivirusProcessed == totalFiles &&
+    checksumProcessed == totalFiles &&
+    ffidProcessed == totalFiles
+  )
 }
