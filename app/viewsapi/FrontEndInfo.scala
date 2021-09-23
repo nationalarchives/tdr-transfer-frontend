a959d400123edeb2b9bc941647afcac488830c7b
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-23 10:59:02 +0100

    Update the FrontEndInfo class on the scala side.
    We don't need the cognito parameters any more and the s3 override
    endpoint has been replaced with the upload url, as this is both the
    upload endpoint and the domain we send the cookie requests to.

--- app/viewsapi/FrontEndInfo.scala
+++ app/viewsapi/FrontEndInfo.scala
@@ -2,11 +2,7 @@ package viewsapi
 
 case class FrontEndInfo(
                          apiUrl: String,
-                         s3EndpointOverride: Option[String],
-                         identityProviderName: String,
-                         identityPoolId: String,
-                         cognitoEndpointOverride: Option[String],
                          stage: String,
                          region: String,
-                         cognitoRoleArn: String
+                         uploadUrl: String
                        )
