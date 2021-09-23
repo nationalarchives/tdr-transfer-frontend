0de0567b1d6321029d253c4a9d409a656982a43b
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-23 10:59:58 +0100

    Update javascript to use the new upload endpoint.
    There are a few parts to this.
    
    * Removing the old cognito/sts code.
    * Adding in an initial call to get the cookies.
    * Adding in another call to get the cookies inside the existing function which gets a new access token when it's about to expire. This way, we'll always have fresh cookies.
    * Update the upload function to use the Keycloak url in the path.
    * Update the front end info interfaces to match the change in the scala.

--- npm/src/s3upload/index.ts
+++ npm/src/s3upload/index.ts
@@ -16,17 +16,16 @@ type TdrS3 = Pick<S3, "upload">
 
 export class S3Upload {
   s3: TdrS3
-  identityId: string
 
-  constructor(identityId: string, region: string) {
+  constructor() {
     const timeout = 20 * 60 * 1000
     const connectTimeout = 20 * 60 * 1000
-    this.s3 = new S3({ region, httpOptions: { timeout, connectTimeout } })
-    this.identityId = identityId
+    this.s3 = new S3({ httpOptions: { timeout, connectTimeout } })
   }
 
   uploadToS3: (
     consignmentId: string,
+    userId: string | undefined,
     files: ITdrFile[],
     callback: TProgressFunction,
     stage: string
@@ -34,41 +33,48 @@ export class S3Upload {
     sendData: S3.ManagedUpload.SendData[]
     processedChunks: number
     totalChunks: number
-  }> = async (consignmentId, files, callback, stage) => {
-    const totalFiles = files.length
-    const totalChunks: number = files.reduce(
-      (fileSizeTotal, file) =>
-        fileSizeTotal + (file.file.size ? file.file.size : 1),
-      0
-    )
-    let processedChunks = 0
-    const sendData: S3.ManagedUpload.SendData[] = []
-    for (const file of files) {
-      const uploadResult = await this.uploadSingleFile(
-        consignmentId,
-        stage,
-        file,
-        callback,
-        {
-          processedChunks,
-          totalChunks,
-          totalFiles
-        }
+  }> = async (consignmentId, userId, files, callback, stage) => {
+    if(userId) {
+      const totalFiles = files.length
+      const totalChunks: number = files.reduce(
+        (fileSizeTotal, file) =>
+          fileSizeTotal + (file.file.size ? file.file.size : 1),
+        0
       )
-      sendData.push(uploadResult)
-      processedChunks += file.file.size ? file.file.size : 1
+      let processedChunks = 0
+      const sendData: S3.ManagedUpload.SendData[] = []
+      for (const file of files) {
+        const uploadResult = await this.uploadSingleFile(
+          consignmentId,
+          userId,
+          stage,
+          file,
+          callback,
+          {
+            processedChunks,
+            totalChunks,
+            totalFiles
+          }
+        )
+        sendData.push(uploadResult)
+        processedChunks += file.file.size ? file.file.size : 1
+      }
+      return { sendData, processedChunks, totalChunks }
+    } else {
+      throw new Error("No valid user id found")
     }
-    return { sendData, processedChunks, totalChunks }
   }
 
   private uploadSingleFile: (
     consignmentId: string,
+    userId: string,
     stage: string,
     tdrFile: ITdrFile,
     updateProgressCallback: TProgressFunction,
     progressInfo: IFileProgressInfo
   ) => Promise<S3.ManagedUpload.SendData> = (
     consignmentId,
+    userId,
     stage,
     tdrFile,
     updateProgressCallback,
@@ -76,9 +82,10 @@ export class S3Upload {
   ) => {
     const { file, fileId } = tdrFile
     const progress: S3.ManagedUpload = this.s3.upload({
-      Key: `${this.identityId}/${consignmentId}/${fileId}`,
+      Key: `${userId}/${consignmentId}/${fileId}`,
       Body: file,
-      Bucket: `tdr-upload-files-dirty-${stage}`
+      Bucket: `suzanne-test-cloudfront`,
+      ACL: 'bucket-owner-full-control'
     })
     const { processedChunks, totalChunks, totalFiles } = progressInfo
     if (file.size >= 1) {
