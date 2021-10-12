ea15a42cbc95e84a2289fdcd54344f6dfa8ce1bf
techncl <nigel.lutale@nationalarchives.gov.uk>
2021-10-12 12:07:45 +0100

    Set the values of disabledOption and checkedOption directly instead of going through FormFunctions

--- app/viewsapi/FormFunctions.scala
+++ app/viewsapi/FormFunctions.scala
@@ -3,26 +3,12 @@ package viewsapi
 import views.html.helper.FieldElements
 
 object FormFunctions {
-  val requiredInputArg = Symbol("_requiredOption")
-  val selectedInputArg = Symbol("_checkedOption")
-  val disabledInputArg = Symbol("_disabledOption")
+  val requiredInputArg: Symbol = Symbol("_requiredOption")
 
   class InputRenderOptions(args: Map[Symbol, Any]) {
 
-    def selectedInput(value: String): Any = {
-      if(args.contains(selectedInputArg)) {
-        args.get(selectedInputArg).filter( _ == value).map{_ => "checked"}
-      }
-    }
-
     def requiredInput(): String = {
       if (args.exists(_ == (requiredInputArg, true))) "required" else ""
-    }
-
-    def disabledInput(value: String): Any = {
-      if(args.contains(disabledInputArg)) {
-        args.get(disabledInputArg).filter( _ == value).map{_ => "disabled"}
-      }
     }
 
     def requiredLabelSuffix(): String = {
