ca772593a9a29207dfcbb9196e4c624f2900132b
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-02-10 11:24:48 +0000

    Remove redundancy and add the test suite name

--- test/controllers/BeforeUploadingControllerSpec.scala
+++ test/controllers/BeforeUploadingControllerSpec.scala
@@ -3,12 +3,11 @@ package controllers
 import com.github.tomakehurst.wiremock.WireMockServer
 import configuration.GraphQLConfiguration
 import play.api.Play.materializer
-import play.api.i18n.Langs
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{GET, contentAsString, contentType, status => playStatus, _}
 import services.ConsignmentService
-import util.{EnglishLang, FrontEndTestHelper}
+import util.FrontEndTestHelper
 
 import java.util.UUID
 import scala.concurrent.ExecutionContext
@@ -27,25 +26,25 @@ class BeforeUploadingControllerSpec extends FrontEndTestHelper {
     wiremockServer.stop()
   }
 
-  val langs: Langs = new EnglishLang
+  "BeforeUploadingController GET" should {
+    "render the before uploading page for judgments" in {
+      val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
+      val beforeUploadingController = instantiateBeforeUploadingController
+      setConsignmentTypeResponse(wiremockServer, "judgment")
 
-  "render the before uploading page for judgments" in {
-    val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
-    val beforeUploadingController = instantiateBeforeUploadingController
-    setConsignmentTypeResponse(wiremockServer, "judgment")
+      val beforeUploadingPage = beforeUploadingController.beforeUploading(consignmentId)
+        .apply(FakeRequest(GET, s"/judgment/$consignmentId/before-uploading").withCSRFToken)
+      val beforeUploadingPageAsString = contentAsString(beforeUploadingPage)
 
-    val beforeUploadingPage = beforeUploadingController.beforeUploading(consignmentId)
-      .apply(FakeRequest(GET, s"/judgment/$consignmentId/before-uploading").withCSRFToken)
-    val beforeUploadingPageAsString = contentAsString(beforeUploadingPage)
-
-    playStatus(beforeUploadingPage) mustBe OK
-    contentType(beforeUploadingPage) mustBe Some("text/html")
-    beforeUploadingPageAsString must include("Your upload must contain the following information:")
-    beforeUploadingPageAsString must include(s"""<a href="/judgment/$consignmentId/upload"""" +
-      """ role="button" draggable="false" class="govuk-button" data-module="govuk-button">""")
+      playStatus(beforeUploadingPage) mustBe OK
+      contentType(beforeUploadingPage) mustBe Some("text/html")
+      beforeUploadingPageAsString must include("Your upload must contain the following information:")
+      beforeUploadingPageAsString must include(s"""<a href="/judgment/$consignmentId/upload"""" +
+        """ role="button" draggable="false" class="govuk-button" data-module="govuk-button">""")
+    }
   }
 
-  forAll(userChecks) { (user, url) =>
+  forAll(userChecks) { (_, url) =>
     s"The $url before uploading page" should {
       s"return 403 if the GET is accessed by a non-judgment user" in {
         val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
