4486f5fb76b4f8fbac6cba82ccf771aca45e814b
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-31 18:18:57 +0100

    Update to use new version of ScalaTest

--- test/controllers/TransferAgreementComplianceControllerSpec.scala
+++ test/controllers/TransferAgreementComplianceControllerSpec.scala
@@ -3,7 +3,6 @@ package controllers
 import com.github.tomakehurst.wiremock.WireMockServer
 import errors.AuthorisationException
 import graphql.codegen.AddTransferAgreementCompliance.{addTransferAgreementCompliance => atac}
-import org.scalatest.Matchers._
 import org.scalatest.concurrent.ScalaFutures._
 import play.api.Play.materializer
 import play.api.test.CSRFTokenHelper._
