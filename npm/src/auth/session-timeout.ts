ef35b3a1133bf4dbd7c536eafa1d83caeeca4757
thanhz <thanhz@hotmail.com>
2022-06-20 17:52:55 +0100

    Move session timeout logic to its own file

---
+++ npm/src/auth/session-timeout.ts
@@ -0,0 +1,66 @@
+import Keycloak from "keycloak-js"
+
+const timeoutDialog: HTMLDialogElement | null =
+  document.querySelector(".timeout-dialog")
+
+export const initialiseSessionTimeout = async (): Promise<void> => {
+  const authModule = await import("./index")
+  const errorHandlingModule = await import("../errorhandling")
+  authModule.getKeycloakInstance().then((keycloak) => {
+    const now: () => number = () => Math.round(new Date().getTime() / 1000)
+    //Set timeToShowDialog to how many seconds from expiry you want the dialog log box to appear
+    const timeToShowDialog = 300
+    setInterval(() => {
+      if (!errorHandlingModule.isError(keycloak)) {
+        const timeUntilExpire = keycloak.refreshTokenParsed!.exp! - now()
+        if (timeUntilExpire < 0) {
+          keycloak.logout()
+        } else if (timeUntilExpire < timeToShowDialog) {
+          showModal(keycloak)
+        }
+      }
+    }, 2000)
+  })
+}
+
+const showModal = async (keycloak: Keycloak): Promise<void> => {
+  const extendTimeout: HTMLButtonElement | null =
+    document.querySelector("#extend-timeout")
+  if (timeoutDialog && !timeoutDialog.open) {
+    timeoutDialog.showModal()
+    if (extendTimeout) {
+      extendTimeout.addEventListener("click", (ev) => {
+        ev.preventDefault()
+        refreshToken(keycloak)
+      })
+    }
+  }
+}
+//Function for extending the keycloak session
+const refreshToken = async (keycloak: Keycloak): Promise<void> => {
+  //Set min validity to the length of the access token, so it will always get a new one.
+  const minValidity = 3600
+  const errorHandlingModule = await import("../errorhandling")
+  if (!errorHandlingModule.isError(keycloak)) {
+    keycloak.updateToken(minValidity).then((e) => {
+      if (e && timeoutDialog && timeoutDialog.open) {
+        timeoutDialog.close()
+      }
+    })
+  }
+}
+
+export function countdown(
+  time: number,
+  progressCallback: (arg0: number) => void,
+  doneCallback: () => void
+) {
+  progressCallback(time)
+  setTimeout(function () {
+    if (time > 1) {
+      countdown(time - 1, progressCallback, doneCallback)
+    } else {
+      doneCallback()
+    }
+  }, 1000)
+}
--- npm/src/index.ts
+++ npm/src/index.ts
@@ -109,48 +109,7 @@ export const renderModules = async () => {
       errorHandlingModule.handleUploadError(frontEndInfo)
     }
   } else if (timeoutDialog) {
-    const authModule = await import("./auth")
-    const errorHandlingModule = await import("./errorhandling")
-    authModule.getKeycloakInstance().then((keycloak) => {
-      const now: () => number = () => Math.round(new Date().getTime() / 1000)
-      //Set timeToShowDialog to how many seconds from expiry you want the dialog log box to appear
-      const timeToShowDialog = 300
-      //Set min validity to the length of the access token + 300 second, so it will always get a new one.
-      const minValidity = 3600 + 300
-      setInterval(() => {
-        if (!errorHandlingModule.isError(keycloak)) {
-          const timeUntilExpire = keycloak.refreshTokenParsed!.exp! - now()
-          if (timeUntilExpire < 0) {
-            keycloak.logout()
-          } else if (timeUntilExpire < timeToShowDialog) {
-            showModal()
-          }
-        }
-      }, 2000)
-
-      const showModal: () => void = () => {
-        //Function for extending the keycloak session
-        const updateToken: () => void = () => {
-          if (!errorHandlingModule.isError(keycloak)) {
-            keycloak.updateToken(minValidity).then((e) => {
-              if (e && timeoutDialog && timeoutDialog.open) {
-                timeoutDialog.close()
-              }
-            })
-          }
-        }
-        if (timeoutDialog && !timeoutDialog.open) {
-          timeoutDialog.showModal()
-          const extendTimeout: HTMLButtonElement | null =
-            document.querySelector("#extend-timeout")
-          if (extendTimeout) {
-            extendTimeout.addEventListener("click", (ev) => {
-              ev.preventDefault()
-              updateToken()
-            })
-          }
-        }
-      }
-    })
+    const sessionTimeoutModule = await import("./auth/session-timeout")
+    await sessionTimeoutModule.initialiseSessionTimeout()
   }
 }
