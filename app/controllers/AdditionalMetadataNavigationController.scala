681663d8da44ce491f937e1eebbade785c75bda1
thanhz <thanhz@hotmail.com>
2022-08-07 22:40:55 +0100

    Add additionalMetadataNavigation Page

---
+++ app/controllers/AdditionalMetadataNavigationController.scala
@@ -0,0 +1,129 @@
+package controllers
+
+import auth.TokenSecurity
+import configuration.KeycloakConfiguration
+import graphql.codegen.GetConsignmentPaginatedFiles.getConsignmentPaginatedFiles.GetConsignment.PaginatedFiles
+import org.pac4j.play.scala.SecurityComponents
+import play.api.cache.redis.{CacheApi, RedisSet, SynchronousResult}
+import play.api.data.Form
+import play.api.data.Forms.{boolean, list, mapping, nonEmptyText, number, optional, seq, text}
+import play.api.mvc.{Action, AnyContent, Request, Result}
+import services.ConsignmentService
+import viewsapi.Caching.preventCaching
+
+import java.util.UUID
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class AdditionalMetadataNavigationController @Inject()(val consignmentService: ConsignmentService,
+                                                       val keycloakConfiguration: KeycloakConfiguration,
+                                                       val controllerComponents: SecurityComponents,
+                                                       val cacheApi: CacheApi
+                                                      ) extends TokenSecurity {
+
+  val navigationForm: Form[NodesFormData] = Form(
+    mapping(
+      "nodes" -> seq(
+        mapping(
+          "fileId" -> nonEmptyText,
+          "displayName" -> nonEmptyText,
+          "isSelected" -> boolean,
+          "isFolder" -> boolean
+        )(NodesToDisplay.apply)(NodesToDisplay.unapply)
+      ),
+      "selected" -> list(text),
+      "allNodes" -> list(text),
+      "pageSelected" -> number,
+      "folderSelected" -> text,
+      "returnToRoot" -> optional(text)
+    )(NodesFormData.apply)(NodesFormData.unapply))
+
+  def getPaginatedFiles(consignmentId: UUID, page: Int, limit: Option[Int], selectedFolderId: UUID): Action[AnyContent] = standardTypeAction(consignmentId)
+  { implicit request: Request[AnyContent] =>
+    consignmentService.getConsignmentPaginatedFile(consignmentId, page - 1, limit, selectedFolderId, request.token.bearerAccessToken)
+      .map { paginatedFiles =>
+        val totalPages = paginatedFiles.paginatedFiles.totalPages
+          .getOrElse(throw new IllegalStateException(s"No 'total pages' returned for folderId $selectedFolderId"))
+        val parentFolder = paginatedFiles.parentFolder.
+          getOrElse(throw new IllegalStateException(s"No 'parent folder' returned for consignment $consignmentId"))
+        val edges = paginatedFiles.paginatedFiles.edges.get.flatten
+        val nodesToDisplay: List[NodesToDisplay] = generateNodesToDisplay(consignmentId, edges, selectedFolderId)
+        Ok(views.html.standard.additionalMetadataNavigation(
+          consignmentId,
+          request.token.name,
+          parentFolder,
+          totalPages,
+          limit,
+          page,
+          selectedFolderId,
+          paginatedFiles.parentFolderId,
+          navigationForm.fill(NodesFormData(nodesToDisplay, selected = List(), allNodes = List(), page, selectedFolderId.toString,
+            paginatedFiles.parentFolder)))
+        ).uncache()
+      }
+  }
+
+  def submit(consignmentId: UUID, limit: Option[Int], selectedFolderId: UUID): Action[AnyContent] = standardTypeAction(consignmentId) {
+    implicit request: Request[AnyContent] =>
+      val errorFunction: Form[NodesFormData] => Future[Result] = { formWithErrors: Form[NodesFormData] =>
+        Future(Ok)
+      }
+
+      val successFunction: NodesFormData => Future[Result] = { formData: NodesFormData =>
+        val selectedFiles: RedisSet[UUID, SynchronousResult] = cacheApi.set[UUID](consignmentId.toString)
+        val pageSelected: Int = formData.pageSelected
+        val folderSelected: String = formData.folderSelected
+        formData.selected.foreach(ids => selectedFiles.add(UUID.fromString(ids)))
+        //Handle deselected Items
+        val deselectedFiles = handleDeselection(selectedFiles, formData)
+        //Deselect the parent folder if the a child file has been deselected
+        if (deselectedFiles.nonEmpty && selectedFiles.contains(selectedFolderId)) selectedFiles.remove(selectedFolderId)
+        //Pass the parent id instead of folderId, if the 'return to root' button is clicked
+        if (formData.returnToRoot.isDefined) {
+          Future(Redirect(routes.AdditionalMetadataNavigationController
+            .getPaginatedFiles(consignmentId, pageSelected, limit, UUID.fromString(formData.returnToRoot.get))))
+        } else {
+          Future(Redirect(routes.AdditionalMetadataNavigationController
+            .getPaginatedFiles(consignmentId, pageSelected, limit, UUID.fromString(folderSelected))))
+        }
+      }
+
+      val formValidationResult: Form[NodesFormData] = navigationForm.bindFromRequest()
+      formValidationResult.fold(
+        errorFunction,
+        successFunction
+      )
+  }
+
+  private def handleDeselection(selectedFiles: RedisSet[UUID, SynchronousResult], formData: NodesFormData): List[String] = {
+    val allNodes: List[String] = formData.allNodes
+    val selected: List[String] = formData.selected
+    val deselectedFiles: List[String] = allNodes.filter(ids => !selected.contains(ids))
+    deselectedFiles.foreach(ids => if (selectedFiles.contains(UUID.fromString(ids))) selectedFiles.remove(UUID.fromString(ids)))
+    deselectedFiles
+  }
+
+  private def generateNodesToDisplay(consignmentId: UUID, edges: List[PaginatedFiles.Edges], selectedFolderId: UUID): List[NodesToDisplay] = {
+    edges.map(edge => {
+      val edgeNode = edge.node
+      val selectedFiles = cacheApi.set[UUID](consignmentId.toString)
+      val isSelected = selectedFiles.contains(edge.node.fileId) || selectedFiles.contains(selectedFolderId)
+      val isFolder = edge.node.fileType.get == "Folder"
+      NodesToDisplay(
+        edgeNode.fileId.toString,
+        edgeNode.fileName.get,
+        isSelected,
+        isFolder
+      )
+    })
+  }
+}
+
+case class NodesFormData(nodesToDisplay: Seq[NodesToDisplay],
+                         selected: List[String],
+                         allNodes: List[String],
+                         pageSelected: Int,
+                         folderSelected: String,
+                         returnToRoot: Option[String])
+
+case class NodesToDisplay(fileId: String, displayName: String, isSelected: Boolean = false, isFolder: Boolean = false)
