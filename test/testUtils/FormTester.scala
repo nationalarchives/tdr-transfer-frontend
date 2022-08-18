afa7bdbb2baa5f2e0903bbb19a9d26c221b62ef6
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-08-18 18:57:42 +0100

    Enable FormTester to test multiple field types & create a class for options

--- test/testUtils/FormTester.scala
+++ test/testUtils/FormTester.scala
@@ -3,9 +3,9 @@ package testUtils
 import org.scalatest.Assertion
 import org.scalatest.matchers.must.Matchers._
 
-class FormTester(options: Map[String, (String, String)], smallCheckbox: String=" govuk-checkboxes--small") {
+class FormTester(options: List[MockInputOption], smallCheckbox: String=" govuk-checkboxes--small") {
   def generateWaysToIncorrectlySubmitAForm(): Seq[Seq[(String, String)]] = {
-    val possibleOptions: Seq[String] = options.keys.toList
+    val possibleOptions: Seq[String] = options.map(_.name)
     val optionsToSelectToGenerateFormErrors =
       for {
         numberRangeOfOptionsToSelect <- (1 until possibleOptions.length).toList
@@ -23,27 +23,27 @@ class FormTester(options: Map[String, (String, String)], smallCheckbox: String="
       s"\nThe option(s) selected ${optionsSelected.keys.mkString(", ")}, do not match the options passed into this class")
 
     options.foreach {
-      case (optionName, (label, errorMessage) ) =>
-        val htmlErrorSummary = s"""                    <a href="#error-$optionName">$errorMessage</a>"""
+      option =>
+        val htmlErrorSummary = s"""                    <a href="#error-${option.name}">${option.errorMessage}</a>"""
         val htmlErrorMessage =
-          s"""    <p class="govuk-error-message" id="error-$optionName">
+          s"""    <p class="govuk-error-message" id="error-${option.name}">
              |        <span class="govuk-visually-hidden">Error:</span>
-             |        $errorMessage
+             |        ${option.errorMessage}
              |    </p>""".stripMargin
         formStatus match {
           case "NotSubmitted" =>
-            val expectedHtmlForOption = addValuesToAttributes(optionName, label)
+            val expectedHtmlForOption = addValuesToAttributes(option)
             checkPageForElements(htmlAsString, expectedHtmlForOption, htmlErrorSummary, htmlErrorMessage, formNotSubmitted=true)
           case "PartiallySubmitted" =>
-            if(optionsSelected.contains(optionName)) {
-              val expectedHtmlForOption = addValuesToAttributes(optionName, label, "checked")
+            if(optionsSelected.contains(option.name)) {
+              val expectedHtmlForOption = addValuesToAttributes(option, selected=true)
               checkPageForElements(htmlAsString, expectedHtmlForOption, htmlErrorSummary, htmlErrorMessage)
             } else {
-              val expectedHtmlForOption = addValuesToAttributes(optionName, label)
+              val expectedHtmlForOption = addValuesToAttributes(option)
               checkPageForElements(htmlAsString, expectedHtmlForOption, htmlErrorSummary, htmlErrorMessage, elementSelected=false)
             }
           case "Submitted" =>
-            val expectedHtmlForOption = addValuesToAttributes(optionName, label, "checked", "disabled")
+            val expectedHtmlForOption = addValuesToAttributes(option, selected=true, "disabled")
             checkPageForElements(htmlAsString, expectedHtmlForOption, htmlErrorSummary, htmlErrorMessage)
           case _ => throw new IllegalStateException(
             s"Unexpected formStatus: $formStatus. statuses can only be 'NotSubmitted', 'PartiallySubmitted' and 'Submitted'"
@@ -54,15 +54,22 @@ class FormTester(options: Map[String, (String, String)], smallCheckbox: String="
 
   private def checkIfCorrectOptionsWerePassedIntoForm(optionsSelected: Map[String, String]): Boolean =
     optionsSelected.keys.toList.forall(
-      optionSelected => options.keys.toList.contains(optionSelected)
+      optionSelected => options.map(_.name).contains(optionSelected)
     )
 
-  private def addValuesToAttributes(name: String,
-                                    label: String,
-                                    checkedStatus: String="",
-                                    disabledStatus: String="",
-                                    fieldType: String="checkbox"): String = {
+  private def addValuesToAttributes(option: MockInputOption, selected: Boolean=false, disabledStatus: String=""): String = {
+    option.fieldType match {
+      case "inputCheckbox" => addValuesToCheckBoxAttributes(option.name, option.label, selected || option.selected, disabledStatus)
+      case "inputDate" => addValuesToDateAttributes(option.id, option.name, option.value, option.placeholder)
+      case "inputDropdown" => addValuesToDropdownAttributes(option.id, option.name, selected || option.selected, option.value)
+      case "inputNumeric" => addValuesToTextBoxAttributes(option.id, option.name, option.value, option.placeholder, option.fieldType)
+      case "inputRadio" => addValuesToRadioAttributes(option.id, option.name, option.label, selected || option.selected, option.value: String)
+      case "inputText" => addValuesToTextBoxAttributes(option.id, option.name, option.value, option.placeholder, option.fieldType)
+    }
+  }
 
+  private def addValuesToCheckBoxAttributes(name: String, label: String, checked: Boolean, disabledStatus: String="") = {
+    val checkedStatus = if(checked) "checked" else ""
     s"""
        |        <div class='govuk-checkboxes__item$smallCheckbox'>
        |            <input
@@ -77,12 +84,66 @@ class FormTester(options: Map[String, (String, String)], smallCheckbox: String="
        |                $label""".stripMargin
   }
 
+  private def addValuesToDateAttributes(id: String, name: String, value: String, placeholder: String): String = {
+    s"""                        <input class="govuk-input
+       |                                      govuk-date-input__input
+       |                                      govuk-input--width-${if(placeholder.length > 2) 3 else 2}
+       |                                      "
+       |                               id="$id"
+       |                               name="$name"
+       |                               value="$value"
+       |                               type="number"
+       |                               inputmode="numeric"
+       |                               placeholder="$placeholder"
+       |                               maxlength="${placeholder.length}"
+       |                        >""".stripMargin
+  }
+
+  private def addValuesToDropdownAttributes(id: String, name: String, selected: Boolean, value: String): String = {
+//    s"""    <select class="govuk-select" id="$id" name="$name"  >""".stripMargin
+    val selectedStatus = if(selected) """selected="selected" """ else ""
+
+    s"""                <option ${selectedStatus}value="$value">$value</option>""".stripMargin
+  }
+
+  private def addValuesToTextBoxAttributes(id: String, name: String, value: String,
+                                           placeholder: String, fieldType: String): String = {
+    val (inputType, inputMode) = fieldType match {
+      case "InputNumeric" => ("number", "numeric")
+      case "InputText" => ("text", "text")
+    }
+    s"""            <input
+      |                class="govuk-input govuk-input--width-5 "
+      |                id="$id"
+      |                name="$name"
+      |                type=$inputType
+      |                value="$value"
+      |                placeholder="$placeholder"
+      |                inputmode=$inputMode
+      |            >""".stripMargin
+  }
+
+  private def addValuesToRadioAttributes(id: String, name: String, label: String, selected: Boolean, value: String): String = {
+    val selectedStatus = if(selected) "checked" else ""
+    s"""            <div class="govuk-radios__item">
+      |                <input
+      |                        class="govuk-radios__input"
+      |                        id="$id"
+      |                        name="$name"
+      |                        type="radio"
+      |                        value="$value"
+      |........................
+      |                        $selectedStatus
+      |                        required
+      |                />""".stripMargin.replace("........................", "                        ")
+  }
+
   private def checkPageForElements(htmlAsString: String,
-                           expectedHtmlForOption: String,
-                           htmlErrorSummary: String,
-                           htmlErrorMessage: String,
-                           elementSelected: Boolean=true,
-                           formNotSubmitted: Boolean=false): Assertion = {
+                                   expectedHtmlForOption: String,
+                                   htmlErrorSummary: String,
+                                   htmlErrorMessage: String,
+                                   elementSelected: Boolean=true,
+                                   formNotSubmitted: Boolean=false): Assertion = {
 
     htmlAsString must include(expectedHtmlForOption)
 
@@ -100,3 +161,12 @@ class FormTester(options: Map[String, (String, String)], smallCheckbox: String="
     }
   }
 }
+
+case class MockInputOption(name: String,
+                           label: String="",
+                           id: String="",
+                           value: String="",
+                           placeholder: String="",
+                           fieldType: String="",
+                           errorMessage: String="",
+                           selected: Boolean=false)
--- test/testUtils/TransferAgreementTestHelper.scala
+++ test/testUtils/TransferAgreementTestHelper.scala
@@ -23,33 +23,45 @@ import scala.concurrent.ExecutionContext
 class TransferAgreementTestHelper(wireMockServer: WireMockServer) extends FrontEndTestHelper{
   implicit val ec: ExecutionContext = ExecutionContext.global
 
-  lazy val privateBetaOptions = Map(
-    "publicRecord" -> (
+  lazy val privateBetaOptions = List(
+    MockInputOption(
+      "publicRecord",
       "I confirm that the records are Public Records.",
-      "All records must be confirmed as public before proceeding"
+      errorMessage="All records must be confirmed as public before proceeding",
+      fieldType="inputCheckbox"
     ),
-    "crownCopyright" -> (
+    MockInputOption(
+      "crownCopyright",
       "I confirm that the records are all Crown Copyright.",
-      "All records must be confirmed Crown Copyright before proceeding"
+      errorMessage="All records must be confirmed Crown Copyright before proceeding",
+      fieldType="inputCheckbox"
     ),
-    "english" -> (
+    MockInputOption(
+      "english",
       "I confirm that the records are all in English.",
-      "All records must be confirmed as English language before proceeding"
+      errorMessage="All records must be confirmed as English language before proceeding",
+      fieldType="inputCheckbox"
     )
   )
 
-  lazy val complianceOptions = Map(
-    "droAppraisalSelection" -> (
+  lazy val complianceOptions = List(
+    MockInputOption(
+      "droAppraisalSelection",
       "I confirm that the Departmental Records Officer (DRO) has signed off on the appraisal and selection",
-      "Departmental Records Officer (DRO) must have signed off the appraisal and selection decision for records"
+      errorMessage="Departmental Records Officer (DRO) must have signed off the appraisal and selection decision for records",
+      fieldType="inputCheckbox"
     ),
-    "droSensitivity" -> (
+    MockInputOption(
+      "droSensitivity",
       "I confirm that the Departmental Records Officer (DRO) has signed off on the sensitivity review.",
-      "Departmental Records Officer (DRO) must have signed off sensitivity review"
+      errorMessage="Departmental Records Officer (DRO) must have signed off sensitivity review",
+      fieldType="inputCheckbox"
     ),
-    "openRecords" -> (
+    MockInputOption(
+      "openRecords",
       "I confirm that all records are open and no Freedom of Information (FOI) exemptions apply to these records.",
-      "All records must be open"
+      errorMessage="All records must be open",
+      fieldType="inputCheckbox"
     )
   )
 
