e716338eabe8f6978459cb1afae396f6f78860ad
thanhz <thanhz@hotmail.com>
2022-09-20 10:18:35 +0100

    Optimise imports

--- app/controllers/ViewHistoryController.scala
+++ app/controllers/ViewHistoryController.scala
@@ -1,22 +1,12 @@
 package controllers
 
 import auth.TokenSecurity
-import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import configuration.KeycloakConfiguration
-import controllers.AdditionalMetadataNavigationController._
-import graphql.codegen.GetAllDescendants.getAllDescendantIds.AllDescendants
-import graphql.codegen.GetConsignmentPaginatedFiles.getConsignmentPaginatedFiles.GetConsignment.PaginatedFiles
 import org.pac4j.play.scala.SecurityComponents
-import play.api.cache.redis.{CacheApi, RedisMap, RedisSet, SynchronousResult}
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.mvc.{Action, AnyContent, Request, Result}
+import play.api.mvc.{Action, AnyContent, Request}
 import services.ConsignmentService
-import viewsapi.Caching.preventCaching
 
-import java.util.UUID
 import javax.inject.Inject
-import scala.concurrent.Future
 
 class ViewHistoryController @Inject()(val consignmentService: ConsignmentService,
                                       val keycloakConfiguration: KeycloakConfiguration,
