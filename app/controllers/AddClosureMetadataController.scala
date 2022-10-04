ba04e0a3c9932e7aa23ec2c15611b3cf605a5f32
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-10-04 15:20:05 +0100

    Add 'multiValue' to FormFields to fix borked merge

--- app/controllers/AddClosureMetadataController.scala
+++ app/controllers/AddClosureMetadataController.scala
@@ -75,12 +75,12 @@ class AddClosureMetadataController @Inject()(val controllerComponents: SecurityC
             }
           } else {
             val metadataInput: List[UpdateFileMetadataInput] = updatedFormFields map {
-              case TextField(fieldId, _, _, nameAndValue, _, _, _) => UpdateFileMetadataInput(fieldId, nameAndValue.value)
-              case DateField(fieldId, _, _, day, month, year, _, _, _) =>
+              case TextField(fieldId, _, _, _, nameAndValue, _, _, _) => UpdateFileMetadataInput(fieldId, nameAndValue.value)
+              case DateField(fieldId, _, _, _, day, month, year, _, _, _) =>
                 val dateTime: LocalDateTime = LocalDate.of(year.value.toInt, month.value.toInt, day.value.toInt).atTime(LocalTime.MIDNIGHT)
                 UpdateFileMetadataInput(fieldId, dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME).replace("T", " "))
-              case RadioButtonGroupField(fieldId, _, _, _, selectedOption, _, _) => UpdateFileMetadataInput(fieldId, selectedOption)
-              case DropdownField(fieldId, _, _, _, selectedOption, _, _) => UpdateFileMetadataInput(fieldId, selectedOption.map(_.value).getOrElse(""))
+              case RadioButtonGroupField(fieldId, _, _, _, _, selectedOption, _, _) => UpdateFileMetadataInput(fieldId, selectedOption)
+              case DropdownField(fieldId, _, _, _, _, selectedOption, _, _) => UpdateFileMetadataInput(fieldId, selectedOption.map(_.value).getOrElse(""))
             }
             customMetadataService.saveMetadata(consignmentId, fileIds, request.token.bearerAccessToken, metadataInput).map(_ => {
               Redirect(routes.AdditionalMetadataSummaryController.getSelectedSummaryPage(consignmentId, fileIds))
