859f3febb6e1af8bb59e867d1b777e76cb60a275
vimleshg <vimlesh.gupta@nationalarchives.gov.uk>
2022-11-03 11:28:34 +0000

    TDR-2335 - show the list of consignments on the view history page

--- test/controllers/ViewHistoryControllerSpec.scala
+++ test/controllers/ViewHistoryControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import configuration.GraphQLConfiguration
+import graphql.codegen.GetConsignments.getConsignments.Consignments.Edges.Node
 import play.api.Play.materializer
 import play.api.http.Status.{FOUND, OK}
 import play.api.test.FakeRequest
@@ -9,6 +10,7 @@ import play.api.test.Helpers.{GET, contentAsString, contentType, defaultAwaitTim
 import services.ConsignmentService
 import testUtils.{CheckPageForStaticElements, FrontEndTestHelper}
 
+import java.time.format.DateTimeFormatter
 import scala.concurrent.ExecutionContext
 
 class ViewHistoryControllerSpec extends FrontEndTestHelper {
@@ -27,9 +29,10 @@ class ViewHistoryControllerSpec extends FrontEndTestHelper {
   implicit val ec: ExecutionContext = ExecutionContext.global
 
   "ViewHistoryController" should {
-    "render the view history page" in {
-      //mock the api response that returns the users consignments
+    "render the view history page with list of consignments" in {
       setConsignmentTypeResponse(wiremockServer, "standard")
+      val consignments = setConsignmentsHistoryResponse(wiremockServer)
+
       val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
       val consignmentService = new ConsignmentService(graphQLConfiguration)
       val controller = new ViewHistoryController(consignmentService, getValidStandardUserKeycloakConfiguration, getAuthorisedSecurityComponents)
@@ -42,16 +45,44 @@ class ViewHistoryControllerSpec extends FrontEndTestHelper {
       status(response) mustBe OK
       contentType(response) mustBe Some("text/html")
 
-      viewHistoryPageAsString.contains(s"""<th scope="col" class="govuk-table__header">Consignment reference</th>
-                                          |                  <th scope="col" class="govuk-table__header">Status</th>
-                                          |                  <th scope="col" class="govuk-table__header">Date of export</th>
-                                          |                  <th scope="col" class="govuk-table__header">Actions</th>""".stripMargin) mustBe true
+      viewHistoryPageAsString.contains("<h1 class=\"govuk-heading-l\">Transfer history</h1>") mustBe true
+      viewHistoryPageAsString.contains(
+        s"""<th scope="col" class="govuk-table__header">Consignment reference</th>
+           |                  <th scope="col" class="govuk-table__header">Status</th>
+           |                  <th scope="col" class="govuk-table__header">Date of export</th>
+           |                  <th scope="col" class="govuk-table__header">Actions</th>""".stripMargin) mustBe true
       viewHistoryPageAsString.contains(s"""View the history of all the consignments you have uploaded and resume incomplete or failed transfers.""") mustBe true
-      viewHistoryPageAsString.contains(s"""MOCK-123-TDR""") mustBe true
-      viewHistoryPageAsString.contains(s"""<li>Consignment uploaded by: JohnDoe@departmentofcookies.gov.uk</li>
-                                          |                          <li>Date started: 19/08/2022</li>
-                                          |                          <li>Date of export: 19/08/2022</li>
-                                          |                          <li>Number of files: 1200 records</li>""".stripMargin) mustBe true
+
+      consignments.foreach(c => verifyConsignmentRow(viewHistoryPageAsString, c.node))
+    }
+
+    "render the view history page with no consignments if the user doesn't have any consignments" in {
+      setConsignmentTypeResponse(wiremockServer, "standard")
+      setConsignmentsHistoryResponse(wiremockServer, noConsignment = true)
+
+      val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
+      val consignmentService = new ConsignmentService(graphQLConfiguration)
+      val controller = new ViewHistoryController(consignmentService, getValidStandardUserKeycloakConfiguration, getAuthorisedSecurityComponents)
+      val response = controller.viewConsignments()
+        .apply(FakeRequest(GET, s"/view-history"))
+      val viewHistoryPageAsString = contentAsString(response)
+
+      checkPageForStaticElements.checkContentOfPagesThatUseMainScala(viewHistoryPageAsString, userType = "standard", consignmentExists = false)
+
+      status(response) mustBe OK
+      contentType(response) mustBe Some("text/html")
+
+      viewHistoryPageAsString.contains("<h1 class=\"govuk-heading-l\">Transfer history</h1>") mustBe true
+      viewHistoryPageAsString.contains(
+        s"""<th scope="col" class="govuk-table__header">Consignment reference</th>
+           |                  <th scope="col" class="govuk-table__header">Status</th>
+           |                  <th scope="col" class="govuk-table__header">Date of export</th>
+           |                  <th scope="col" class="govuk-table__header">Actions</th>""".stripMargin) mustBe true
+      viewHistoryPageAsString.contains(s"""View the history of all the consignments you have uploaded and resume incomplete or failed transfers.""") mustBe true
+      viewHistoryPageAsString.contains(
+        """              <tbody class="govuk-table__body">""" +
+          "\n                " +
+          "\n              </tbody>") mustBe true
     }
 
     "redirect to the login page if the page is accessed by a logged out user" in {
@@ -64,5 +95,44 @@ class ViewHistoryControllerSpec extends FrontEndTestHelper {
       status(response) mustBe FOUND
       redirectLocation(response).get must startWith("/auth/realms/tdr/protocol/openid-connect/auth")
     }
+  }
+
+  def verifyConsignmentRow(viewHistoryPageAsString: String, node: Node): Unit = {
+
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val exportDate = node.exportDatetime.map(_.format(formatter)).get
+    val createdDate = node.createdDatetime.map(_.format(formatter)).get
+    val status = if (node.currentStatus.`export`.contains("Completed")) "Exported" else "InProgress"
+    val summary =
+      s"""
+         |                          <span class="govuk-details__summary-text">
+         |                          ${node.consignmentReference}
+         |                          </span>
+         |""".stripMargin
+    val details =
+      s"""
+         |                        <div class="govuk-details__text">
+         |                          <p class="govuk-body">Please do not delete the original files you exported until you are notified that your records have been preserved.</p>
+         |                          <ul class="govuk-list govuk-list--bullet">
+         |                            <li>Consignment uploaded by: test@example.com</li>
+         |                            <li>Date started: $createdDate</li>
+         |                            <li>Date of export: $exportDate</li>
+         |                            <li>Number of files: ${node.totalFiles} records</li>
+         |                          </ul>
+         |                        </div>
+         |""".stripMargin
+    val statusAndDate =
+      s"""
+         |                    <td class="govuk-table__cell">
+         |                      <strong class="govuk-tag govuk-tag--green">
+         |                      $status
+         |                      </strong>
+         |                    </td>
+         |                    <td class="govuk-table__cell">$exportDate</td>
+         |                    <td class="govuk-table__cell"></td>
+         |""".stripMargin
+    viewHistoryPageAsString.contains(summary) mustBe true
+    viewHistoryPageAsString.contains(details) mustBe true
+    viewHistoryPageAsString.contains(statusAndDate) mustBe true
   }
 }
