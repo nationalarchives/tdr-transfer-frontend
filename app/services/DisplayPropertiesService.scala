e3716f318dc7c0713dca8642eb5f4b7899a1f247
TomJKing <tom.king@nationalarchives.gov.uk>
2022-12-13 09:33:59 +0000

    Service to handle 'display properties'

---
+++ app/services/DisplayPropertiesService.scala
@@ -0,0 +1,21 @@
+package services
+
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken
+import configuration.GraphQLConfiguration
+import graphql.codegen.GetDisplayProperties.displayProperties.{DisplayProperties, Variables}
+import graphql.codegen.GetDisplayProperties.{displayProperties => dp}
+import services.ApiErrorHandling._
+import uk.gov.nationalarchives.tdr.GraphQLClient
+
+import java.util.UUID
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DisplayPropertiesService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(implicit val ec: ExecutionContext) {
+  private val displayPropertiesClient: GraphQLClient[dp.Data, Variables] = graphqlConfiguration.getClient[dp.Data, dp.Variables]
+
+  def getDisplayProperties(consignmentId: UUID, token: BearerAccessToken): Future[List[DisplayProperties]] = {
+    val variables = new Variables(consignmentId)
+    sendApiRequest(displayPropertiesClient, dp.document, token, variables).map(data => data.displayProperties)
+  }
+}
