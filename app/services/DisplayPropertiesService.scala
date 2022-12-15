f32bf43a3510b5fcabc51bd27feb07ad07f122c0
TomJKing <tom.king@nationalarchives.gov.uk>
2022-12-15 10:39:09 +0000

    Convert display properties to case class for use

--- app/services/DisplayPropertiesService.scala
+++ app/services/DisplayPropertiesService.scala
@@ -5,6 +5,7 @@ import configuration.GraphQLConfiguration
 import graphql.codegen.GetDisplayProperties.displayProperties.{DisplayProperties, Variables}
 import graphql.codegen.GetDisplayProperties.{displayProperties => dp}
 import graphql.codegen.types.DataType
+import graphql.codegen.types.DataType.{Boolean, Integer, Text}
 import services.ApiErrorHandling._
 import uk.gov.nationalarchives.tdr.GraphQLClient
 
@@ -15,9 +16,72 @@ import scala.concurrent.{ExecutionContext, Future}
 class DisplayPropertiesService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(implicit val ec: ExecutionContext) {
   private val displayPropertiesClient: GraphQLClient[dp.Data, Variables] = graphqlConfiguration.getClient[dp.Data, dp.Variables]()
 
-  def getDisplayProperties(consignmentId: UUID, token: BearerAccessToken): Future[List[DisplayProperties]] = {
+  implicit class AttributeHelper(attribute: Option[DisplayProperties.Attributes]) {
+    def getStringValue: String = {
+      attribute match {
+        case Some(a) => a.value.getOrElse("")
+        case _       => ""
+      }
+    }
+
+    def getBoolean: Boolean = {
+      attribute match {
+        case Some(a) => a.value.contains("true")
+        case _       => false
+      }
+    }
+  }
+
+  private def toDataType(dataType: Option[String]): DataType = {
+    dataType match {
+      case Some("text")    => Text
+      case Some("integer") => Integer
+      case Some("boolean") => Boolean
+      case _               => throw new Exception(s"Invalid data type $dataType")
+    }
+  }
+
+  private def toDisplayProperty(p: DisplayProperties): DisplayProperty = {
+    val attributes = p.attributes
+
+    val active: Boolean = attributes.find(_.attribute == "Active").getBoolean
+
+    val componentType: String = attributes.find(_.attribute == "ComponentType").getStringValue
+
+    val dataType: DataType = {
+      attributes.find(_.attribute == "Datatype") match {
+        case Some(dt) => toDataType(dt.value)
+        case _        => throw new Exception(s"No datatype")
+      }
+    }
+
+    val description: String = attributes.find(_.attribute == "Description").getStringValue
+
+    val editable: Boolean = attributes.find(_.attribute == "Editable").getBoolean
+
+    val group: String = attributes.find(_.attribute == "Group").getStringValue
+
+    val label: String = attributes.find(_.attribute == "Label").getStringValue
+
+    val multiValue: Boolean = attributes.find(_.attribute == "MultiValue").getBoolean
+
+    val displayName: String = attributes.find(_.attribute == "Name").getStringValue
+
+    val ordinal: Int = {
+      attributes.find(_.attribute == "Ordinal") match {
+        case Some(o) => o.value.get.toInt
+        case _       => 0
+      }
+    }
+
+    val propertyType: String = attributes.find(_.attribute == "PropertyType").getStringValue
+
+    DisplayProperty(active, componentType, dataType, description, displayName, editable, group, label, multiValue, ordinal, p.propertyName, propertyType)
+  }
+
+  def getDisplayProperties(consignmentId: UUID, token: BearerAccessToken): Future[List[DisplayProperty]] = {
     val variables = new Variables(consignmentId)
-    sendApiRequest(displayPropertiesClient, dp.document, token, variables).map(data => data.displayProperties)
+    sendApiRequest(displayPropertiesClient, dp.document, token, variables).map(data => data.displayProperties.map(p => toDisplayProperty(p)))
   }
 }
 
--- test/services/DisplayPropertiesServiceSpec.scala
+++ test/services/DisplayPropertiesServiceSpec.scala
@@ -5,7 +5,7 @@ import configuration.GraphQLBackend.backend
 import configuration.GraphQLConfiguration
 import errors.AuthorisationException
 import graphql.codegen.GetDisplayProperties.{displayProperties => dp}
-import graphql.codegen.types.DataType.Text
+import graphql.codegen.types.DataType.{Text, Boolean, Integer}
 import org.mockito.Mockito
 import org.mockito.Mockito.when
 import org.scalatest.BeforeAndAfterEach
@@ -44,31 +44,17 @@ class DisplayPropertiesServiceSpec extends AnyFlatSpec with MockitoSugar with Be
           dp.DisplayProperties(
             "property1",
             List(
-              dp.DisplayProperties.Attributes(
-                "attribute1",
-                Some("value1"),
-                Text
-              ),
-              dp.DisplayProperties.Attributes(
-                "attribute2",
-                Some("value2"),
-                Text
-              )
-            )
-          ),
-          dp.DisplayProperties(
-            "property2",
-            List(
-              dp.DisplayProperties.Attributes(
-                "attribute1",
-                Some("value1"),
-                Text
-              ),
-              dp.DisplayProperties.Attributes(
-                "attribute2",
-                Some("value2"),
-                Text
-              )
+              dp.DisplayProperties.Attributes("Active", Some("true"), Boolean),
+              dp.DisplayProperties.Attributes("ComponentType", Some("componentType"), Text),
+              dp.DisplayProperties.Attributes("Datatype", Some("text"), Text),
+              dp.DisplayProperties.Attributes("Description", Some("description value"), Text),
+              dp.DisplayProperties.Attributes("Name", Some("display name"), Text),
+              dp.DisplayProperties.Attributes("Editable", Some("true"), Boolean),
+              dp.DisplayProperties.Attributes("Group", Some("group"), Text),
+              dp.DisplayProperties.Attributes("Label", Some("label"), Text),
+              dp.DisplayProperties.Attributes("MultiValue", Some("false"), Boolean),
+              dp.DisplayProperties.Attributes("Ordinal", Some("11"), Integer),
+              dp.DisplayProperties.Attributes("PropertyType", Some("propertyType"), Text)
             )
           )
         )
@@ -78,42 +64,58 @@ class DisplayPropertiesServiceSpec extends AnyFlatSpec with MockitoSugar with Be
     when(displayPropertiesClient.getResult(token, dp.document, Some(dp.Variables(consignmentId))))
       .thenReturn(Future.successful(response))
 
-    val status: List[dp.DisplayProperties] = displayPropertiesService.getDisplayProperties(consignmentId, token).futureValue
-    status should equal(
-      List(
-        dp.DisplayProperties(
-          "property1",
-          List(
-            dp.DisplayProperties.Attributes(
-              "attribute1",
-              Some("value1"),
-              Text
-            ),
-            dp.DisplayProperties.Attributes(
-              "attribute2",
-              Some("value2"),
-              Text
-            )
-          )
-        ),
-        dp.DisplayProperties(
-          "property2",
-          List(
-            dp.DisplayProperties.Attributes(
-              "attribute1",
-              Some("value1"),
-              Text
-            ),
-            dp.DisplayProperties.Attributes(
-              "attribute2",
-              Some("value2"),
-              Text
-            )
+    val properties: List[DisplayProperty] = displayPropertiesService.getDisplayProperties(consignmentId, token).futureValue
+    properties.size shouldBe 1
+    val property1 = properties.find(_.propertyName == "property1").get
+    property1.active shouldBe true
+    property1.componentType shouldBe "componentType"
+    property1.dataType shouldBe Text
+    property1.displayName shouldBe "display name"
+    property1.description shouldBe "description value"
+    property1.editable shouldBe true
+    property1.group shouldBe "group"
+    property1.label shouldBe "label"
+    property1.multiValue shouldBe false
+    property1.ordinal shouldBe 11
+    property1.propertyType shouldBe "propertyType"
+  }
+
+  "getDisplayProperties" should "return default values for all optional property fields" in {
+    val data: Option[dp.Data] = Some(
+      dp.Data(
+        List(
+          dp.DisplayProperties(
+            "property1",
+            List(dp.DisplayProperties.Attributes("Datatype", Some("text"), Text))
           )
         )
       )
     )
+    val response = GraphQlResponse(data, Nil)
+    when(displayPropertiesClient.getResult(token, dp.document, Some(dp.Variables(consignmentId))))
+      .thenReturn(Future.successful(response))
+
+    val properties: List[DisplayProperty] = displayPropertiesService.getDisplayProperties(consignmentId, token).futureValue
+    properties.size shouldBe 1
+    val property1 = properties.find(_.propertyName == "property1").get
+    property1.active shouldBe false
+    property1.componentType shouldBe ""
+    property1.dataType shouldBe Text
+    property1.displayName shouldBe ""
+    property1.description shouldBe ""
+    property1.editable shouldBe false
+    property1.group shouldBe ""
+    property1.label shouldBe ""
+    property1.multiValue shouldBe false
+    property1.ordinal shouldBe 0
+    property1.propertyType shouldBe ""
   }
+
+  "getDisplayProperties" should "return an error if property is missing a 'data type" in {}
+
+  "getDisplayProperties" should "return an error if the property's data type is invalid" in {}
+
+  "getDisplayProperties" should "return an error if the property's ordinal value is invalid" in {}
 
   "getDisplayProperties" should "return an error if the API call fails" in {
     when(displayPropertiesClient.getResult(token, dp.document, Some(dp.Variables(consignmentId))))
