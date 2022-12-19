ec8e512d704bb0a87363d3eb6621c39e8200ca1b
TomJKing <tom.king@nationalarchives.gov.uk>
2022-12-19 14:23:37 +0000

    Refactor form unit test to account for different types of form

---
+++ test/testUtils/CheckFormPageElements.scala
@@ -0,0 +1,56 @@
+package testUtils
+
+import org.scalatest.matchers.must.Matchers.{convertToAnyMustWrapper, include}
+
+class CheckFormPageElements() {
+  val twoOrMoreSpaces = "\\s{2,}"
+
+  private val expectedClosureFormHtmlElements = Set(
+    """      <title>Add closure metadata to files</title>""",
+    """      <h1 class="govuk-heading-l">Add closure metadata to</h1>""",
+    """      <p class="govuk-body">Enter metadata for closure fields here.</p>""",
+    """            <h2 class="govuk-label govuk-label--m">
+      |                FOI decision asserted
+      |            </h2>""",
+    """        <div id="date-input-FoiExemptionAsserted-hint" class="govuk-hint">
+      |            Date of the Advisory Council approval (or SIRO approval if appropriate)
+      |        </div>""",
+    """            <h2 class="govuk-label govuk-label--m">
+      |                Closure start date
+      |            </h2>""",
+    """        <div id="date-input-ClosureStartDate-hint" class="govuk-hint">
+      |            This has been defaulted to the last date modified. If this is not correct, amend the field below.
+      |        </div>""",
+    """        <label class="govuk-label govuk-label--m" for=years>
+      |            Closure period
+      |        </label>""",
+    """    <div id="numeric-input-hint" class="govuk-hint">
+      |        Number of years the record is closed from the closure start date
+      |    </div>""",
+    """            <label class="govuk-label govuk-label--m" for="inputdropdown-FoiExemptionCode">
+      |                FOI exemption code
+      |            </label>""".replace("................", "                "),
+    """        <div id="inputdropdown-FoiExemptionCode-hint" class="govuk-hint">
+      |            Add one or more exemption code to this closure. Here is a<a target="_blank" href="https://www.legislation.gov.uk/ukpga/2000/36/contents">full list of FOI codes and their designated exemptions</a>.
+      |        </div>""",
+    """<select class="govuk-select" id="inputdropdown-FoiExemptionCode" name="inputdropdown-FoiExemptionCode"  >""",
+    """        <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
+      |            Is the title closed?
+      |        </legend>""",
+    """        <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
+      |            Is the description closed?
+      |        </legend>"""
+  )
+
+  def checkFormContent(metadataType: String, formPageAsFormattedString: String): Unit = {
+    val formPageAsString = formPageAsFormattedString.replaceAll(twoOrMoreSpaces, "")
+    val expectedFormHtmlElements = if (metadataType == "descriptive") { Set() }
+    else expectedClosureFormHtmlElements
+
+    expectedFormHtmlElements.foreach { htmlElement =>
+      formPageAsString must include(
+        htmlElement.stripMargin.replaceAll(twoOrMoreSpaces, "")
+      )
+    }
+  }
+}
