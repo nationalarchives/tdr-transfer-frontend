f7f82a1b5641153e43b5c87163a8ac7ee1192d2e
TomJKing <tom.king@nationalarchives.gov.uk>
2022-12-20 12:47:34 +0000

    Controller unit test set up

--- test/controllers/AddAdditionalMetadataControllerSpec.scala
+++ test/controllers/AddAdditionalMetadataControllerSpec.scala
@@ -22,7 +22,7 @@ import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{GET, contentAsString, contentType, status => playStatus, _}
 import services.{ConsignmentService, CustomMetadataService, DisplayPropertiesService}
-import testUtils.DefaultMockFormOptions.{expectedClosureDefaultOptions, expectedClosureDependencyDefaultOptions}
+import testUtils.DefaultMockFormOptions.{expectedClosureDefaultOptions, expectedClosureDependencyDefaultOptions, expectedDescriptiveDefaultOptions}
 import testUtils.{CheckFormPageElements, CheckPageForStaticElements, FormTester, FrontEndTestHelper}
 import uk.gov.nationalarchives.tdr.GraphQLClient
 import org.scalatest.concurrent.ScalaFutures._
@@ -55,6 +55,7 @@ class AddAdditionalMetadataControllerSpec extends FrontEndTestHelper {
   }
 
   private val closureMetadataType = metadataType(0)
+  private val descriptiveMetadataType = metadataType(1)
 
   "AddAdditionalMetadataController GET" should {
     "render the add additional metadata page, with the default closure form, if file has no additional metadata, for an authenticated standard user" in {
@@ -87,6 +88,32 @@ class AddAdditionalMetadataControllerSpec extends FrontEndTestHelper {
 
       checkPageForStaticElements.checkContentOfPagesThatUseMainScala(addAdditionalMetadataPageAsString, userType = "standard")
       checkFormElements.checkFormContent(closureMetadataType, addAdditionalMetadataPageAsString)
+      formTester.checkHtmlForOptionAndItsAttributes(addAdditionalMetadataPageAsString, expectedDefaultForm.toMap)
+    }
+
+    "render the add additional metadata page, with the default descriptive form, if file has no additional metadata, for an authenticated standard user" in {
+      val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
+      val addAdditionalMetadataController = instantiateAddAdditionalMetadataController()
+      val formTester = new FormTester(expectedDescriptiveDefaultOptions)
+
+      setConsignmentTypeResponse(wiremockServer, "standard")
+      setConsignmentFilesMetadataResponse(wiremockServer, fileHasMetadata = false)
+      setCustomMetadataResponse(wiremockServer)
+      setDisplayPropertiesResponse(wiremockServer)
+
+      val addAdditionalMetadataPage = addAdditionalMetadataController
+        .addAdditionalMetadata(consignmentId, descriptiveMetadataType, fileIds)
+        .apply(FakeRequest(GET, s"/standard/$consignmentId/additional-metadata/add/$descriptiveMetadataType").withCSRFToken)
+      val addAdditionalMetadataPageAsString = contentAsString(addAdditionalMetadataPage)
+      val expectedDefaultForm = Seq(
+        ("inputtext-description-description", ""),
+        ("inputdropdown-Language", "English")
+      )
+      playStatus(addAdditionalMetadataPage) mustBe OK
+      contentType(addAdditionalMetadataPage) mustBe Some("text/html")
+
+      checkPageForStaticElements.checkContentOfPagesThatUseMainScala(addAdditionalMetadataPageAsString, userType = "standard")
+      checkFormElements.checkFormContent(descriptiveMetadataType, addAdditionalMetadataPageAsString)
       formTester.checkHtmlForOptionAndItsAttributes(addAdditionalMetadataPageAsString, expectedDefaultForm.toMap)
     }
 
