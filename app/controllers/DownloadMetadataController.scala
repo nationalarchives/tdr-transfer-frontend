7fe4016ab4bbe78c42d41cb3a8dbdfa078dcbf4a
thanhz <thanhz@users.noreply.github.com>
2023-01-09 13:51:34 +0000

    TDR-2767 Correctly format datetime datatypes for download csv (#2512)
    Correctly format datetime datatypes for download csv

--- app/controllers/DownloadMetadataController.scala
+++ app/controllers/DownloadMetadataController.scala
@@ -4,11 +4,14 @@ import auth.TokenSecurity
 import configuration.KeycloakConfiguration
 import controllers.util.CsvUtils
 import graphql.codegen.GetConsignmentFilesMetadata.getConsignmentFilesMetadata.GetConsignment.Files.FileMetadata
-import graphql.codegen.types.FileFilters
+import graphql.codegen.GetCustomMetadata.customMetadata.CustomMetadata
+import graphql.codegen.types.DataType
 import org.pac4j.play.scala.SecurityComponents
 import play.api.mvc.{Action, AnyContent, Request}
 import services.{ConsignmentService, CustomMetadataService}
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 import javax.inject.Inject
 
@@ -35,16 +38,29 @@ class DownloadMetadataController @Inject() (
 
   def downloadMetadataCsv(consignmentId: UUID): Action[AnyContent] = standardTypeAction(consignmentId) { implicit request: Request[AnyContent] =>
     for {
-      metadata <- consignmentService.getConsignmentFileMetadata(consignmentId, request.token.bearerAccessToken, None, None, None)
+      metadata <- consignmentService.getConsignmentFileMetadata(consignmentId, request.token.bearerAccessToken, None, None)
       customMetadata <- customMetadataService.getCustomMetadata(consignmentId, request.token.bearerAccessToken)
     } yield {
-      val filteredMetadata = customMetadata.filter(_.allowExport).sortBy(_.exportOrdinal.getOrElse(Int.MaxValue))
+      val parseFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd[ ]['T']HH:mm:ss[.SSS][.SS][.S]")
+      val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+      val filteredMetadata: List[CustomMetadata] = customMetadata.filter(_.allowExport).sortBy(_.exportOrdinal.getOrElse(Int.MaxValue))
       val header: List[String] = filteredMetadata.map(f => f.fullName.getOrElse(f.name))
-      val rows: List[List[String]] = metadata.files.map(file => {
+      val fileMetadataRows: List[List[String]] = metadata.files.map(file => {
         val groupedMetadata = file.fileMetadata.groupBy(_.name).view.mapValues(_.map(_.value).mkString("|")).toMap
-        filteredMetadata.map(fm => groupedMetadata.getOrElse(fm.name, ""))
+        filteredMetadata.map(customMetadata =>
+          groupedMetadata
+            .get(customMetadata.name)
+            .map(fileMetadataValue => {
+              if (filteredMetadata.find(_.name == customMetadata.name).exists(_.dataType == DataType.DateTime)) {
+                LocalDateTime.parse(fileMetadataValue, parseFormatter).format(formatter)
+              } else {
+                fileMetadataValue
+              }
+            })
+            .getOrElse("")
+        )
       })
-      val csvString = CsvUtils.writeCsv(header :: rows)
+      val csvString = CsvUtils.writeCsv(header :: fileMetadataRows)
       Ok(csvString)
         .as("text/csv")
         .withHeaders("Content-Disposition" -> s"attachment; filename=${metadata.consignmentReference}-metadata.csv")
