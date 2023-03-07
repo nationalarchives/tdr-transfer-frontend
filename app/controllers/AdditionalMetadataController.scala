dd86cba9e924480f390d22436b81c49bc8cb8afd
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2023-03-07 09:25:10 +0000

    Add a line break before the object declaration

--- app/controllers/AdditionalMetadataController.scala
+++ app/controllers/AdditionalMetadataController.scala
@@ -52,6 +52,7 @@ class AdditionalMetadataController @Inject() (
     } getOrElse notEntered
   }
 }
+
 object AdditionalMetadataController {
   case class MetadataProgress(value: String, colour: String)
   case class AdditionalMetadataStartPage(
