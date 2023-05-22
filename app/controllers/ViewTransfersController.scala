2c91d1e054c5e6246fe677d8a4db6f13d9b71647
thanhz <tri.lam21@outlook.com>
2023-05-22 15:44:24 +0100

    refactor + scalafmt

--- app/controllers/ViewTransfersController.scala
+++ app/controllers/ViewTransfersController.scala
@@ -109,11 +109,10 @@ class ViewTransfersController @Inject() (
       if (judgmentType) routes.TransferCompleteController.judgmentTransferComplete(consignmentId).url
       else routes.DownloadMetadataController.downloadMetadataCsv(consignmentId).url
     status.value match {
-      //Even though export is InProgress once a user clicks export there is nothing else they can do, hence setting the status to transferred
-      case s if s == InProgressValue.value => UserAction(Transferred.value, url, actionText)
-      case s if s == CompletedValue.value => UserAction(Transferred.value, url, actionText)
-      case s if s == FailedValue.value => UserAction(Failed.value, s"""mailto:%s?subject=Ref: $consignmentRef - Export failure""", ContactUs.value)
-      case _ => toContactUsAction(consignmentRef)
+      // Even though export is InProgress once a user clicks export there is nothing else they can do, hence setting the status to transferred
+      case s if s == InProgressValue.value || s == CompletedValue.value => UserAction(Transferred.value, url, actionText)
+      case s if s == FailedValue.value                                  => UserAction(Failed.value, s"""mailto:%s?subject=Ref: $consignmentRef - Export failure""", ContactUs.value)
+      case _                                                            => toContactUsAction(consignmentRef)
     }
   }
 
