630ceb7f560c3246ac4b791da5eb45f8e697c8b5
TomJKing <tom.king@nationalarchives.gov.uk>
2023-06-08 09:59:55 +0100

    Return error if backend trigger is non-200 response
    The transfer fails if the backend checks are not triggered. This can occur if a user PLAY_SESSION timeouts after an hour. This throws a 401 error on the request to trigger the backend check
    
    The non-200 errors from the request were no being caught and users were directed to file checks results page, making it appear the the transfer is proceeding successfully, but the file checks page just hangs indefinitely.
    
    This causes the users' confusion
    
    Now if a non-200 response is received this causes an error to be returned and the user is redirected to the error page making it clearer that there has been a problem

--- npm/test/triggerbackendchecks.test.ts
+++ npm/test/triggerbackendchecks.test.ts
@@ -1,9 +1,13 @@
-import fetchMock, { enableFetchMocks } from "jest-fetch-mock"
 import { TriggerBackendChecks } from "../src/triggerbackendchecks"
+
+import fetchMock, { enableFetchMocks } from "jest-fetch-mock"
+import { UpdateConsignmentStatus } from "../src/updateconsignmentstatus"
 
 enableFetchMocks()
 
 const consignmentId = "a201c866-0423-4c90-a024-7d894f290df8"
+
+jest.mock("uuid", () => "eb7b7961-395d-4b4c-afc6-9ebcadaf0150")
 
 beforeEach(() => {
   document.body.innerHTML =
@@ -30,4 +34,25 @@ test("triggerBackendChecks calls the correct url", async () => {
   expect(fetchMock.mock.calls[0][0]).toEqual(
     `/consignment/${consignmentId}/trigger-backend-checks`
   )
+})
+
+test("triggerBackendChecks returns error if the response is not 200", async () => {
+  fetchMock.mockResponse("error", {
+    statusText: "There was an error",
+    status: 500
+  })
+  const triggerBackendChecks = new TriggerBackendChecks()
+  await expect(
+    triggerBackendChecks.triggerBackendChecks(consignmentId)
+  ).resolves.toStrictEqual(
+    Error("Backend checks failed to trigger: There was an error")
+  )
+})
+
+test("triggerBackendChecks returns error if the front end call fails", async () => {
+  fetchMock.mockReject(Error("Error from frontend"))
+  const triggerBackendChecks = new TriggerBackendChecks()
+  await expect(
+    triggerBackendChecks.triggerBackendChecks(consignmentId)
+  ).resolves.toStrictEqual(Error("Error: Error from frontend"))
 })
