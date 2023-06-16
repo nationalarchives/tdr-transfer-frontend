00935f77f68b4ec7ee42c1a29495bb885dc5ae49
thanhz <tri.lam21@outlook.com>
2023-06-16 12:49:23 +0100

    Set series to InProgress

--- app/services/ConsignmentStatusService.scala
+++ app/services/ConsignmentStatusService.scala
@@ -5,6 +5,9 @@ import configuration.GraphQLConfiguration
 import graphql.codegen.GetConsignmentStatus.getConsignmentStatus.GetConsignment.ConsignmentStatuses
 import graphql.codegen.GetConsignmentStatus.getConsignmentStatus.{GetConsignment, Variables}
 import graphql.codegen.GetConsignmentStatus.{getConsignmentStatus => gcs}
+import graphql.codegen.AddConsignmentStatus.addConsignmentStatus.{Variables => acsv}
+import graphql.codegen.AddConsignmentStatus.{addConsignmentStatus => acs}
+import graphql.codegen.types.ConsignmentStatusInput
 import services.ApiErrorHandling._
 import services.Statuses.StatusType
 
@@ -14,6 +17,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class ConsignmentStatusService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(implicit val ec: ExecutionContext) {
   private val getConsignmentStatusClient = graphqlConfiguration.getClient[gcs.Data, gcs.Variables]()
+  private val addConsignmentStatusClient = graphqlConfiguration.getClient[acs.Data, acs.Variables]()
 
   def getStatusValues(statuses: List[ConsignmentStatuses], statusTypes: StatusType*): Map[StatusType, Option[String]] = {
     statusTypes
@@ -32,8 +36,14 @@ class ConsignmentStatusService @Inject() (val graphqlConfiguration: GraphQLConfi
     }
   }
 
+  def addConsignmentStatus(consignmentId: UUID, statusType: String, statusValue: String, token: BearerAccessToken): Future[acs.Data] = {
+    val variables = new acsv(ConsignmentStatusInput(consignmentId, statusType, Some(statusValue)))
+    sendApiRequest(addConsignmentStatusClient, acs.document, token, variables)
+  }
+
   def consignmentStatusSeries(consignmentId: UUID, token: BearerAccessToken): Future[Option[GetConsignment]] = {
     val variables = new Variables(consignmentId)
     sendApiRequest(getConsignmentStatusClient, gcs.document, token, variables).map(data => data.getConsignment)
   }
+
 }
