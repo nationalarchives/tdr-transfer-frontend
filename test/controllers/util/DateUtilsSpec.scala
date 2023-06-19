36f1711e77cc0183627e6916b6bfbb0a6dec242c
thanhz <tri.lam21@outlook.com>
2023-06-19 12:11:44 +0100

    remove comment

--- test/controllers/util/DateUtilsSpec.scala
+++ test/controllers/util/DateUtilsSpec.scala
@@ -12,7 +12,7 @@ class DateUtilsSpec extends AnyWordSpec with MockitoSugar with BeforeAndAfterEac
   "DateUtils" should {
     "format ZonedDateTime to a string with the default format" in {
       val dateTime = ZonedDateTime.of(2023, 6, 19, 15, 30, 0, 0, ZoneId.of("UTC"))
-      val expected = "19/06/2023 16:30:00" // Adjusted for Europe/London timezone
+      val expected = "19/06/2023 16:30:00"
 
       val result = DateUtils.format(dateTime)
 
@@ -22,7 +22,7 @@ class DateUtilsSpec extends AnyWordSpec with MockitoSugar with BeforeAndAfterEac
     "format ZonedDateTime to a string with a custom format" in {
       val dateTime = ZonedDateTime.of(2023, 6, 19, 15, 30, 0, 0, ZoneId.of("UTC"))
       val customFormat = "yyyy-MM-dd HH:mm:ss"
-      val expected = "2023-06-19 16:30:00" // Adjusted for Europe/London timezone
+      val expected = "2023-06-19 16:30:00"
 
       val result = DateUtils.format(dateTime, customFormat)
 
