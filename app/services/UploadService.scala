f9d96c606331c78d71520a1e8b56a1230dbd03b1
thanhz <tri.lam21@outlook.com>
2023-07-17 12:54:08 +0100

    remove unused code

--- app/services/UploadService.scala
+++ app/services/UploadService.scala
@@ -2,10 +2,9 @@ package services
 
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import configuration.GraphQLConfiguration
-import graphql.codegen.StartUpload.{startUpload => su}
 import graphql.codegen.AddFilesAndMetadata.{addFilesAndMetadata => afam}
-import graphql.codegen.UpdateConsignmentStatus.{updateConsignmentStatus => ucs}
-import graphql.codegen.types.{AddFileAndMetadataInput, ConsignmentStatusInput, StartUploadInput}
+import graphql.codegen.StartUpload.{startUpload => su}
+import graphql.codegen.types.{AddFileAndMetadataInput, StartUploadInput}
 import services.ApiErrorHandling.sendApiRequest
 
 import javax.inject.Inject
@@ -14,17 +13,6 @@ import scala.concurrent.{ExecutionContext, Future}
 class UploadService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(implicit val ec: ExecutionContext) {
   private val startUploadClient = graphqlConfiguration.getClient[su.Data, su.Variables]()
   private val addFilesAndMetadataClient = graphqlConfiguration.getClient[afam.Data, afam.Variables]()
-  private val updateConsignmentStatusClient = graphqlConfiguration.getClient[ucs.Data, ucs.Variables]()
-
-//  def updateConsignmentStatus(consignmentStatusInput: ConsignmentStatusInput, token: BearerAccessToken): Future[Int] = {
-//    val variables = ucs.Variables(consignmentStatusInput)
-//    sendApiRequest(updateConsignmentStatusClient, ucs.document, token, variables).map(data => {
-//      data.updateConsignmentStatus match {
-//        case Some(response) => response
-//        case None           => throw new RuntimeException(s"No data returned when updating the consignment status for ${consignmentStatusInput.consignmentId}")
-//      }
-//    })
-//  }
 
   def startUpload(startUploadInput: StartUploadInput, token: BearerAccessToken): Future[String] = {
     val variables = su.Variables(startUploadInput)
