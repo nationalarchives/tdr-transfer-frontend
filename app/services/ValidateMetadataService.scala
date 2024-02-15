77b7c65a49cf361804f10da3f722cd965caa69ee
TomJKing <tom.king@nationalarchives.gov.uk>
2024-02-15 12:24:56 +0000

    Pull request feedback

--- app/services/ValidateMetadataService.scala
+++ app/services/ValidateMetadataService.scala
@@ -1,25 +1,25 @@
 package services
 
-import configuration.GraphQLConfiguration
+import configuration.ApplicationConfig
+import play.api.Logging
 import play.api.libs.ws.WSClient
-import play.api.{Configuration, Logging}
 
 import java.util.UUID
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class ValidateMetadataService @Inject() (val ws: WSClient, val configuration: Configuration, graphQLConfiguration: GraphQLConfiguration)(implicit
+class ValidateMetadataService @Inject() (val ws: WSClient, val applicationConfig: ApplicationConfig)(implicit
     val executionContext: ExecutionContext
 ) extends Logging {
 
   def triggerMetadataValidation(consignmentId: UUID, token: String): Future[Boolean] = {
-    val url = s"${configuration.get[String]("metadatavalidation.baseUrl")}/draft-metadata/validate/$consignmentId"
+    val url = s"${applicationConfig.metadataValidationBaseUrl}/draft-metadata/validate/$consignmentId"
     ws.url(url)
       .addHttpHeaders(("Authorization", token), ("Content-Type", "application/json"))
       .post("{}")
       .flatMap(r =>
         r.status match {
-          case 200 => Future(true)
+          case 200 => Future.successful(true)
           case _ =>
             logger.error(s"Validate draft metadata api response ${r.status} ${r.body}")
             Future.failed(new Exception(s"Call to validate draft metadata API has returned a non 200 response for consignment $consignmentId"))
