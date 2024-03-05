736291ad0e70f89462b293b607721fe922e70188
Thanh <tri.lam21@outlook.com>
2024-03-05 11:09:27 +0000

    Add unit test

--- test/controllers/DraftMetadataChecksResultsControllerSpec.scala
+++ test/controllers/DraftMetadataChecksResultsControllerSpec.scala
@@ -2,6 +2,8 @@ package controllers
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import configuration.{ApplicationConfig, GraphQLConfiguration, KeycloakConfiguration}
+import controllers.DraftMetadataChecksResultsController.DraftMetadataProgress
+import graphql.codegen.GetConsignmentStatus.getConsignmentStatus.{GetConsignment => gcs}
 import org.mockito.Mockito.when
 import org.pac4j.play.scala.SecurityComponents
 import org.scalatest.matchers.should.Matchers._
@@ -10,9 +12,11 @@ import play.api.Play.materializer
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status => playStatus, _}
+import services.Statuses.{CompletedValue, CompletedWithIssuesValue, DraftMetadataType, FailedValue}
 import services.{ConsignmentService, ConsignmentStatusService}
 import testUtils.FrontEndTestHelper
 
+import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import java.util.UUID
 import scala.concurrent.ExecutionContext
 
@@ -35,19 +39,23 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
 
   "DraftMetadataChecksResultsControllerSpec GET" should {
     "render 'draft metadata checks results' page when 'blockDraftMetadataUpload' set to 'false'" in {
-
       val controller = instantiateController(blockDraftMetadataUpload = false)
       val additionalMetadataEntryMethodPage = controller
         .draftMetadataChecksResultsPage(consignmentId)
         .apply(FakeRequest(GET, "/draft-metadata/checks-results").withCSRFToken)
       setConsignmentTypeResponse(wiremockServer, "standard")
       setConsignmentReferenceResponse(wiremockServer)
+      val someDateTime = ZonedDateTime.of(LocalDateTime.of(2022, 3, 10, 1, 0), ZoneId.systemDefault())
+      val consignmentStatuses = List(
+        gcs.ConsignmentStatuses(UUID.randomUUID(), UUID.randomUUID(), DraftMetadataType.id, CompletedValue.value, someDateTime, None)
+      )
+      setConsignmentStatusResponse(app.configuration, wiremockServer, consignmentStatuses = consignmentStatuses)
 
       val pageAsString = contentAsString(additionalMetadataEntryMethodPage)
 
       playStatus(additionalMetadataEntryMethodPage) mustBe OK
       contentType(additionalMetadataEntryMethodPage) mustBe Some("text/html")
-      pageAsString must include("<title>[WIP] Draft Metadata - Transfer Digital Records - GOV.UK</title>")
+      pageAsString must include("<title>Results of CSV Checks - Transfer Digital Records - GOV.UK</title>")
     }
 
     "render page not found error when 'blockDraftMetadataUpload' set to 'true'" in {
@@ -60,6 +68,62 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
       playStatus(additionalMetadataEntryMethodPage) mustBe OK
       contentType(additionalMetadataEntryMethodPage) mustBe Some("text/html")
       pageAsString must include("<title>Page not found - Transfer Digital Records - GOV.UK</title>")
+    }
+  }
+
+  "DraftMetadataChecksResultsController should render the page with the correct status" should {
+    val draftMetadataStatuses = Table(
+      ("status", "progress"),
+      (CompletedValue.value, DraftMetadataProgress("IMPORTED", "blue")),
+      (CompletedWithIssuesValue.value, DraftMetadataProgress("ERRORS", "red")),
+      (FailedValue.value, DraftMetadataProgress("FAILED", "red"))
+    )
+    forAll(draftMetadataStatuses) { (statusValue, progress) =>
+      s"render the draftMetadataResults page when the status is $statusValue" in {
+        val controller = instantiateController(blockDraftMetadataUpload = false)
+        val additionalMetadataEntryMethodPage = controller
+          .draftMetadataChecksResultsPage(consignmentId)
+          .apply(FakeRequest(GET, "/draft-metadata/checks-results").withCSRFToken)
+        setConsignmentTypeResponse(wiremockServer, "standard")
+        setConsignmentReferenceResponse(wiremockServer)
+        val someDateTime = ZonedDateTime.of(LocalDateTime.of(2022, 3, 10, 1, 0), ZoneId.systemDefault())
+        val consignmentStatuses = List(
+          gcs.ConsignmentStatuses(UUID.randomUUID(), UUID.randomUUID(), DraftMetadataType.id, statusValue, someDateTime, None)
+        )
+        setConsignmentStatusResponse(app.configuration, wiremockServer, consignmentStatuses = consignmentStatuses)
+
+        val pageAsString = contentAsString(additionalMetadataEntryMethodPage)
+
+        playStatus(additionalMetadataEntryMethodPage) mustBe OK
+        contentType(additionalMetadataEntryMethodPage) mustBe Some("text/html")
+        pageAsString must include("<title>Results of CSV Checks - Transfer Digital Records - GOV.UK</title>")
+        pageAsString must include(
+          """            <h1 class="govuk-heading-l">
+            |                Results of your metadata checks
+            |            </h1>""".stripMargin
+        )
+        pageAsString must include(
+          s"""          <dl class="govuk-summary-list">
+            |                <div class="govuk-summary-list__row">
+            |                    <dt class="govuk-summary-list__key">
+            |                        Status
+            |                    </dt>
+            |                    <dd class="govuk-summary-list__value">
+            |                        <strong class="govuk-tag govuk-tag--${progress.colour}">
+            |                            ${progress.value}
+            |                        </strong>
+            |                    </dd>
+            |                </div>
+            |            </dl>""".stripMargin
+        )
+        pageAsString must include(
+          s"""            <div class="govuk-button-group">
+             |                <a href="/consignment/$consignmentId/additional-metadata/download-metadata" role="button" draggable="false" class="govuk-button" data-module="govuk-button">
+             |                    Next
+             |                </a>
+             |            </div>""".stripMargin
+        )
+      }
     }
   }
 
