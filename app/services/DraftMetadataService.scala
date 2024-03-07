c90242b00a5a344c173739b7eb9097b7139142ea
TomJKing <tom.king@nationalarchives.gov.uk>
2024-03-07 14:39:18 +0000

    Service to call metadata validation endpoint

---
+++ app/services/DraftMetadataService.scala
@@ -0,0 +1,27 @@
+package services
+
+import com.google.inject.Inject
+import play.api.libs.ws.WSClient
+import play.api.{Configuration, Logging}
+
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+
+class DraftMetadataService @Inject()(val wsClient: WSClient, val configuration: Configuration)(implicit val executionContext: ExecutionContext) extends Logging {
+
+  def triggerDraftMetadataValidator(consignmentId: UUID, token: String): Future[Boolean] = {
+    val url = s"${configuration.get[String]("metadatavalidation.baseUrl")}/draft-metadata/validate/$consignmentId"
+    wsClient
+      .url(url)
+      .addHttpHeaders(("Authorization", token), ("Content-Type", "application/json"))
+      .post("{}")
+      .flatMap(r =>
+        r.status match {
+          case 200 => Future(true)
+          case _ =>
+            logger.error(s"Draft metadata validation api response ${r.status} ${r.body}")
+            Future.failed(new Exception(s"Call to draft metadata validator failed API has returned a non 200 response for consignment $consignmentId"))
+        }
+      )
+  }
+}
