aceecb776649feacd4e5fe5fbb3f30a58e430927
thanhz <thanhz@users.noreply.github.com>
2024-03-07 11:08:18 +0000

    TDRD-6 Results of metadata upload page (#3763)
    Happy path for metadata results check

--- test/controllers/DraftMetadataChecksResultsControllerSpec.scala
+++ test/controllers/DraftMetadataChecksResultsControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import configuration.{ApplicationConfig, GraphQLConfiguration, KeycloakConfiguration}
+import graphql.codegen.GetConsignmentStatus.getConsignmentStatus.{GetConsignment => gcs}
 import org.mockito.Mockito.when
 import org.pac4j.play.scala.SecurityComponents
 import org.scalatest.matchers.should.Matchers._
@@ -10,9 +11,11 @@ import play.api.Play.materializer
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status => playStatus, _}
-import services.ConsignmentService
+import services.Statuses.{CompletedValue, CompletedWithIssuesValue, DraftMetadataType, FailedValue}
+import services.{ConsignmentService, ConsignmentStatusService}
 import testUtils.FrontEndTestHelper
 
+import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import java.util.UUID
 import scala.concurrent.ExecutionContext
 
@@ -33,23 +36,7 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
     wiremockServer.stop()
   }
 
-  "DraftMetadataChecksResultsControllerSpec GET" should {
-    "render 'draft metadata checks results' page when 'blockDraftMetadataUpload' set to 'false'" in {
-
-      val controller = instantiateController(blockDraftMetadataUpload = false)
-      val additionalMetadataEntryMethodPage = controller
-        .draftMetadataChecksResultsPage(consignmentId)
-        .apply(FakeRequest(GET, "/draft-metadata/checks-results").withCSRFToken)
-      setConsignmentTypeResponse(wiremockServer, "standard")
-      setConsignmentReferenceResponse(wiremockServer)
-
-      val pageAsString = contentAsString(additionalMetadataEntryMethodPage)
-
-      playStatus(additionalMetadataEntryMethodPage) mustBe OK
-      contentType(additionalMetadataEntryMethodPage) mustBe Some("text/html")
-      pageAsString must include("<title>[WIP] Draft Metadata - Transfer Digital Records - GOV.UK</title>")
-    }
-
+  "DraftMetadataChecksResultsController GET" should {
     "render page not found error when 'blockDraftMetadataUpload' set to 'true'" in {
       val controller = instantiateController()
       val additionalMetadataEntryMethodPage = controller.draftMetadataChecksResultsPage(consignmentId).apply(FakeRequest(GET, "/draft-metadata/checks-results").withCSRFToken)
@@ -63,6 +50,62 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
     }
   }
 
+  "DraftMetadataChecksResultsController should render the page with the correct status" should {
+    val draftMetadataStatuses = Table(
+      ("status", "progress"),
+      (CompletedValue.value, DraftMetadataProgress("IMPORTED", "blue")),
+      (CompletedWithIssuesValue.value, DraftMetadataProgress("ERRORS", "red")),
+      (FailedValue.value, DraftMetadataProgress("FAILED", "red"))
+    )
+    forAll(draftMetadataStatuses) { (statusValue, progress) =>
+      s"render the draftMetadataResults page when the status is $statusValue" in {
+        val controller = instantiateController(blockDraftMetadataUpload = false)
+        val additionalMetadataEntryMethodPage = controller
+          .draftMetadataChecksResultsPage(consignmentId)
+          .apply(FakeRequest(GET, "/draft-metadata/checks-results").withCSRFToken)
+        setConsignmentTypeResponse(wiremockServer, "standard")
+        setConsignmentReferenceResponse(wiremockServer)
+        val someDateTime = ZonedDateTime.of(LocalDateTime.of(2022, 3, 10, 1, 0), ZoneId.systemDefault())
+        val consignmentStatuses = List(
+          gcs.ConsignmentStatuses(UUID.randomUUID(), UUID.randomUUID(), DraftMetadataType.id, statusValue, someDateTime, None)
+        )
+        setConsignmentStatusResponse(app.configuration, wiremockServer, consignmentStatuses = consignmentStatuses)
+
+        val pageAsString = contentAsString(additionalMetadataEntryMethodPage)
+
+        playStatus(additionalMetadataEntryMethodPage) mustBe OK
+        contentType(additionalMetadataEntryMethodPage) mustBe Some("text/html")
+        pageAsString must include("<title>Results of CSV Checks - Transfer Digital Records - GOV.UK</title>")
+        pageAsString must include(
+          """            <h1 class="govuk-heading-l">
+            |                Results of your metadata checks
+            |            </h1>""".stripMargin
+        )
+        pageAsString must include(
+          s"""          <dl class="govuk-summary-list">
+            |                <div class="govuk-summary-list__row">
+            |                    <dt class="govuk-summary-list__key">
+            |                        Status
+            |                    </dt>
+            |                    <dd class="govuk-summary-list__value">
+            |                        <strong class="govuk-tag govuk-tag--${progress.colour}">
+            |                            ${progress.value}
+            |                        </strong>
+            |                    </dd>
+            |                </div>
+            |            </dl>""".stripMargin
+        )
+        pageAsString must include(
+          s"""            <div class="govuk-button-group">
+             |                <a href="/consignment/$consignmentId/additional-metadata/download-metadata" role="button" draggable="false" class="govuk-button" data-module="govuk-button">
+             |                    Next
+             |                </a>
+             |            </div>""".stripMargin
+        )
+      }
+    }
+  }
+
   private def instantiateController(
       securityComponents: SecurityComponents = getAuthorisedSecurityComponents,
       keycloakConfiguration: KeycloakConfiguration = getValidStandardUserKeycloakConfiguration,
@@ -72,7 +115,8 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
     val applicationConfig: ApplicationConfig = new ApplicationConfig(configuration)
     val graphQLConfiguration = new GraphQLConfiguration(app.configuration)
     val consignmentService = new ConsignmentService(graphQLConfiguration)
+    val consignmentStatusService = new ConsignmentStatusService(graphQLConfiguration)
 
-    new DraftMetadataChecksResultsController(securityComponents, keycloakConfiguration, frontEndInfoConfiguration, consignmentService, applicationConfig)
+    new DraftMetadataChecksResultsController(securityComponents, keycloakConfiguration, consignmentService, applicationConfig, consignmentStatusService)
   }
 }
