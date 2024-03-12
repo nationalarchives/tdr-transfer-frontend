776c54972c7028662bfdf186742587fff06c6a19
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-03-12 10:03:16 +0000

    tidy up

--- app/controllers/DraftMetadataUploadController.scala
+++ app/controllers/DraftMetadataUploadController.scala
@@ -2,18 +2,16 @@ package controllers
 
 import auth.TokenSecurity
 import cats.effect.IO
-import cats.effect.IO.{fromOption, fromTry}
+import cats.effect.IO.fromOption
 import cats.effect.unsafe.implicits.global
 import configuration.{ApplicationConfig, KeycloakConfiguration}
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
-import play.api.libs.Files
 import play.api.libs.Files.TemporaryFile
 import play.api.mvc.{Action, AnyContent, MultipartFormData, Request, Result}
 import services._
 import viewsapi.Caching.preventCaching
 
-import java.io.{BufferedReader, FileReader, FileWriter}
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
@@ -47,20 +45,19 @@ class DraftMetadataUploadController @Inject() (
   def saveDraftMetadata(consignmentId: java.util.UUID): Action[MultipartFormData[TemporaryFile]] = secureAction.async(parse.multipartFormData) {
 
     implicit request: Request[MultipartFormData[TemporaryFile]] =>
-
       val successPage = routes.DraftMetadataChecksController.draftMetadataChecksPage(consignmentId)
 
       def uploadMetaData: IO[Result] = for {
-        firstFilePart <- fromOption(request.body.files.headOption)(new RuntimeException("No meta data file provided B "))
-        file <- fromOption(request.body.file(firstFilePart.key))(new RuntimeException("No meta data file provided C"))
-        draftMetadata <- fromOption(Using(scala.io.Source.fromFile(file.ref.getAbsoluteFile))(_.mkString).toOption)(new RuntimeException("No meta data file provided E"))
+        firstFilePart <- fromOption(request.body.files.headOption)(new RuntimeException("No meta data file provided "))
+        file <- fromOption(request.body.file(firstFilePart.key))(new RuntimeException("No meta data file provided"))
+        draftMetadata <- fromOption(Using(scala.io.Source.fromFile(file.ref.getAbsoluteFile))(_.mkString).toOption)(new RuntimeException("No meta data file provided"))
         _ <- uploadService.uploadDraftMetadata("twickenham-ian", "test.csv", draftMetadata)
         successPage <- IO(play.api.mvc.Results.Redirect(successPage))
       } yield successPage
 
       uploadMetaData
-        .recoverWith { case t =>
-          IO(InternalServerError("A server error occurred: " + t.getMessage))
+        .recoverWith { case error =>
+          IO(InternalServerError("Unable to upload draft metadata: " + error.getMessage))
         }
         .unsafeToFuture()
 
--- app/services/UploadService.scala
+++ app/services/UploadService.scala
@@ -1,7 +1,7 @@
 package services
 
 import cats.effect.IO
-import cats.effect.IO.raiseError
+
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import configuration.GraphQLConfiguration
 import graphql.codegen.AddFilesAndMetadata.{addFilesAndMetadata => afam}
@@ -9,12 +9,11 @@ import graphql.codegen.StartUpload.{startUpload => su}
 import graphql.codegen.types.{AddFileAndMetadataInput, StartUploadInput}
 import services.ApiErrorHandling.sendApiRequest
 import software.amazon.awssdk.core.internal.async.ByteBuffersAsyncRequestBody
-import software.amazon.awssdk.transfer.s3.model.CompletedUpload
 import uk.gov.nationalarchives.DAS3Client
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+
 
 class UploadService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(implicit val ec: ExecutionContext) {
   private val startUploadClient = graphqlConfiguration.getClient[su.Data, su.Variables]()
@@ -30,10 +29,10 @@ class UploadService @Inject() (val graphqlConfiguration: GraphQLConfiguration)(i
     sendApiRequest(addFilesAndMetadataClient, afam.document, token, variables).map(data => data.addFilesAndMetadata)
   }
 
-   def uploadDraftMetadata(bucket:String, key:String, draftMetadata: String): IO[Any] = {
-        val s3: DAS3Client[IO] = DAS3Client[IO]()
-        val bytes = draftMetadata.getBytes
-        val publisher = ByteBuffersAsyncRequestBody.from("application/octet-stream", bytes)
-        s3.upload(bucket, key, bytes.size, publisher)
-   }
+  def uploadDraftMetadata(bucket: String, key: String, draftMetadata: String): IO[Any] = {
+    val s3: DAS3Client[IO] = DAS3Client[IO]()
+    val bytes = draftMetadata.getBytes
+    val publisher = ByteBuffersAsyncRequestBody.from("application/octet-stream", bytes)
+    s3.upload(bucket, key, bytes.size, publisher)
+  }
 }
