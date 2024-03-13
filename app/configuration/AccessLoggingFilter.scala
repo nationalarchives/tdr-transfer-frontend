443973af4f8017e8f5373fa0bc439a434e28222e
Tom Hallett <tehallett+github@gmail.com>
2024-03-13 10:55:58 +0000

    Initial fix using OidcProfile

--- app/configuration/AccessLoggingFilter.scala
+++ app/configuration/AccessLoggingFilter.scala
@@ -1,9 +1,9 @@
 package configuration
 
 import auth.OidcSecurity
-import com.nimbusds.oauth2.sdk.token.BearerAccessToken
 import org.apache.pekko.stream.Materializer
 import org.pac4j.core.profile.ProfileManager
+import org.pac4j.oidc.profile.OidcProfile
 import org.pac4j.play.PlayWebContext
 import org.pac4j.play.context.PlayFrameworkParameters
 import org.pac4j.play.scala.SecurityComponents
@@ -11,7 +11,6 @@ import play.api.Logging
 import play.api.mvc.{Filter, RequestHeader, Result}
 
 import javax.inject.Inject
-import scala.compat.java8.OptionConverters._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.OptionConverters.RichOptional
 
@@ -35,8 +34,9 @@ class AccessLoggingFilter @Inject() (implicit
         val sessionStore = config.getSessionStoreFactory.newSessionStore(parameters)
         val webContext = new PlayWebContext(request)
         val profileManager = new ProfileManager(webContext, sessionStore)
+
         val userId: String = profileManager.getProfile.toScala
-          .map(_.getAttribute("access_token").asInstanceOf[String])
+          .map(_.asInstanceOf[OidcProfile].getAccessToken.toString)
           .flatMap(token => keycloakConfiguration.token(token))
           .map(_.userId.toString)
           .getOrElse("user-logged-out")
