37966b270b5407f98e16c01b7e55d5c01dddf062
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-04-04 16:31:14 +0100

    removed unused import

--- test/controllers/UploadControllerSpec.scala
+++ test/controllers/UploadControllerSpec.scala
@@ -12,13 +12,12 @@ import graphql.codegen.types.{AddFileAndMetadataInput, AddFileStatusInput, AddMu
 import io.circe.syntax._
 import io.circe.parser.decode
 import io.circe.generic.auto._
-import org.mockito.Mockito.when
 import play.api.Play.materializer
 import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{GET, redirectLocation, status, _}
 import testUtils.{CheckPageForStaticElements, FrontEndTestHelper}
-import services.{BackendChecksService, ConsignmentService, FileStatusService, Statuses, UploadService}
+import services.{BackendChecksService, ConsignmentService, FileStatusService, UploadService}
 import play.api.libs.json._
 
 import java.util.UUID
@@ -28,7 +27,7 @@ import org.scalatest.concurrent.ScalaFutures._
 import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.Configuration
 import play.api.test.WsTestClient.InternalWSClient
-import services.Statuses.{CompletedWithIssuesValue, StatusValue}
+import services.Statuses.{CompletedValue, CompletedWithIssuesValue, InProgressValue, StatusValue}
 
 import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import scala.jdk.CollectionConverters._
@@ -360,7 +359,7 @@ class UploadControllerSpec extends FrontEndTestHelper {
     }
 
     "render the 'upload in progress error' page if a judgment file upload has a 'completedWithIssues' status" in {
-      val uploadStatus = Statuses.CompletedWithIssuesValue
+      val uploadStatus = CompletedWithIssuesValue
       val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
 
       val uploadPage = executeJudgmentsUpload(uploadStatus, consignmentId, applicationConfig)
@@ -443,7 +442,7 @@ class UploadControllerSpec extends FrontEndTestHelper {
     }
 
     "render the 'upload in progress' page if a judgment file upload is in progress" in {
-      val uploadStatus = Statuses.InProgressValue
+      val uploadStatus = InProgressValue
       val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
 
       val uploadPage = executeJudgmentsUpload(uploadStatus, consignmentId, applicationConfig)
@@ -456,7 +455,7 @@ class UploadControllerSpec extends FrontEndTestHelper {
     }
 
     "render the judgment 'upload is complete' page if the upload has completed" in {
-      val uploadStatus = Statuses.CompletedValue
+      val uploadStatus = CompletedValue
       val consignmentId = UUID.fromString("c2efd3e6-6664-4582-8c28-dcf891f60e68")
 
       val uploadPage = executeJudgmentsUpload(uploadStatus, consignmentId, applicationConfig)
