843ec6dbb538e375234bfc73362a71190a99a969
Tom Hallett <tehallett+github@gmail.com>
2024-06-14 17:33:52 +0100

    Updated after PR review

--- app/controllers/util/ExcelUtils.scala
+++ app/controllers/util/ExcelUtils.scala
@@ -4,27 +4,24 @@ import org.dhatim.fastexcel.{Workbook, Worksheet}
 import org.apache.commons.io.output.ByteArrayOutputStream
 import graphql.codegen.types.DataType
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.UUID
 
 object ExcelUtils {
 
-  def writeExcel(consignmentID: UUID, rows: List[List[String]], dataTypes: List[DataType]): Array[Byte] = {
+  def writeExcel(consignmentRef: String, rows: List[List[Any]]): Array[Byte] = {
     val xlBas = new ByteArrayOutputStream()
-    val wb = new Workbook(xlBas, "MetadataDownload", "1.0")
-    val ws: Worksheet = wb.newWorksheet(s"Metadata for ${consignmentID.toString}")
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val wb = new Workbook(xlBas, "TNA - Transfer Digital Records", "1.0")
+    val ws: Worksheet = wb.newWorksheet(s"Metadata for ${consignmentRef}")
 
     rows.head.zipWithIndex.foreach { case (header, col) =>
-      ws.value(0, col, header)
+      ws.value(0, col, header.toString)
     }
 
     for ((row, rowNo) <- rows.tail.zipWithIndex) {
       for ((col, colNo) <- row.zipWithIndex) {
         col match {
-          case a if !a.equals("") && dataTypes(colNo) == DataType.DateTime => ws.value(rowNo + 1, colNo, LocalDate.parse(col, formatter))
-          case a if !a.equals("") && dataTypes(colNo) == DataType.Integer  => ws.value(rowNo + 1, colNo, Integer.valueOf(col))
-          case _                                                           => ws.value(rowNo + 1, colNo, col)
+          case value: LocalDate => ws.value(rowNo + 1, colNo, value)
+          case value: Integer => ws.value(rowNo + 1, colNo, value)
+          case value => ws.value(rowNo + 1, colNo, value.toString)
         }
       }
     }
