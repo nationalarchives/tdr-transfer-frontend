4de8d0543274790b037b9bdafd79e5ccca77b162
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-07-02 11:31:34 +0100

    * New `dta view transfers` page * New `tnaUserAction`

---
+++ app/controllers/MetadataReviewController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import auth.TokenSecurity
+import configuration.{ApplicationConfig, KeycloakConfiguration}
+import org.pac4j.play.scala.SecurityComponents
+import play.api.mvc.{Action, AnyContent, Request}
+import services.ConsignmentService
+
+import javax.inject.Inject
+
+class MetadataReviewController @Inject()(
+    val keycloakConfiguration: KeycloakConfiguration,
+    val controllerComponents: SecurityComponents,
+    val consignmentService: ConsignmentService,
+    val applicationConfig: ApplicationConfig
+) extends TokenSecurity {
+
+  def metadataReviews(): Action[AnyContent] = tnaUserAction { implicit request: Request[AnyContent] =>
+    for {
+      consignments <- consignmentService.getConsignmentsForReview(request.token.bearerAccessToken)
+    } yield {
+      Ok(views.html.tna.dtaReviews(consignments))
+    }
+  }
+}
