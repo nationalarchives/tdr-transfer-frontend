83925d67f2b0eb0e9b7e3bba771e59bcc02c7e1c
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2024-07-09 01:39:17 +0100

    Continue transfer on the file check page for judgment without waiting for the backend checks to be completed

--- app/controllers/FileChecksController.scala
+++ app/controllers/FileChecksController.scala
@@ -7,16 +7,14 @@ import graphql.codegen.types.ConsignmentStatusInput
 import io.circe.syntax._
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, Request, RequestHeader}
-import services.Statuses.{CompletedValue, CompletedWithIssuesValue, UploadType}
-import services.{BackendChecksService, ConsignmentService, ConsignmentStatusService}
+import play.api.mvc._
+import services.Statuses._
+import services._
 import viewsapi.Caching.preventCaching
 
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.util.control.Breaks.{break, breakable}
+import scala.concurrent.{ExecutionContext, Future, blocking}
 
 @Singleton
 class FileChecksController @Inject() (
@@ -26,14 +24,16 @@ class FileChecksController @Inject() (
     val consignmentService: ConsignmentService,
     val applicationConfig: ApplicationConfig,
     val backendChecksService: BackendChecksService,
-    val consignmentStatusService: ConsignmentStatusService
+    val consignmentStatusService: ConsignmentStatusService,
+    val confirmTransferService: ConfirmTransferService,
+    val consignmentExportService: ConsignmentExportService
 )(implicit val ec: ExecutionContext)
     extends TokenSecurity
     with I18nSupport {
 
   private def getFileChecksProgress(request: Request[AnyContent], consignmentId: UUID)(implicit requestHeader: RequestHeader): Future[FileChecksProgress] = {
     consignmentService
-      .getConsignmentFileChecks(consignmentId, request.token.bearerAccessToken)
+      .fileCheckProgress(consignmentId, request.token.bearerAccessToken)
       .map { fileCheckProgress =>
         FileChecksProgress(
           fileCheckProgress.totalFiles,
@@ -95,6 +95,24 @@ class FileChecksController @Inject() (
     } yield result
   }
 
+  def judgmentCompleteTransfer(consignmentId: UUID): Action[AnyContent] = judgmentTypeAction(consignmentId) { implicit request: Request[AnyContent] =>
+    val token = request.token.bearerAccessToken
+    for {
+      reference <- consignmentService.getConsignmentRef(consignmentId, token)
+      result <-
+        (
+          for {
+            _ <- waitForFileChecksToBeCompleted(consignmentId)
+            result <- JudgmentCompleteTransfer(consignmentId)
+          } yield result
+        ).recover { case _: Exception =>
+          Ok(views.html.uploadInProgress(consignmentId, reference, "Uploading your records", request.token.name, isJudgmentUser = true)).uncache()
+        }
+    } yield {
+      result
+    }
+  }
+
   private def handleSuccessfulUpload(consignmentId: UUID, reference: String, isJudgmentUser: Boolean)(implicit request: Request[AnyContent]) = {
     val token = request.token.bearerAccessToken
     (for {
@@ -121,13 +139,7 @@ class FileChecksController @Inject() (
         ).uncache()
       } else {
         if (isJudgmentUser) {
-          if (applicationConfig.blockAutomateJudgmentTransfers) {
-            Ok(views.html.judgment.judgmentFileChecksProgress(consignmentId, reference, applicationConfig.frontEndInfo, request.token.name)).uncache()
-          } else {
-            val totalSleepTime = 3 * 60 * 1000 // Total sleep time in milliseconds (5 minutes)
-            Await.result(waitForFileChecksToBeCompleted(consignmentId, totalSleepTime), totalSleepTime.millis + 1.minute) // Adding a little buffer time
-            Redirect(routes.FileChecksResultsController.judgmentFileCheckResultsPage(consignmentId)).uncache()
-          }
+          Ok(views.html.judgment.judgmentFileChecksProgress(consignmentId, reference, applicationConfig.frontEndInfo, request.token.name)).uncache()
         } else {
           Ok(views.html.standard.fileChecksProgress(consignmentId, reference, applicationConfig.frontEndInfo, request.token.name))
             .uncache()
@@ -138,27 +150,60 @@ class FileChecksController @Inject() (
     }
   }
 
-  private def waitForFileChecksToBeCompleted(consignmentId: UUID, totalSleepTime: Int)(implicit request: Request[AnyContent]): Future[Unit] = {
+  private def JudgmentCompleteTransfer(consignmentId: UUID)(implicit request: Request[AnyContent]): Future[Result] = {
+    val pageTitle = "Results of checks"
+    for {
+      consignmentStatuses <- consignmentStatusService.getConsignmentStatuses(consignmentId, request.token.bearerAccessToken)
+      reference <- consignmentService.getConsignmentRef(consignmentId, request.token.bearerAccessToken)
+      exportStatus = consignmentStatusService.getStatusValues(consignmentStatuses, ExportType).values.headOption.flatten
+      result <- exportStatus match {
+        case Some(InProgressValue.value) | Some(CompletedValue.value) | Some(FailedValue.value) =>
+          Future(Ok(views.html.transferAlreadyCompleted(consignmentId, reference, request.token.name, isJudgmentUser = true)).uncache())
+        case None =>
+          for {
+            fileCheck <- consignmentService.fileCheckProgress(consignmentId, request.token.bearerAccessToken)
+            result <-
+              if (fileCheck.allChecksSucceeded) {
+                val token: BearerAccessToken = request.token.bearerAccessToken
+                val legalCustodyTransferConfirmation = FinalTransferConfirmationData(transferLegalCustody = true)
+                for {
+                  _ <- confirmTransferService.addFinalTransferConfirmation(consignmentId, token, legalCustodyTransferConfirmation)
+                  _ <- consignmentExportService.updateTransferInitiated(consignmentId, request.token.bearerAccessToken)
+                  _ <- consignmentExportService.triggerExport(consignmentId, request.token.bearerAccessToken.toString)
+                  res <- Future(Redirect(routes.TransferCompleteController.judgmentTransferComplete(consignmentId)).uncache())
+                } yield res
+              } else {
+                Future(Ok(views.html.fileChecksResultsFailed(request.token.name, pageTitle, reference, isJudgmentUser = true)).uncache())
+              }
+          } yield result
+        case _ =>
+          throw new IllegalStateException(s"Unexpected Export status: $exportStatus for consignment $consignmentId")
+      }
+    } yield result
+  }
+
+  private def waitForFileChecksToBeCompleted(consignmentId: UUID)(implicit request: Request[AnyContent]): Future[Unit] = {
+    val totalSleepTime = applicationConfig.fileChecksTotalTimoutInSeconds * 1000 // Total sleep time in milliseconds
     val interval = 5 * 1000 // Interval time in milliseconds (5 seconds)
     val intervals = totalSleepTime / interval
 
-    def fileCheckProgress: Future[Boolean] = consignmentService
-      .fileCheckProgress(consignmentId, request.token.bearerAccessToken)
-      .map(consignment =>
-        consignment.fileChecks.checksumProgress.filesProcessed == consignment.totalFiles &&
-          consignment.fileChecks.ffidProgress.filesProcessed == consignment.totalFiles &&
-          consignment.fileChecks.antivirusProgress.filesProcessed == consignment.totalFiles
-      )
-    breakable {
-      for (_ <- 1 to intervals) {
-        if (Await.result(fileCheckProgress, 10.seconds)) {
-          break()
-        } else {
-          Thread.sleep(interval)
+    def checkStatus(remainingIntervals: Int): Future[Unit] = {
+      if (remainingIntervals <= 0) {
+        Future.unit
+      } else {
+        getFileChecksProgress(request, consignmentId).flatMap { progress =>
+          if (progress.isComplete) {
+            Future.unit
+          } else {
+            Future {
+              blocking(Thread.sleep(interval))
+            }.flatMap(_ => checkStatus(remainingIntervals - 1))
+          }
         }
       }
     }
-    Future.unit
+
+    checkStatus(intervals)
   }
 }
 
