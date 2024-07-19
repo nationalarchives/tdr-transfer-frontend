8b20b2b5dd682219d39a09f8414b0dcc0c8b9a99
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-07-19 16:37:30 +0100

    revert KeycloakConfiguration

--- app/configuration/KeycloakConfiguration.scala
+++ app/configuration/KeycloakConfiguration.scala
@@ -1,24 +1,15 @@
 package configuration
 
-import configuration.GraphQLBackend.backend
+import javax.inject.Inject
 import play.api.Configuration
-import uk.gov.nationalarchives.tdr.GraphQlResponse
 import uk.gov.nationalarchives.tdr.keycloak.{KeycloakUtils, TdrKeycloakDeployment, Token}
 
-import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 class KeycloakConfiguration @Inject() (configuration: Configuration)(implicit val executionContext: ExecutionContext) {
-
   def token(value: String): Option[Token] = {
     implicit val tdrKeycloakDeployment: TdrKeycloakDeployment =
       TdrKeycloakDeployment(s"${configuration.get[String]("auth.url")}", "tdr", 3600)
     KeycloakUtils().token(value).toOption
-  }
-
-  def userDetails(userId: String) = {
-    implicit val tdrKeycloakDeployment: TdrKeycloakDeployment =
-      TdrKeycloakDeployment(s"${configuration.get[String]("auth.url")}", "tdr", 3600)
-    KeycloakUtils().userDetails(userId, "tdr", s"${configuration.get[String]("auth.secret")}")
   }
 }
