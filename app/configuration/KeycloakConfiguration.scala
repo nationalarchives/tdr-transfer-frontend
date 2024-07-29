14f22d5ce9ad99ad9d967726e7690b0b8853422d
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-07-29 16:08:54 +0100

    * Read `READ_AUTH_SECRET` environment variable

--- app/configuration/KeycloakConfiguration.scala
+++ app/configuration/KeycloakConfiguration.scala
@@ -10,7 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class KeycloakConfiguration @Inject() (configuration: Configuration)(implicit val executionContext: ExecutionContext) {
   implicit val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
   val authUrl: String = configuration.get[String]("auth.url")
-  val secret: String = configuration.get[String]("auth.secret")
+  val secret: String = configuration.get[String]("read.auth.secret")
 
   def token(value: String): Option[Token] = {
     implicit val tdrKeycloakDeployment: TdrKeycloakDeployment =
@@ -21,6 +21,6 @@ class KeycloakConfiguration @Inject() (configuration: Configuration)(implicit va
   def userDetails(userId: String): Future[KeycloakUtils.UserDetails] = {
     implicit val tdrKeycloakDeployment: TdrKeycloakDeployment =
       TdrKeycloakDeployment(authUrl, "tdr", 3600)
-    KeycloakUtils().userDetails(userId, "tdr", secret)
+    KeycloakUtils().userDetails(userId, "tdr-user-read", secret)
   }
 }
