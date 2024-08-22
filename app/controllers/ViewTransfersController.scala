95538a6ab3ea1b97d24616efa3989eb673cc4026
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-08-22 07:22:32 +0100

    Put functionality behind FAB

--- app/controllers/ViewTransfersController.scala
+++ app/controllers/ViewTransfersController.scala
@@ -88,7 +88,7 @@ class ViewTransfersController @Inject() (
       case s if s.containsStatuses(ExportType) => toExportAction(s.find(_.statusType == ExportType.id).get, judgmentType, consignmentId, consignmentRef)
       case s if s.statusValue(ConfirmTransferType).contains(CompletedValue.value) =>
         UserAction(InProgress.value, routes.TransferCompleteController.transferComplete(consignmentId).url, Resume.value)
-      case s if s.containsStatuses(MetadataReviewType) =>
+      case s if s.containsStatuses(MetadataReviewType) && !applicationConfig.blockMetadataReview =>
         UserAction(InProgress.value, routes.MetadataReviewStatusController.metadataReviewStatusPage(consignmentId).url, Resume.value)
       case s if additionalMetadataEntered(s) =>
         UserAction(InProgress.value, routes.AdditionalMetadataController.start(consignmentId).url, Resume.value)
