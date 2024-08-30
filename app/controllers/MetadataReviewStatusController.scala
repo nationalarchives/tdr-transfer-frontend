d757c99d6b6cafd98cc1a98d756dfa4a73150f82
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-08-30 15:48:05 +0100

    Merge continued

--- app/controllers/MetadataReviewStatusController.scala
+++ app/controllers/MetadataReviewStatusController.scala
@@ -28,42 +28,9 @@ class MetadataReviewStatusController @Inject() (
         consignmentStatus <- consignmentStatusService.consignmentStatusSeries(consignmentId, request.token.bearerAccessToken)
         reviewStatus = consignmentStatus.flatMap(s => consignmentStatusService.getStatusValues(s.consignmentStatuses, MetadataReviewType).values.headOption.flatten)
         reference <- consignmentService.getConsignmentRef(consignmentId, request.token.bearerAccessToken)
-<<<<<<< HEAD
-        result <- reviewStatus match {
-          case Some(InProgressValue.value) | Some(CompletedValue.value) | Some(CompletedWithIssuesValue.value) =>
-            Future(
-              Ok(views.html.standard.metadataReviewStatus(consignmentId, reference, request.token.name, request.token.email, reviewStatus.get))
-            )
-          case None =>
-            Future(Ok(views.html.notFoundError(name = request.token.name, isLoggedIn = true, isJudgmentUser = false)))
-          case _ =>
-            throw new IllegalStateException(s"Unexpected review status: $reviewStatus for consignment $consignmentId")
-        }
-      } yield result
-    }
-  }
-
-  def metadataReviewActionRequired(consignmentId: UUID): Action[AnyContent] = standardUserAndTypeAction(consignmentId) { implicit request: Request[AnyContent] =>
-    for {
-      _ <- consignmentStatusService.updateConsignmentStatus(
-        ConsignmentStatusInput(consignmentId, DescriptiveMetadataType.id, Some(InProgressValue.value)),
-        request.token.bearerAccessToken
-      )
-      _ <- consignmentStatusService.updateConsignmentStatus(
-        ConsignmentStatusInput(consignmentId, ClosureMetadataType.id, Some(InProgressValue.value)),
-        request.token.bearerAccessToken
-      )
-      _ <- consignmentStatusService.updateConsignmentStatus(
-        ConsignmentStatusInput(consignmentId, DraftMetadataType.id, Some(InProgressValue.value)),
-        request.token.bearerAccessToken
-      )
-    } yield {
-      Redirect(routes.AdditionalMetadataController.start(consignmentId))
-=======
       } yield reviewStatus
         .map(status => Ok(views.html.standard.metadataReviewStatus(consignmentId, reference, request.token.name, request.token.email, status)))
         .getOrElse(Ok(views.html.notFoundError(name = request.token.name, isLoggedIn = true, isJudgmentUser = false)))
->>>>>>> master
     }
   }
 }
