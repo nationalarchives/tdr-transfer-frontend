2a4e61b2ffe803739e289a2cb5849bdc8c357e5c
thanhz <thanhz@users.noreply.github.com>
2025-04-02 15:15:34 +0100

    Merge pull request #4530 from nationalarchives/TRDRD-350/717-results-of-your-metadata-checks-content-changes

--- test/controllers/DraftMetadataChecksResultsControllerSpec.scala
+++ test/controllers/DraftMetadataChecksResultsControllerSpec.scala
@@ -97,6 +97,9 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
              |            </h1>""".stripMargin
       )
       pageAsString must include(
+        s"""<p class="govuk-body">The metadata in your uploaded <abbr title="Comma Separated Values">CSV</abbr> has been successfully imported.</p>"""
+      )
+      pageAsString must include(
         s"""          <dl class="govuk-summary-list">
             |                <div class="govuk-summary-list__row">
             |                    <dt class="govuk-summary-list__key">
@@ -109,6 +112,10 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
             |                    </dd>
             |                </div>
             |            </dl>""".stripMargin
+      )
+      pageAsString must include(
+        s"""<p class="govuk-body">If you need to make any changes to your metadata you can return to <a class="govuk-link" href=/consignment/$consignmentId/draft-metadata/upload>
+          |                upload a metadata CSV</a>, otherwise continue with your transfer.</p>""".stripMargin
       )
       pageAsString must include(
         s"""            <div class="govuk-button-group">
@@ -158,7 +165,22 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
           )
           pageAsString must include(detailsMessage)
           pageAsString must include(actionMessage)
-          pageAsString must include("""<p class="govuk-body">The report below contains details about issues found.</p>""")
+          pageAsString must include(
+            """<p class="govuk-body">The report below contains detailed errors with reference to the file path and column title, which caused the error.</p>"""
+          )
+          pageAsString must include(
+            """<div class="da-alert da-alert--default">
+              |    <div class="da-alert__content">
+              |        <h2 class="da-alert__heading da-alert__heading--s">
+              |            Leaving and returning to this transfer
+              |        </h2>
+              |        <p>
+              |            You can sign out and return to continue working on this transfer at any time from <a class='govuk-link' href='/view-transfers'>View transfers</a>.
+              |        </p>
+              |    </div>
+              |</div>
+              |""".stripMargin
+          )
           pageAsString must include(
             s"""<a class="govuk-button govuk-button--secondary govuk-!-margin-bottom-8 download-metadata" href="/consignment/$consignmentId/draft-metadata/download-report">
                |    <span aria-hidden="true" class="tna-button-icon">
@@ -236,6 +258,19 @@ class DraftMetadataChecksResultsControllerSpec extends FrontEndTestHelper {
           pageAsString must include(detailsMessage)
           pageAsString must include(actionMessage)
           affectedProperties.foreach(p => pageAsString must include(s"<li>$p</li>"))
+          pageAsString must include(
+            """<div class="da-alert da-alert--default">
+              |    <div class="da-alert__content">
+              |        <h2 class="da-alert__heading da-alert__heading--s">
+              |            Leaving and returning to this transfer
+              |        </h2>
+              |        <p>
+              |            You can sign out and return to continue working on this transfer at any time from <a class='govuk-link' href='/view-transfers'>View transfers</a>.
+              |        </p>
+              |    </div>
+              |</div>
+              |""".stripMargin
+          )
         }
       }
     }
