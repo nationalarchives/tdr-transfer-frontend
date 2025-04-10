39a97d03482b6a3c3f451f2c7cc4aed96d657db4
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2025-04-10 15:47:16 +0100

    Add/update `draftmetadata` status on submit

--- app/controllers/PrepareMetadataController.scala
+++ app/controllers/PrepareMetadataController.scala
@@ -2,9 +2,11 @@ package controllers
 
 import auth.TokenSecurity
 import configuration.KeycloakConfiguration
+import graphql.codegen.types.ConsignmentStatusInput
 import org.pac4j.play.scala.SecurityComponents
 import play.api.mvc.{Action, AnyContent, Request}
-import services.ConsignmentService
+import services.Statuses.{DraftMetadataType, InProgressValue}
+import services.{ConsignmentService, ConsignmentStatusService}
 
 import java.util.UUID
 import javax.inject.Inject
@@ -12,12 +14,27 @@ import javax.inject.Inject
 class PrepareMetadataController @Inject() (
     val keycloakConfiguration: KeycloakConfiguration,
     val controllerComponents: SecurityComponents,
-    val consignmentService: ConsignmentService
+    val consignmentService: ConsignmentService,
+    val consignmentStatusService: ConsignmentStatusService
 ) extends TokenSecurity {
 
   def prepareMetadata(consignmentId: UUID): Action[AnyContent] = standardUserAndTypeAction(consignmentId) { implicit request: Request[AnyContent] =>
     for {
       consignmentRef <- consignmentService.getConsignmentRef(consignmentId, request.token.bearerAccessToken)
     } yield Ok(views.html.draftmetadata.prepareMetadata(consignmentId, consignmentRef))
+  }
+
+  def prepareMetadataSubmit(consignmentId: UUID): Action[AnyContent] = standardUserAndTypeAction(consignmentId) { implicit request: Request[AnyContent] =>
+    val token = request.token.bearerAccessToken
+    for {
+      consignmentStatuses <- consignmentStatusService.getConsignmentStatuses(consignmentId, token)
+      draftMetadataStatus = consignmentStatusService.getStatusValues(consignmentStatuses, DraftMetadataType).values.headOption.flatten
+      _ <-
+        if (draftMetadataStatus.isEmpty) {
+          consignmentStatusService.addConsignmentStatus(consignmentId, DraftMetadataType.id, InProgressValue.value, token)
+        } else {
+          consignmentStatusService.updateConsignmentStatus(ConsignmentStatusInput(consignmentId, DraftMetadataType.id, Some(InProgressValue.value)), token)
+        }
+    } yield Redirect(routes.DraftMetadataUploadController.draftMetadataUploadPage(consignmentId))
   }
 }
