0461823e5597088010d1805cfc4b4dd6d4e8da7f
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-07-17 16:06:00 +0100

    new metadataquickgude page

--- app/controllers/HelpController.scala
+++ app/controllers/HelpController.scala
@@ -1,10 +1,15 @@
 package controllers
 
 import auth.UnprotectedPageController
+
 import javax.inject.{Inject, Singleton}
 import org.pac4j.play.scala.SecurityComponents
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, Request}
+import uk.gov.nationalarchives.tdr.schemautils.ConfigUtils
+
+import scala.io.Source
+import scala.util.Using
 
 @Singleton
 class HelpController @Inject() (securityComponents: SecurityComponents) extends UnprotectedPageController(securityComponents) with I18nSupport {
@@ -17,6 +22,12 @@ class HelpController @Inject() (securityComponents: SecurityComponents) extends 
   }
 
   def metadataQuickGuide(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.metadataquickguide(request.isLoggedIn, request.name, request.isJudgmentUser))
+    val METADATA_GUIDANCE_LOCATION = "/guidance/metadata-template.json"
+    val nodeSchema = getClass.getResourceAsStream(METADATA_GUIDANCE_LOCATION)
+    val source = Source.fromInputStream(nodeSchema)
+    val guideContent = Using(source)(_.mkString).get
+    val propertyNameMapper = ConfigUtils.loadConfiguration.propertyToOutputMapper("tdrFileHeader")
+    Ok(views.html.metadataquickguide(request.isLoggedIn, request.name, request.isJudgmentUser, guideContent, propertyNameMapper))
   }
 }
+
