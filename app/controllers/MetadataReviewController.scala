a88b75884c94749d9888c3ed1226ab879ebbfc0c
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2025-10-06 15:27:01 +0100

    TDRD-359 - Show submitted date on the metadata review page and sort it by submitted date

--- app/controllers/MetadataReviewController.scala
+++ app/controllers/MetadataReviewController.scala
@@ -2,10 +2,15 @@ package controllers
 
 import auth.TokenSecurity
 import configuration.KeycloakConfiguration
+import graphql.codegen.GetConsignmentsForMetadataReview.getConsignmentsForMetadataReview.GetConsignmentsForMetadataReview
 import org.pac4j.play.scala.SecurityComponents
 import play.api.mvc.{Action, AnyContent, Request}
 import services.ConsignmentService
+import services.Statuses.MetadataReviewType
 
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, ZonedDateTime}
+import java.util.UUID
 import javax.inject.Inject
 
 class MetadataReviewController @Inject() (
@@ -18,7 +23,46 @@ class MetadataReviewController @Inject() (
     for {
       consignments <- consignmentService.getConsignmentsForReview(request.token.bearerAccessToken)
     } yield {
-      Ok(views.html.tna.metadataReviews(consignments))
+      Ok(views.html.tna.metadataReviews(consignments.map(toMetadataReviewRequest).sortBy(_.submittedDate)(Ordering[ZonedDateTime])))
     }
   }
+
+  private def toMetadataReviewRequest(consignment: GetConsignmentsForMetadataReview): MetadataReviewRequest = {
+    val formatter = DateTimeFormatter.ofPattern("d'%s' MMMM yyyy")
+    val status = consignment.consignmentStatuses.find(_.statusType == MetadataReviewType.id)
+    val submittedDate = if (status.exists(_.modifiedDatetime.nonEmpty)) {
+      status.map(_.modifiedDatetime.get)
+    } else {
+      status.map(_.createdDatetime)
+    }
+    MetadataReviewRequest(
+      consignmentId = consignment.consignmentid.get,
+      consignmentRef = consignment.consignmentReference,
+      body = consignment.transferringBodyName.getOrElse(""),
+      series = consignment.seriesName.getOrElse(""),
+      submittedDate = submittedDate.get,
+      submittedDateHtml = formatDate(formatter, submittedDate.get)
+    )
+  }
+
+  private def formatDate(formatter: DateTimeFormatter, dateTime: ZonedDateTime): String = {
+    val dayOfMonth = dateTime.getDayOfMonth
+    val date = formatter.format(dateTime).format(getDaySuffix(dayOfMonth))
+    val noOfDays = LocalDate.now().toEpochDay - dateTime.toLocalDate.toEpochDay
+    val days = noOfDays match {
+      case 0   => "Today"
+      case 1   => "Yesterday"
+      case day => s"$day days ago"
+    }
+    s"$date <br> ($days)"
+  }
+
+  private def getDaySuffix(day: Int): String = day match {
+    case 1 | 21 | 31 => "st"
+    case 2 | 22      => "nd"
+    case 3 | 23      => "rd"
+    case _           => "th"
+  }
 }
+
+case class MetadataReviewRequest(consignmentId: UUID, consignmentRef: String, body: String, series: String, submittedDate: ZonedDateTime, submittedDateHtml: String)
