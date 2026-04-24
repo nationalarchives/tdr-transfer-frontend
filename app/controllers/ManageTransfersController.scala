d027c57d0bd45619566c03a9f059847019aa358f
ian-hoyle <ianhoyle@gmail.com>
2026-04-24 08:32:31 +0100

    correct date (#5105)
    * local time for UI
    
    * not include preceding 0 in time

--- app/controllers/ManageTransfersController.scala
+++ app/controllers/ManageTransfersController.scala
@@ -2,13 +2,12 @@ package controllers
 
 import auth.TokenSecurity
 import configuration.{ApplicationConfig, KeycloakConfiguration}
+import controllers.util.DateUtils
 import graphql.codegen.GetConsignmentReviewDetails.getConsignmentReviewDetails.GetConsignmentReviewDetails
 import org.pac4j.play.scala.SecurityComponents
 import play.api.mvc.{Action, AnyContent, Request}
 import services.ConsignmentService
 
-import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, ZonedDateTime}
 import java.util.UUID
 import javax.inject.Inject
 import scala.concurrent.Future
@@ -37,7 +36,6 @@ class ManageTransfersController @Inject() (
   }
 
   private def toConsignmentReviewDetails(reviewDetails: GetConsignmentReviewDetails): ConsignmentReviewDetails = {
-    val formatter = DateTimeFormatter.ofPattern("d'%s' MMMM yyyy, hh:mma")
     ConsignmentReviewDetails(
       consignmentId = reviewDetails.consignmentId,
       // Replace ASCII hyphens with non-breaking hyphens (U+2011) so the reference renders on a single line
@@ -46,30 +44,8 @@ class ManageTransfersController @Inject() (
       reviewStatus = reviewDetails.reviewStatus,
       transferringBodyName = reviewDetails.transferringBodyName.getOrElse(""),
       seriesName = reviewDetails.seriesName.getOrElse(""),
-      lastUpdated = formatDate(formatter, reviewDetails.lastUpdated)
+      lastUpdated = DateUtils.formatWithDaySuffixAndRelative(reviewDetails.lastUpdated)
     )
-  }
-
-  private def formatDate(formatter: DateTimeFormatter, dateTime: ZonedDateTime): String = {
-    val formatted = formatter.format(dateTime).format(getDaySuffix(dateTime.getDayOfMonth))
-    // Lowercase only the trailing AM/PM so the month stays capitalised, e.g. "20th April 2026, 10:32am"
-    val date =
-      if (formatted.endsWith("AM") || formatted.endsWith("PM")) formatted.dropRight(2) + formatted.takeRight(2).toLowerCase
-      else formatted
-    val noOfDays = LocalDate.now().toEpochDay - dateTime.toLocalDate.toEpochDay
-    val days = noOfDays match {
-      case 0   => "Today"
-      case 1   => "Yesterday"
-      case day => s"$day days ago"
-    }
-    s"$date ($days)"
-  }
-
-  private def getDaySuffix(day: Int): String = day match {
-    case 1 | 21 | 31 => "st"
-    case 2 | 22      => "nd"
-    case 3 | 23      => "rd"
-    case _           => "th"
   }
 }
 
