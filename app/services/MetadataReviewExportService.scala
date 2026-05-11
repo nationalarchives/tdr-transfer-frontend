1f9e48fa1c7f1b9039a4451ac7f49246d1e4b0ab
ian-hoyle <ianhoyle@gmail.com>
2026-05-11 14:34:23 +0100

    Tdrd 1372 download all metadata review history (#5119)

---
+++ app/services/MetadataReviewExportService.scala
@@ -0,0 +1,120 @@
+package services
+
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken
+import configuration.{ApplicationConfig, KeycloakConfiguration}
+import controllers.util.{DateUtils, ExcelUtils}
+import graphql.codegen.GetConsignmentDetailsForMetadataReview.{getConsignmentDetailsForMetadataReview => gcdfmr}
+import graphql.codegen.GetConsignmentReviewDetails.getConsignmentReviewDetails.GetConsignmentReviewDetails
+import uk.gov.nationalarchives.tdr.common.utils.statuses.MetadataReviewLogAction._
+
+import java.util.UUID
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+
+case class ResolvedUser(email: String, name: String)
+
+class MetadataReviewExportService @Inject() (
+    consignmentService: ConsignmentService,
+    keycloakConfiguration: KeycloakConfiguration,
+    applicationConfig: ApplicationConfig
+)(implicit val executionContext: ExecutionContext) {
+  private val excelDateFormat = "yyyy-MM-dd'T'HH:mm"
+  private val header = List(
+    "Consignment Ref",
+    "Current Status",
+    "User Email Address",
+    "Last Updated",
+    "Department",
+    "Series",
+    "No. Records",
+    "No. Open",
+    "No. Closed",
+    "Submission No.",
+    "Date Submitted",
+    "Review Action",
+    "Date Reviewed",
+    "Reviewed By",
+    "Reason for Action"
+  )
+
+  def generateReviewHistoryExcel(token: BearerAccessToken): Future[Array[Byte]] = {
+    for {
+      reviewDetails <- consignmentService.getConsignmentReviewDetails(None, token)
+      filteredDetails = reviewDetails.filterNot(rd => applicationConfig.seriesNameFilters.exists(filter => rd.seriesName.exists(_.contains(filter))))
+      consignmentDetails <- batchedTraverse(filteredDetails) { rd =>
+        consignmentService.getConsignmentDetailForMetadataReview(rd.consignmentId, token).map(rd -> _)
+      }
+      allUserIds = collectUserIds(consignmentDetails.map { case (_, consignment) => consignment })
+      resolvedUsers <- resolveUsers(allUserIds)
+    } yield {
+      val rows = consignmentDetails.flatMap { case (reviewDetail, consignment) =>
+        buildRowsForConsignment(reviewDetail, consignment, resolvedUsers)
+      }
+      ExcelUtils.writeExcel("Metadata Review History", header :: rows)
+    }
+  }
+
+  private def collectUserIds(consignments: List[gcdfmr.GetConsignment]): Set[UUID] = {
+    consignments.flatMap { consignment =>
+      consignment.userid :: consignment.metadataReviewLogs.map(_.userId)
+    }.toSet
+  }
+
+  private def resolveUsers(userIds: Set[UUID]): Future[Map[UUID, ResolvedUser]] = {
+    val lookups = batchedTraverse(userIds.toList) { userId =>
+      keycloakConfiguration
+        .userDetails(userId.toString)
+        .map(details => userId -> ResolvedUser(details.email, s"${details.firstName} ${details.lastName}"))
+        .recover { case NonFatal(_) => userId -> ResolvedUser("", "") }
+    }
+    lookups.map(_.toMap)
+  }
+
+  private def batchedTraverse[A, B](items: List[A], batchSize: Int = 5)(f: A => Future[B]): Future[List[B]] = {
+    items.grouped(batchSize).foldLeft(Future.successful(List.empty[B])) { (accFuture, batch) =>
+      accFuture.flatMap { acc =>
+        Future.traverse(batch)(f).map(acc ++ _)
+      }
+    }
+  }
+
+  private def buildRowsForConsignment(
+      reviewDetail: GetConsignmentReviewDetails,
+      consignment: gcdfmr.GetConsignment,
+      resolvedUsers: Map[UUID, ResolvedUser]
+  ): List[List[String]] = {
+    val consignmentRef = reviewDetail.consignmentReference
+    val currentStatus = reviewDetail.reviewStatus
+    val userEmail = resolvedUsers.get(consignment.userid).map(_.email).getOrElse("")
+    val lastUpdated = DateUtils.format(reviewDetail.lastUpdated, excelDateFormat)
+    val department = reviewDetail.transferringBodyName.getOrElse("")
+    val series = reviewDetail.seriesName.getOrElse("")
+    val totalRecords = consignment.totalFiles.toString
+    val totalClosed = consignment.totalClosedRecords.toString
+    val totalOpen = (consignment.totalFiles - consignment.totalClosedRecords).toString
+    val commonFields = List(consignmentRef, currentStatus, userEmail, lastUpdated, department, series, totalRecords, totalOpen, totalClosed)
+    val sortedLogs = consignment.metadataReviewLogs.sortBy(_.eventTime.toInstant.toEpochMilli)
+    val submissions = sortedLogs.filter(_.action == Submission.value)
+    val reviews = sortedLogs.filter(log => log.action == Rejection.value || log.action == Approval.value)
+    val submissionRows = submissions.zipWithIndex.map { case (submission, index) =>
+      val submissionNum = (index + 1).toString
+      val submissionDate = DateUtils.format(submission.eventTime, excelDateFormat)
+      reviews.lift(index) match {
+        case Some(review) =>
+          val logAction = MetadataReviewLogAction(review.action)
+          val reviewedBy = resolvedUsers.get(review.userId).map(_.name).getOrElse("")
+          val reason = review.metadataReviewNotes.getOrElse("")
+          commonFields ++ List(submissionNum, submissionDate, logAction.reviewStatus.value, DateUtils.format(review.eventTime, excelDateFormat), reviewedBy, reason)
+        case None =>
+          commonFields ++ List(submissionNum, submissionDate, "", "", "", "")
+      }
+    }
+    val transferredRow =
+      if (reviewDetail.reviewStatus == Confirmation.reviewStatus.value)
+        List(commonFields ++ List(submissions.size.toString, lastUpdated, Confirmation.reviewStatus.value, "", "", ""))
+      else
+        List.empty
+    submissionRows ++ transferredRow
+  }
+}
