fdc193f57823d5c2eabf4d15ea3c7db9e32e8566
Ian Hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-07-17 12:54:08 +0100

    tidy up

---
+++ npm/src/upload/form/file-types.ts
@@ -0,0 +1,28 @@
+import { IFileWithPath } from "@nationalarchives/file-information"
+
+export type IEntryWithPath = IFileWithPath | IDirectoryWithPath
+export interface IDirectoryWithPath {
+  path: string
+  unreadable?: boolean
+}
+
+export function isFile(entry: IEntryWithPath): entry is IFileWithPath {
+  return (entry as IFileWithPath).file !== undefined
+}
+
+export function isDirectory(
+  entry: IEntryWithPath
+): entry is IDirectoryWithPath {
+  return !isFile(entry)
+}
+
+export function withTimeout<T>(
+  promise: Promise<T>,
+  ms: number,
+  msg: string
+): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) => setTimeout(() => reject(new Error(msg)), ms))
+  ])
+}
