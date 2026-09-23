71d666b398d6cc906907c926c39cc9a9a5588fe3
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-09-23 13:11:18 +0100

    Add CSRF form field to metadata checks page

--- npm/src/checks/redirect-to-checks-results.ts
+++ npm/src/checks/redirect-to-checks-results.ts
@@ -1,8 +1,9 @@
 export const redirectToChecksResults: (
   navigate?: (url: string) => void
 ) => void | Error = (navigate = (url: string) => location.assign(url)) => {
-  const resultsUrlElement: HTMLInputElement | null =
-    document.querySelector("#fileChecksResultsUrl")
+  const resultsUrlElement: HTMLInputElement | null = document.querySelector(
+    "#fileChecksResultsUrl"
+  )
   if (!resultsUrlElement || !resultsUrlElement.value) {
     return Error("No checks results url provided")
   }
